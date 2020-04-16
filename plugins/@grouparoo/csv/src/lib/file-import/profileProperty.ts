2b6aa79abd64f9c3305a80e5002ae5002af0b07d
Evan Tahler <evantahler@gmail.com>
2020-04-16 15:10:06 -0700

    plugins that do not return profile properties return null

--- plugins/@grouparoo/csv/src/lib/file-import/profileProperty.ts
+++ plugins/@grouparoo/csv/src/lib/file-import/profileProperty.ts
@@ -2,5 +2,5 @@ import { ProfilePropertyPluginMethod } from "@grouparoo/core";
 
 export const profileProperty: ProfilePropertyPluginMethod = async () => {
   // we always do nothing when called by profile update... we only exist to be used by the schedule & runs
-  return {};
+  return null;
 };
