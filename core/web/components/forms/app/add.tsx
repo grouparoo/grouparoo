e21acf8339a4eaa6ca5df3c001e5e21810ba76ef
Andy Jih <andy.y.jih@gmail.com>
2020-04-16 11:45:34 -0700

    styling changes, still a WIP

--- core/web/components/forms/app/add.tsx
+++ core/web/components/forms/app/add.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useApi } from "../../../hooks/useApi";
-import { Form, Button, Row, Col, Card } from "react-bootstrap";
+import { Form, Button, Row, Col, Card, CardColumns } from "react-bootstrap";
 import Router from "next/router";
 import AppIcon from "../../appIcon";
 
@@ -37,40 +37,61 @@ export default function ({ apiVersion, errorHandler }) {
     <>
       <Form id="form" onSubmit={create}>
         <Row>
-          {types.map((_app) => {
-            return (
-              <Col key={`plugin-${_app.name}`} md={3}>
-                <Card
-                  style={{ marginBottom: 20 }}
-                  bg={app.type === _app.name ? "success" : "secondary"}
-                  onClick={() => {
-                    const __app = Object.assign({}, app);
-                    __app.type = _app.name;
-                    setApp(__app);
-                  }}
-                >
-                  <Card.Body>
-                    <AppIcon
-                      className="card-img"
-                      src={_app.plugin.icon}
-                      fluid
-                    />
-                    <br />
-                    <br />
-                    <div style={{ textAlign: "center" }}>
-                      <h4>{_app.name}</h4>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
+          <CardColumns>
+            {types.map((_app) => {
+              return (
+                <Col key={`plugin-${_app.name}`} md={3}>
+                  <Card
+                    style={{
+                      width: "16em",
+                      borderRadius: "4px",
+                    }}
+                    bg={app.type === _app.name ? "success" : "secondary"}
+                    onClick={() => {
+                      const __app = Object.assign({}, app);
+                      __app.type = _app.name;
+                      setApp(__app);
+                    }}
+                  >
+                    <Card.Body>
+                      <div
+                        style={{
+                          textAlign: "center",
+                        }}
+                      >
+                        <div
+                          style={{
+                            backgroundColor: "white",
+                            width: "100px",
+                            height: "100px",
+                            borderRadius: "4px",
+                            textAlign: "center",
+                            paddingTop: "5px",
+                          }}
+                        >
+                          <AppIcon
+                            className="card-img"
+                            src={_app.plugin.icon}
+                            size={90}
+                          />
+                        </div>
+                        <br />
+                        <h4>
+                          <div style={{ color: "white" }}>{_app.name}</div>
+                        </h4>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </CardColumns>
         </Row>
 
         <br />
 
         <Button variant="primary" type="submit" active={!loading}>
-          Submit
+          Continue
         </Button>
       </Form>
     </>
