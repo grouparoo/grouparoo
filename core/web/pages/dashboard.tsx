63ba6ecae828fbb823f4bd6ee7073539b90bc4ec
Evan Tahler <evantahler@gmail.com>
2020-05-05 16:39:38 -0700

    nullish check should not check a number

--- core/web/pages/dashboard.tsx
+++ core/web/pages/dashboard.tsx
@@ -15,7 +15,7 @@ export default function (props) {
 
   async function load() {
     const response = await execApi("get", `/api/${props.apiVersion}/apps/`);
-    if (response?.total) {
+    if (response?.apps) {
       setAppsCount(response?.total);
     }
   }
