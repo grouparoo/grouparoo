80837bae663f3f048f58d2df0048969a76e72aab
Evan Tahler <evantahler@gmail.com>
2020-05-06 20:24:21 -0700

    autoload CSRFtoken

--- core/web/pages/swagger.tsx
+++ core/web/pages/swagger.tsx
@@ -11,11 +11,26 @@ export default class Swagger extends Component<Props> {
 
     const { apiVersion } = this.props;
 
-    SwaggerUi({
+    const swagger = SwaggerUi({
       dom_id: "#swaggerContainer",
       url: `/api/${apiVersion}/swagger`,
       presets: [SwaggerUi.presets.apis],
       deepLinking: true,
+      docExpansion: "none",
+      filter: true,
+      tagsSorter: (a, b) => {
+        if (a > b) {
+          return 1;
+        } else {
+          return -1;
+        }
+      },
+      onComplete: function () {
+        swagger.preauthorizeApiKey(
+          "GrouparooCSRFTokenAndSessionCookie",
+          localStorage.getItem("session:csrfToken")
+        );
+      },
     });
   }
 
