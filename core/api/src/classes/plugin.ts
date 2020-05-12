6fb5c84d03a56263392d38c9dbb142edef58c0ff
Evan Tahler <evantahler@gmail.com>
2020-05-12 16:35:25 -0700

    skipSourceMapping is optional

--- core/api/src/classes/plugin.ts
+++ core/api/src/classes/plugin.ts
@@ -70,7 +70,7 @@ export interface PluginConnection {
   name: string;
   description: string;
   direction: "import" | "export";
-  skipSourceMapping: boolean;
+  skipSourceMapping?: boolean;
   app: string;
   options: ConnectionOption[];
   profilePropertyRuleOptions?: PluginConnectionProfilePropertyRuleOption[];
