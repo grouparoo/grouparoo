023b00162e3cb8beee7e488ce78125b480bbfd8f
Evan Tahler <evantahler@gmail.com>
2020-05-14 11:55:22 -0700

    next/webpack should always alias react to be core's react

--- core/web/next.config.js
+++ core/web/next.config.js
@@ -1,9 +1,17 @@
+const path = require("path");
 const env = require("./env");
 
 module.exports = {
   env,
 
   webpack: (config, options) => {
+    config.resolve.alias["react"] = path.resolve(
+      __dirname,
+      "..",
+      "node_modules",
+      "react"
+    );
+
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
