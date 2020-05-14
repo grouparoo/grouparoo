b2ef22d5c1744f91a12812514d28c9216a0562cd
Evan Tahler <evan.tahler@grouparoo.com>
2020-05-14 12:11:07 -0700

    React is a Peer Dependency for UI plugins (#143)
    * react and react-dom are peer deps of plugins
    
    * next/webpack should always alias react to be core's react
    
    * alias react-dom too

--- core/web/next.config.js
+++ core/web/next.config.js
@@ -1,9 +1,23 @@
+const path = require("path");
 const env = require("./env");
 
 module.exports = {
   env,
 
   webpack: (config, options) => {
+    config.resolve.alias["react"] = path.resolve(
+      __dirname,
+      "..",
+      "node_modules",
+      "react"
+    );
+    config.resolve.alias["react-dom"] = path.resolve(
+      __dirname,
+      "..",
+      "node_modules",
+      "react-dom"
+    );
+
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
