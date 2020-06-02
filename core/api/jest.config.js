0bd3b501497d9f9548e583de5109d3598d29f37e
Evan Tahler <evantahler@gmail.com>
2020-06-02 10:49:27 -0700

    load SERVER_TOKEN into test ENV

--- core/api/jest.config.js
+++ core/api/jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   testEnvironment: "node",
+  setupFiles: ["<rootDir>/jest.setup.js"],
   transform: {
-    "^.+\\.ts?$": "ts-jest"
+    "^.+\\.ts?$": "ts-jest",
   },
   testPathIgnorePatterns: [
     "<rootDir>/__tests__/utils",
     "<rootDir>/__tests__/factories",
     "<rootDir>/src/.*/*.ts",
     "<rootDir>/dist/.*/*.ts",
-    "<rootDir>/dist/.*/*.js"
-  ]
+    "<rootDir>/dist/.*/*.js",
+  ],
 };
