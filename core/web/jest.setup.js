3a078af8b59cc02d54a9afc6b87af60fc8021940
Evan Tahler <evantahler@gmail.com>
2020-06-02 09:47:43 -0700

    set process.env.SERVER_TOKEN for page tests

--- core/web/jest.setup.js
+++ core/web/jest.setup.js
@@ -2,3 +2,7 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+if (!process.env.SERVER_TOKEN) {
+  process.env.SERVER_TOKEN = "test-server-token";
+}
