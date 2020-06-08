d88199140d82552c77d0093bf6ce4ca02ba8b501
Evan Tahler <evan.tahler@grouparoo.com>
2020-06-08 08:51:17 -0700

    increase jest defautl test timeout to 10s (#308)

--- core/web/jest.config.js
+++ core/web/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js"],
+  testTimeout: 10000,
   testPathIgnorePatterns: [
     "<rootDir>/.next/",
     "<rootDir>/__tests__/utils",
-    "<rootDir>/__tests__/__mocks__"
-  ]
+    "<rootDir>/__tests__/__mocks__",
+  ],
 };
