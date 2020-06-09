1b86928d586510d8a4e2edec911cb7ab48f17b2d
Evan Tahler <evantahler@gmail.com>
2020-06-09 09:35:58 -0700

    hide rules tab for manual groups

--- core/web/components/tabs/group.tsx
+++ core/web/components/tabs/group.tsx
@@ -5,6 +5,11 @@ import { GroupAPIData } from "../../utils/apiData";
 export default function ({ group }: { group: GroupAPIData }) {
   const [plugins, pluginMetadata] = usePlugins("group/tabs");
   const tabs = ["edit", "rules", "members", "destinations", "runs", "logs"];
+
+  if (group.type === "manual") {
+    tabs.splice(tabs.indexOf("rules"), 1);
+  }
+
   pluginMetadata.forEach((p) => {
     tabs.push(p.key);
   });
