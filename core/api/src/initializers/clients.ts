612d033c489d724c6effd43dcde7e63972cbc98d
Evan Tahler <evantahler@gmail.com>
2020-06-10 16:18:45 -0700

    ensure publicJSDir exists

--- core/api/src/initializers/clients.ts
+++ core/api/src/initializers/clients.ts
@@ -9,6 +9,17 @@ export class Files extends Initializer {
   }
 
   async initialize() {
+    const publicJSDir = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "web",
+      "public",
+      "javascript"
+    );
+    if (!fs.existsSync(publicJSDir)) fs.mkdirSync(path.join(publicJSDir));
+
     // copy the web client javascript from the plugin to the public directory
     const source = path.join(
       __dirname,
@@ -22,17 +33,7 @@ export class Files extends Initializer {
       "grouparooWebClient.js"
     );
 
-    const destination = path.join(
-      __dirname,
-      "..",
-      "..",
-      "..",
-      "web",
-      "public",
-      "javascript",
-      "grouparooWebClient.js"
-    );
-
+    const destination = path.join(publicJSDir, "grouparooWebClient.js");
     fs.copyFileSync(source, destination);
   }
 }
