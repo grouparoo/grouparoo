6a419e006342761c975f0bf990dfeb3f8b6dd33f
Evan Tahler <evantahler@gmail.com>
2020-06-24 11:27:08 -0700

    project init

---
+++ plugins/@grouparoo/hubspot/README.md
@@ -0,0 +1,5 @@
+# @Grouparoo/Hubspot
+
+## Attribution
+
+The Hubspot icon used in this plugin is from https://www.hubspot.com/style-guide
---
+++ plugins/@grouparoo/hubspot/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: "node",
+  testTimeout: 10000,
+  setupFiles: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/src",
+    "<rootDir>/dist",
+    "<rootDir>/__tests__/fixtures",
+  ],
+};
