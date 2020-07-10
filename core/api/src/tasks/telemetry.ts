f82b70c401bf436edb80c484663e2d9ff9bc7329
Evan Tahler <evantahler@gmail.com>
2020-07-10 16:55:02 -0700

    do not report full paths about the telemetry error

--- core/api/src/tasks/telemetry.ts
+++ core/api/src/tasks/telemetry.ts
@@ -1,6 +1,7 @@
 import { Task, api, config } from "actionhero";
 import fetch from "isomorphic-fetch";
 import { plugin } from "../modules/plugin";
+import path from "path";
 import os from "os";
 
 export class Telemetry extends Task {
@@ -26,40 +27,59 @@ export class Telemetry extends Task {
 
       return response.status === 200;
     } catch (error) {
-      // if there's something wrong with our ability to gather telemetry, we should try to report that error to the Telemetry service...
       try {
-        const clusterName = (await plugin.readSetting("core", "cluster-name"))
-          .value;
-        const customerGuid = (
-          await plugin.readSetting("telemetry", "customer-guid")
-        ).value;
-        const customerLicense = (
-          await plugin.readSetting("telemetry", "customer-license")
-        ).value;
-
-        const errorPayload = {
-          name: clusterName,
-          guid: customerGuid,
-          license: customerLicense,
-          metrics: [
-            {
-              collection: "telemetry",
-              topic: "error",
-              aggregation: "exact",
-              key: `${process.platform}/${os.release()}`,
-              value: error.stack || error.toString(),
-            },
-          ],
-        };
-
+        // If there's something wrong with our ability to gather telemetry,
+        // we should try to report that error to the Telemetry service...
         await fetch(config.telemetry.url, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(errorPayload),
+          body: JSON.stringify(await this.generateErrorPayload(error)),
         });
       } catch (newError) {}
 
       throw error;
     }
+  }
+
+  async generateErrorPayload(error) {
+    const clusterName = (await plugin.readSetting("core", "cluster-name"))
+      .value;
+    const customerGuid = (
+      await plugin.readSetting("telemetry", "customer-guid")
+    ).value;
+    const customerLicense = (
+      await plugin.readSetting("telemetry", "customer-license")
+    ).value;
+
+    const errorPayload = {
+      name: clusterName,
+      guid: customerGuid,
+      license: customerLicense,
+      metrics: [
+        {
+          collection: "telemetry",
+          topic: "error",
+          aggregation: "exact",
+          key: `${process.platform}/${os.release()}`,
+          value: this.sanitizeErrorPayload(error),
+        },
+      ],
+    };
+
+    return errorPayload;
+  }
+
+  private sanitizeErrorPayload(error: Error): string {
+    if (!error.stack) return error.toString();
+
+    return error.stack
+      .split(os.EOL)
+      .map((line: string) => {
+        // It's likely the part of the path with 'grouparoo' in it would be safely down in the path structure to not include app-information
+        // There may be many instances of the word "grouparoo" in the file system, so we'll only take the last part of the path
+        const chunks = line.split("grouparoo" + path.sep);
+        return chunks[chunks.length - 1];
+      })
+      .join(os.EOL);
   }
 }
