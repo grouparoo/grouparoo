6162cc2b21e0d5ced6da6e567a59071271b4c4f1
Evan Tahler <evantahler@gmail.com>
2020-07-14 12:07:39 -0700

    Disable next.js when testing api server

--- core/api/src/config/next.ts
+++ core/api/src/config/next.ts
@@ -1,10 +1,11 @@
 // learn more about the next.js app options here https://nextjs.org/docs/advanced-features/custom-server
 
 export const DEFAULT = {
-  next: (config) => {
+  next: () => {
     const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 
     return {
+      enabled: process.env.NEXT_DISABLED === "true" ? false : true,
       dev: process.env.NEXT_DEVELOPMENT_MODE
         ? process.env.NEXT_DEVELOPMENT_MODE === "false"
           ? false
