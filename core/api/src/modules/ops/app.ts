ef9083b0181bd1d0daa0d1b02aeecb21cd4f55ea
Evan Tahler <evantahler@gmail.com>
2020-07-15 18:09:13 -0700

    revert changes that led to multi-line IFs (+1)

--- core/api/src/modules/ops/app.ts
+++ core/api/src/modules/ops/app.ts
@@ -52,8 +52,9 @@ export namespace AppOps {
     let error;
 
     const { pluginApp } = await app.getPlugin();
-    if (!pluginApp)
+    if (!pluginApp) {
       throw new Error(`cannot find a pluginApp type of ${app.type}`);
+    }
 
     if (!options) {
       options = await app.getOptions();
