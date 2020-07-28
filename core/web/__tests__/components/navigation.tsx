c62285a45738234129f5e107ce6ad9ca629f5672
Evan Tahler <evantahler@gmail.com>
2020-07-27 21:06:29 -0700

    Mock next/link in test

--- core/web/__tests__/components/navigation.tsx
+++ core/web/__tests__/components/navigation.tsx
@@ -4,53 +4,54 @@ import Component from "../../components/navigation";
 import "../../components/icons"; // this is needed to load the library
 import commonProps from "../utils/commonProps";
 
+// TODO: it seems that with next v9.5.0 you cannot test a component which uses <Link> outside of a page...
+// https://github.com/vercel/next.js/issues/15543
+jest.mock("next/link", () => {
+  return ({ children }) => {
+    return children;
+  };
+});
+
 describe("navigation", () => {
   let wrapper;
 
-  // TODO: it seems that with next v9.5.0 you cannot test a component which uses <Link> outside of a page...
-  // https://github.com/vercel/next.js/issues/15543
+  beforeEach(() => {
+    wrapper = mount(
+      <Component
+        {...commonProps}
+        pathname="/some/page"
+        currentTeamMember={{
+          firstName: "mario",
+          guid: "abc123",
+        }}
+        navigationMode="authenticated"
+        navigation={{
+          navigationItems: [
+            { type: "link", title: "Dashboard", href: "/dashboard" },
+          ],
+          platformItems: [],
+          bottomMenuItems: [
+            {
+              type: "link",
+              title: "Something Cool",
+              href: "/something/cool",
+            },
+            { type: "link", title: "Sign Out", href: "/session/sign-out" },
+          ],
+        }}
+      />
+    );
+  });
 
-  // beforeEach(() => {
-  //   wrapper = mount(
-  //     <Component
-  //       {...commonProps}
-  //       pathname="/some/page"
-  //       currentTeamMember={{
-  //         firstName: "mario",
-  //         guid: "abc123",
-  //       }}
-  //       navigationMode="authenticated"
-  //       navigation={{
-  //         navigationItems: [
-  //           { type: "link", title: "Dashboard", href: "/dashboard" },
-  //         ],
-  //         platformItems: [],
-  //         bottomMenuItems: [
-  //           {
-  //             type: "link",
-  //             title: "Something Cool",
-  //             href: "/something/cool",
-  //           },
-  //           { type: "link", title: "Sign Out", href: "/session/sign-out" },
-  //         ],
-  //       }}
-  //     />
-  //   );
-  // });
+  afterEach(() => {
+    wrapper.unmount();
+    mockAxios.reset();
+  });
 
-  // afterEach(() => {
-  //   wrapper.unmount();
-  //   mockAxios.reset();
-  // });
-
-  // test("shows the nav returned from the server", async () => {
-  //   const html = wrapper.html();
-  //   expect(html).toContain('href="/dashboard">Dashboard</a>');
-  //   expect(html).toContain('href="/something/cool">Something Cool</a>');
-  //   expect(html).toContain('href="/session/sign-out">Sign Out</a>');
-  // });
-
-  it("should work", () => {
-    expect(1).toBe(1);
+  test("shows the nav returned from the server", async () => {
+    const html = wrapper.html();
+    expect(html).toContain("Dashboard</a>");
+    expect(html).toContain("Something Cool</a>");
+    expect(html).toContain("Sign Out</a>");
   });
 });
