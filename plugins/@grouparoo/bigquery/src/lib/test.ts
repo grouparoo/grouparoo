e27cad8019b57161d5f64816d50c291be13287fa
Evan Tahler <evan.tahler@grouparoo.com>
2020-07-30 12:17:47 -0700

    Redshift Plugin (Source and Destination) (#515)
    * remove errant log messages
    
    * The redshift plugin
    
    * show draft app name in title
    
    * show loading component when fetching source preview
    
    * postres (and redshift) cannot rely on ON CONFLICT DO NOTHING
    
    * Updating things automatically
    
    Co-authored-by: Grouparoo Bot <hello@grouparoo.com>

--- plugins/@grouparoo/bigquery/src/lib/test.ts
+++ plugins/@grouparoo/bigquery/src/lib/test.ts
@@ -5,13 +5,11 @@ export const test: TestPluginMethod = async ({ connection }) => {
     query: 'SELECT "grouparoo" as message',
   });
   const success = response[0][0].message === "grouparoo";
-  console.log({ success });
 
   const rowCountResponse = await connection.query({
     query: `select count(catalog_name) as __count from INFORMATION_SCHEMA.SCHEMATA`,
   });
   const message = `found ${rowCountResponse[0][0].__count} tables in this database`;
-  console.log({ message });
 
   return { success, message };
 };
---
+++ plugins/@grouparoo/redshift/README.md
@@ -0,0 +1,9 @@
+# @Grouparoo/Redshift
+
+## Notes
+
+This plugin relies on `@grouparoo/postgres`
+
+## Attribution
+
+The Postgres icon used in this plugin is from https://worldvectorlogo.com/downloaded/aws-redshift-logo
