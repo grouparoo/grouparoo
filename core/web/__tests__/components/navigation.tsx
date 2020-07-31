ccafd7fa7c54c5d6efdf19c47d3782f084637b63
Evan Tahler <evan.tahler@grouparoo.com>
2020-07-30 17:09:19 -0700

    Upgrade dependencies (#516)
    * Update deps
    
    * remove next/link test hack

--- core/web/__tests__/components/navigation.tsx
+++ core/web/__tests__/components/navigation.tsx
@@ -4,14 +4,6 @@ import Component from "../../components/navigation";
 import "../../components/icons"; // this is needed to load the library
 import commonProps from "../utils/commonProps";
 
-// TODO: it seems that with next v9.5.0 you cannot test a component which uses <Link> outside of a page...
-// https://github.com/vercel/next.js/issues/15543
-jest.mock("next/link", () => {
-  return ({ children }) => {
-    return children;
-  };
-});
-
 describe("navigation", () => {
   let wrapper;
 
