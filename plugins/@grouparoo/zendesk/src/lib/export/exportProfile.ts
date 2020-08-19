f657d67bfb9457eca6683fd412d083646d34f492
Brian Leonard <brian@bleonard.com>
2020-08-18 17:56:18 -0700

    rate limiting

--- plugins/@grouparoo/zendesk/src/lib/export/exportProfile.ts
+++ plugins/@grouparoo/zendesk/src/lib/export/exportProfile.ts
@@ -16,119 +16,144 @@ export const exportProfile: ExportProfilePluginMethod = async ({
     oldGroups,
   },
 }) => {
-  const client = await connect(appOptions);
-  let external_id = newProfileProperties.external_id;
-  let email = newProfileProperties.email;
+  try {
+    const client = await connect(appOptions);
+    let external_id = newProfileProperties.external_id;
+    let email = newProfileProperties.email;
 
-  // console.log("exportProfile", newProfileProperties);
+    // console.log("exportProfile", newProfileProperties);
 
-  if (!external_id) {
-    throw new Error(`newProfileProperties[external_id] is a required mapping`);
-  }
-
-  // zendesk does this itself
-  if (email) {
-    email = email.toLowerCase().trim();
-  }
-
-  const user = await findUser(
-    client,
-    newProfileProperties,
-    oldProfileProperties
-  );
-
-  if (toDelete) {
-    if (user) {
-      // this does a soft delete
-      await client.users.delete(user.id);
-
-      // TODO: should we fully delete it? (see test for how to set it up)
-      // this might delete tickets
-      // await client.deleted_users.delete(id);
-
-      // Note: From Documentation
-      // You can permanently delete 700 users every 10 minutes.
-      // The rate limiting mechanism behaves as described in Rates Limits in the API introduction.
-      // Zendesk recommends that you obey the Retry-After header values.
+    if (!external_id) {
+      throw new Error(
+        `newProfileProperties[external_id] is a required mapping`
+      );
     }
-    return { success: true };
-  }
 
-  const payload: any = { verified: true };
-
-  // set profile properties, including old ones
-  const newKeys = Object.keys(newProfileProperties);
-  const oldKeys = Object.keys(oldProfileProperties);
-  const allKeys = oldKeys.concat(newKeys);
-  const fields = await getFields(client);
-  for (const key of allKeys) {
-    const value = newProfileProperties[key]; // includes clearing out removed ones
-    const field = fields[key];
-    if (!field) {
-      // TODO: need to handle these changing out from under?
-      throw `unknown zendesk field: ${key}`;
+    // zendesk does this itself
+    if (email) {
+      email = email.toLowerCase().trim();
     }
-    const formatted = formatVar(value, field);
-    if (field.custom) {
-      payload.user_fields = payload.user_fields || {};
-      payload.user_fields[key] = formatted;
+
+    const user = await findUser(
+      client,
+      newProfileProperties,
+      oldProfileProperties
+    );
+
+    if (toDelete) {
+      if (user) {
+        // this does a soft delete
+        await client.users.delete(user.id);
+
+        // TODO: should we fully delete it? (see test for how to set it up)
+        // this might delete tickets
+        // await client.deleted_users.delete(id);
+
+        // Note: From Documentation
+        // You can permanently delete 700 users every 10 minutes.
+        // The rate limiting mechanism behaves as described in Rates Limits in the API introduction.
+        // Zendesk recommends that you obey the Retry-After header values.
+      }
+      return { success: true };
+    }
+
+    const payload: any = { verified: true };
+
+    // set profile properties, including old ones
+    const newKeys = Object.keys(newProfileProperties);
+    const oldKeys = Object.keys(oldProfileProperties);
+    const allKeys = oldKeys.concat(newKeys);
+    const fields = await getFields(client);
+    for (const key of allKeys) {
+      const value = newProfileProperties[key]; // includes clearing out removed ones
+      const field = fields[key];
+      if (!field) {
+        // TODO: need to handle these changing out from under?
+        throw `unknown zendesk field: ${key}`;
+      }
+      const formatted = formatVar(value, field);
+      if (field.custom) {
+        payload.user_fields = payload.user_fields || {};
+        payload.user_fields[key] = formatted;
+      } else {
+        payload[key] = formatted;
+      }
+    }
+
+    // tags need to be formatted
+    const newTags = newGroups.map((groupName) => makeTagName(groupName));
+    const oldTags = oldGroups.map((groupName) => makeTagName(groupName));
+    const removedTags = oldTags.filter((k) => !newTags.includes(k));
+    // get the current tags if there was a user
+    const currentTags = user?.tags || [];
+    for (const tagName of currentTags) {
+      // add it to newTags if it's not there already or in removedTags
+      if (removedTags.includes(tagName)) {
+        // it got removed, don't add it back
+        continue;
+      }
+      if (!newTags.includes(tagName)) {
+        // it is from zendesk directly, add it back
+        newTags.push(tagName);
+      }
+    }
+    payload.tags = newTags.sort();
+
+    // do the update
+
+    // it needs a "name" property on the user record
+    if (payload.hasOwnProperty("name")) {
+      // grouparoo is handling the name, default it
+      if ((payload.name || "").trim().length === 0) {
+        payload.name = getDefaultName(payload);
+      }
     } else {
-      payload[key] = formatted;
+      // grouparoo is not handling the name
+      if (!user || (user.name || "").trim().length === 0) {
+        // still needs one to be created
+        payload.name = getDefaultName(payload);
+      }
     }
-  }
 
-  // tags need to be formatted
-  const newTags = newGroups.map((groupName) => makeTagName(groupName));
-  const oldTags = oldGroups.map((groupName) => makeTagName(groupName));
-  const removedTags = oldTags.filter((k) => !newTags.includes(k));
-  // get the current tags if there was a user
-  const currentTags = user?.tags || [];
-  for (const tagName of currentTags) {
-    // add it to newTags if it's not there already or in removedTags
-    if (removedTags.includes(tagName)) {
-      // it got removed, don't add it back
-      continue;
+    let updated;
+    if (user) {
+      // console.log("user.update", payload);
+      updated = await client.users.update(user.id, { user: payload });
+    } else {
+      // console.log("user.createOrUpdate", payload);
+      updated = await client.users.createOrUpdate({ user: payload });
     }
-    if (!newTags.includes(tagName)) {
-      // it is from zendesk directly, add it back
-      newTags.push(tagName);
+
+    if (email && updated.email !== email) {
+      // console.log("updating email", `${updated.email} -> ${email}`);
+      // have to make this the primary
+      // https://developer.zendesk.com/rest_api/docs/support/users#email-address
+      await makeEmailPrimary(client, updated.id, email);
     }
-  }
-  payload.tags = newTags.sort();
 
-  // do the update
+    return { success: true };
+  } catch (error) {
+    // https://develop.zendesk.com/hc/en-us/articles/360001074328-Best-practices-for-avoiding-rate-limiting
 
-  // it needs a "name" property on the user record
-  if (payload.hasOwnProperty("name")) {
-    // grouparoo is handling the name, default it
-    if ((payload.name || "").trim().length === 0) {
-      payload.name = getDefaultName(payload);
+    // headers
+    // X-Rate-Limit: 700
+    // X-Rate-Limit-Remaining: 699
+
+    // The response contains the following key information:
+    // error.statusCode = 429;
+    // error.retryAfter = retryAfter;
+    if (error?.statusCode === 429) {
+      let retryIn = error.retryAfter; // seconds
+      if (!retryIn || retryIn < 0) {
+        retryIn = 60; // per minute
+      }
+      // add some random time to that to spread it out
+      retryIn += Math.floor(Math.random() * 30) + 1;
+      return { error, success: false, retryDelay: 1000 * retryIn };
     }
-  } else {
-    // grouparoo is not handling the name
-    if (!user || (user.name || "").trim().length === 0) {
-      // still needs one to be created
-      payload.name = getDefaultName(payload);
-    }
-  }
 
-  let updated;
-  if (user) {
-    // console.log("user.update", payload);
-    updated = await client.users.update(user.id, { user: payload });
-  } else {
-    // console.log("user.createOrUpdate", payload);
-    updated = await client.users.createOrUpdate({ user: payload });
+    throw error;
   }
-
-  if (email && updated.email !== email) {
-    // console.log("updating email", `${updated.email} -> ${email}`);
-    // have to make this the primary
-    // https://developer.zendesk.com/rest_api/docs/support/users#email-address
-    await makeEmailPrimary(client, updated.id, email);
-  }
-
-  return { success: true };
 };
 
 async function searchForUser(client, findBy: any) {
