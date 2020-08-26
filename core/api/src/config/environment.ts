0e155df96ef0a2e1cba167e28870897f1ec4abca
Evan Tahler <evantahler@gmail.com>
2020-08-26 13:44:17 -0700

    load .env and serverInjection as part of config

---
+++ core/api/src/config/environment.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import path from "path";
+import { getParentPath } from "../utils/pluginDetails";
+
+// load env from .env in development
+// this needs to be done before requiring any part of actionhero
+const env = process.env.NODE_ENV || "development";
+if (env === "development") {
+  const envFile = path.resolve(path.join(getParentPath(), ".env"));
+  if (fs.existsSync(envFile)) {
+    require("dotenv").config({ path: envFile });
+    console.log(`modified your runtime environment with ${envFile}`);
+  }
+}
