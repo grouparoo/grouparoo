317cbea4c178a07e9cce7bb88caafb38a717ace8
Evan Tahler <evantahler@gmail.com>
2020-09-09 16:15:31 -0700

    For Sources, get random records with a JS-generated random offset

--- plugins/@grouparoo/mysql/src/lib/table-import/sourcePreview.ts
+++ plugins/@grouparoo/mysql/src/lib/table-import/sourcePreview.ts
@@ -6,11 +6,38 @@ export const sourcePreview: SourcePreviewMethod = async ({
 }) => {
   const response = [];
 
-  const preview = await connection.asyncQuery(
-    `SELECT * FROM ?? ORDER BY RAND() LIMIT 10 `,
+  // For large datasets, `order by RAND()` is actually very slow in MySQL
+  // To that end, we are going to pick numbers at random in JS and select just the rows we want, one at a time.
+  // We also cannot assume that our source has an incremental & unique primary key.
+  // https://stackoverflow.com/questions/1244555/how-can-i-optimize-mysqls-order-by-rand-function
+
+  const countResult = await connection.asyncQuery(
+    `SELECT COUNT(1) as __count FROM ??`,
     [sourceOptions.table]
   );
-  preview.map((row) => response.push(row));
+  const count = parseInt(countResult[0]["__count"]) - 1;
+  const offsets = getRandomNumbersInRange(count, 10);
+
+  for (const i in offsets) {
+    const rowResult = await connection.asyncQuery(
+      `SELECT * FROM ?? LIMIT 1 OFFSET ?`,
+      [sourceOptions.table, offsets[i]]
+    );
+    response.push(rowResult[0]);
+  }
 
   return response;
 };
+
+function getRandomNumbersInRange(max: number, size: number) {
+  const array: number[] = [];
+  let attempts = 0;
+
+  while (array.length < size && attempts < size * 2) {
+    var attempt = Math.floor(Math.random() * max) + 1;
+    if (!array.includes(attempt)) array.push(attempt);
+    attempts++;
+  }
+
+  return array;
+}
--- plugins/@grouparoo/postgres/src/lib/table-import/sourcePreview.ts
+++ plugins/@grouparoo/postgres/src/lib/table-import/sourcePreview.ts
@@ -7,10 +7,41 @@ export const sourcePreview: SourcePreviewMethod = async ({
 }) => {
   const response = [];
 
-  const { rows } = await connection.query(
-    format(`SELECT * FROM %I ORDER BY RANDOM() LIMIT 10`, sourceOptions.table)
+  // For large datasets, `order by RANDOM()` is actually very slow in Postgres
+  // To that end, we are going to pick numbers at random in JS and select just the rows we want, one at a time.
+  // We also cannot assume that our source has an incremental & unique primary key.
+  // We cannot use TABLESAMPLE as it was only introduced in Postgres 9.5 (redshift for example uses Postgres 8)
+  // https://stackoverflow.com/questions/8674718/best-way-to-select-random-rows-postgresql
+
+  const { rows: countRows } = await connection.query(
+    format(`SELECT COUNT(1) as __count FROM %I`, sourceOptions.table)
   );
-  rows.map((row) => response.push(row));
+  const count = parseInt(countRows[0]["__count"]) - 1;
+  const offsets = getRandomNumbersInRange(count, 10);
+
+  for (const i in offsets) {
+    const { rows: recordRows } = await connection.query(
+      format(
+        `SELECT * FROM %I LIMIT 1 OFFSET ?`,
+        sourceOptions.table,
+        offsets[i]
+      )
+    );
+    response.push(recordRows[0]);
+  }
 
   return response;
 };
+
+function getRandomNumbersInRange(max: number, size: number) {
+  const array: number[] = [];
+  let attempts = 0;
+
+  while (array.length < size && attempts < size * 2) {
+    var attempt = Math.floor(Math.random() * max) + 1;
+    if (!array.includes(attempt)) array.push(attempt);
+    attempts++;
+  }
+
+  return array;
+}
