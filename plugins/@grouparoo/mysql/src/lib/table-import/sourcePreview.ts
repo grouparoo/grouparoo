df5cc49bab8e7c9e8d2000c1fbc945ac2cc43f48
Evan Tahler <evantahler@gmail.com>
2020-09-09 17:15:26 -0700

    Just show the first 10 rows of the source in SourcePreview

--- plugins/@grouparoo/mysql/src/lib/table-import/sourcePreview.ts
+++ plugins/@grouparoo/mysql/src/lib/table-import/sourcePreview.ts
@@ -7,37 +7,14 @@ export const sourcePreview: SourcePreviewMethod = async ({
   const response = [];
 
   // For large datasets, `order by RAND()` is actually very slow in MySQL
-  // To that end, we are going to pick numbers at random in JS and select just the rows we want, one at a time, as we cannot use functions in the OFFSET in MySQL.
+  // To that end, we would need to pick numbers at random in JS and select just the rows we want, one at a time, as we cannot use functions in the OFFSET in MySQL.
   // We also cannot assume that our source has an incremental & unique primary key.
   // https://stackoverflow.com/questions/1244555/how-can-i-optimize-mysqls-order-by-rand-function
+  // ... But for now we will just show the first 10 rows of the table
 
-  const countResult = await connection.asyncQuery(
-    `SELECT COUNT(1) as __count FROM ??`,
-    [sourceOptions.table]
-  );
-  const count = parseInt(countResult[0]["__count"]);
-  const offsets = getRandomNumbersInRange(count, 10);
-
-  for (const i in offsets) {
-    const rowResult = await connection.asyncQuery(
-      `SELECT * FROM ?? LIMIT 1 OFFSET ?`,
-      [sourceOptions.table, offsets[i]]
-    );
-    response.push(rowResult[0]);
-  }
+  await connection
+    .asyncQuery(`SELECT * FROM ?? LIMIT 10`, [sourceOptions.table])
+    .map((row) => response.push(row));
 
   return response;
 };
-
-function getRandomNumbersInRange(max: number, size: number) {
-  const array: number[] = [];
-  let attempts = 0;
-
-  while (array.length < size && attempts < size * 2) {
-    var attempt = Math.floor(Math.random() * max - 1) + 1;
-    if (!array.includes(attempt)) array.push(attempt);
-    attempts++;
-  }
-
-  return array;
-}
--- plugins/@grouparoo/postgres/src/lib/table-import/sourcePreview.ts
+++ plugins/@grouparoo/postgres/src/lib/table-import/sourcePreview.ts
@@ -8,30 +8,16 @@ export const sourcePreview: SourcePreviewMethod = async ({
   const response = [];
 
   // For large datasets, `order by RANDOM()` is actually very slow in Postgres
-  // To that end, we are going to pick numbers at random in the Offset as a multiple of the number of rows.
+  // To that end, we would need to pick numbers at random in the Offset as a multiple of the number of rows.
   // We also cannot assume that our source has an incremental & unique primary key.
   // We cannot use TABLESAMPLE as it was only introduced in Postgres 9.5 (redshift for example uses Postgres 8)
   // https://stackoverflow.com/questions/8674718/best-way-to-select-random-rows-postgresql
+  // ... But for now we will just show the first 10 rows of the table
 
-  const { rows: countRows } = await connection.query(
-    format(`SELECT COUNT(1) as __count FROM %I`, sourceOptions.table)
+  const { rows } = await connection.query(
+    format(`SELECT * FROM %I LIMIT 10`, sourceOptions.table)
   );
-  const count = parseInt(countRows[0]["__count"]);
-  const limit = 10;
-  let attempts = 0;
-
-  while (attempts < limit) {
-    const { rows: recordRows } = await connection.query(
-      format(
-        `SELECT * FROM %I LIMIT 1 OFFSET floor(random() * %L)`,
-        sourceOptions.table,
-        count
-      )
-    );
-
-    response.push(recordRows[0]);
-    attempts++;
-  }
+  rows.map((row) => response.push(row));
 
   return response;
 };
