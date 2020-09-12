71dcb554a2ed6d763462b7cdc9cf893fc6ba22fc
Evan Tahler <evan.tahler@grouparoo.com>
2020-09-11 17:10:11 -0700

    /object can route schedule links to the proper page (#721)

--- core/web/pages/object/[guid].tsx
+++ core/web/pages/object/[guid].tsx
@@ -1,5 +1,6 @@
 import Loader from "../../components/loader";
 import Router from "next/router";
+import { useApi } from "../../hooks/useApi";
 
 const guidPrefixes = {
   api: "/apiKey/[guid]/edit",
@@ -13,19 +14,35 @@ const guidPrefixes = {
   pro: "/profile/[guid]/edit",
   rul: "/profilePropertyRule/[guid]/edit",
   run: "/run/[guid]/edit",
-  // sch: "/schedule/[guid]/edit",
+  sch: "/source/[guid]/overview",
   src: "/source/[guid]/overview",
   tea: "/team/[guid]/edit",
   tem: "/teamMember/[guid]/edit",
 };
 
-export default function FindObject({ query, errorHandler }) {
+export default function FindObject(props) {
+  const { query, errorHandler } = props;
+  const { execApi } = useApi(props, errorHandler);
+
   const guid = query.guid;
   const prefix = guid.split("_")[0];
   const route = guidPrefixes[prefix];
 
+  async function load(model, guid) {
+    return execApi("get", `/${model}/${guid}`);
+  }
+
+  async function routeScheduleToSource() {
+    const response = await load("schedule", guid);
+    if (response?.schedule) {
+      Router.push(`/source/${response.schedule.source.guid}/schedule`);
+    }
+  }
+
   if (!route) {
     errorHandler.set({ error: `Sorry, I don't know what a "${guid}" is :(` });
+  } else if (prefix === "sch") {
+    routeScheduleToSource();
   } else {
     const href = route.replace("[guid]", guid);
     Router.push(href);
