d0edb4b7c9bc552b49b87145033600bcdd533f3a
Evan Tahler <evantahler@gmail.com>
2020-09-17 11:58:32 -0700

    Use stats API to load something about your Marketo instance for Test

--- plugins/@grouparoo/marketo/src/lib/test.ts
+++ plugins/@grouparoo/marketo/src/lib/test.ts
@@ -4,10 +4,16 @@ import { connect } from "./connect";
 export const test: TestPluginMethod = async ({ appOptions }) => {
   try {
     const client = await connect(appOptions);
-    const response = await client.lead.describe();
+    const response = await client.stats.usage();
     const success = response.success ? true : false;
-    return { success };
+    const message = ` Seeing stats as ${
+      response?.result[0]?.users[0]?.userId || "Marketo user"
+    }`;
+    return { success, message };
   } catch (error) {
-    return { success: false, error: error.error_description };
+    return {
+      success: false,
+      error: error.error_description,
+    };
   }
 };
