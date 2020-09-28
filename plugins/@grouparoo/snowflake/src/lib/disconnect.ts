1c70e71ce15c9052d665e21ca9eaace6353f5dad
Brian Leonard <brian@bleonard.com>
2020-09-28 15:22:35 -0700

    Remove comment

--- plugins/@grouparoo/snowflake/src/lib/disconnect.ts
+++ plugins/@grouparoo/snowflake/src/lib/disconnect.ts
@@ -1,6 +1,5 @@
 import { DisconnectPluginAppMethod } from "@grouparoo/core";
 
 export const disconnect: DisconnectPluginAppMethod = async ({ connection }) => {
-  // nothing to do?
   await connection.destroy();
 };
