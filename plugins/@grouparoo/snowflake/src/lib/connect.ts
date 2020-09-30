9858bd2605ec13b488bf332184f8f3ad76be1f2e
Brian Leonard <brian@bleonard.com>
2020-09-30 14:29:20 -0700

    Use to action hero logger and not write to file.

--- plugins/@grouparoo/snowflake/src/lib/connect.ts
+++ plugins/@grouparoo/snowflake/src/lib/connect.ts
@@ -1,9 +1,15 @@
 import { Snowflake } from "snowflake-promise";
+import SnowflakeLogger from "snowflake-sdk/lib/logger";
+import SnowflakeUtil from "snowflake-sdk/lib/util";
 import { ConnectPluginAppMethod } from "@grouparoo/core";
+import { log } from "actionhero";
 
 export const connect: ConnectPluginAppMethod = async ({ appOptions }) => {
   let { account, username, password, warehouse, database, schema } = appOptions;
   schema = schema || "PUBLIC";
+
+  const logger = SnowflakeLogger.getInstance();
+  SnowflakeLogger.setInstance(logToActionhero(logger));
 
   const client = new Snowflake({
     account,
@@ -19,3 +25,18 @@ export const connect: ConnectPluginAppMethod = async ({ appOptions }) => {
   await client.connect();
   return client;
 };
+
+function logMessage(level) {
+  return function (...messageAndArgs) {
+    const message = SnowflakeUtil.format(...messageAndArgs);
+    log(message, level);
+  };
+}
+function logToActionhero(orig) {
+  orig.error = logMessage("error");
+  orig.warn = logMessage("warning");
+  orig.info = logMessage("info");
+  orig.debug = logMessage("debug");
+  orig.trace = logMessage("debug");
+  return orig;
+}
