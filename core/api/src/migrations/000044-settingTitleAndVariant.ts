d1a7b9bed4551377b9aa628f76d2ed10174ca03e
Evan Tahler <evantahler@gmail.com>
2020-10-01 17:27:19 -0700

    fixup setting migration not-null constaint

--- core/api/src/migrations/000044-settingTitleAndVariant.ts
+++ core/api/src/migrations/000044-settingTitleAndVariant.ts
@@ -2,10 +2,20 @@ module.exports = {
   up: async function (migration, DataTypes) {
     await migration.addColumn("settings", "title", {
       type: DataTypes.STRING(191),
+      allowNull: true,
+      defaultValue: "",
+    });
+    await migration.changeColumn("settings", "title", {
+      type: DataTypes.STRING(191),
       allowNull: false,
     });
 
     await migration.addColumn("settings", "variant", {
+      type: DataTypes.STRING(191),
+      allowNull: true,
+      defaultValue: "",
+    });
+    await migration.changeColumn("settings", "variant", {
       type: DataTypes.STRING(191),
       allowNull: false,
     });
