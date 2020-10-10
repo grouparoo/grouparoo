e1d71a22f7ff87df80d40e038dc7fdfd079c7280
Evan Tahler <evantahler@gmail.com>
2020-10-09 17:19:45 -0700

    Display out of date package versions against latest NPM tag

--- core/api/src/actions/plugins.ts
+++ core/api/src/actions/plugins.ts
@@ -1,6 +1,16 @@
-import { Action } from "actionhero";
+import { Action, log } from "actionhero";
 import PluginDetails from "./../utils/pluginDetails";
+import pacote from "pacote";
+
 const pluginManifest = PluginDetails.getPluginManifest();
+const coreVersion = PluginDetails.getCoreVersion();
+
+interface Plugin {
+  name: string;
+  version: string;
+  license: string;
+  url: string;
+}
 
 export class PluginsList extends Action {
   constructor() {
@@ -11,16 +21,50 @@ export class PluginsList extends Action {
     this.outputExample = {};
   }
 
+  async getLatestNPMVersion(plugin: Plugin, tag = "latest") {
+    // there's a lot more that is returned, like dependencies, dist, shas, etc.. but we don't need it
+    const manifest: { name: string; version: string } = await pacote.manifest(
+      `${plugin.name}@${tag}`
+    );
+    return manifest;
+  }
+
   async run() {
-    return {
-      plugins: pluginManifest.plugins.sort((a, b) => {
-        if (a.name > b.name) {
-          return 1;
+    const plugins: Array<Plugin> = [
+      {
+        name: "@grouparoo/core",
+        version: coreVersion,
+        license: "MPL-2.0",
+        url: "https://www.grouparoo.com",
+      },
+    ].concat(pluginManifest.plugins);
+
+    const pluginResponse: {
+      name: string;
+      version: string;
+      license: string;
+      url: string;
+      latestVersion: string;
+    }[] = await Promise.all(
+      plugins.map(async (plugin) => {
+        let latestVersion = "unknown";
+        try {
+          const manifest = await this.getLatestNPMVersion(plugin);
+          latestVersion = manifest.version;
+        } catch (error) {
+          log(error, "error");
         }
-        if (a.name < b.name) {
-          return -1;
-        }
-      }),
-    };
+
+        return {
+          name: plugin.name,
+          version: plugin.version,
+          license: plugin.license,
+          url: plugin.url,
+          latestVersion,
+        };
+      })
+    );
+
+    return { plugins: pluginResponse };
   }
 }
