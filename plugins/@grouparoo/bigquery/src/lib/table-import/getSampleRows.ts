4e16dc5ade6bd64d22bcac31ec38de424f2f8269
Brian Leonard <brian@bleonard.com>
2020-10-10 19:04:00 -0700

    Remove old code

--- plugins/@grouparoo/bigquery/src/lib/table-import/getSampleRows.ts
+++ plugins/@grouparoo/bigquery/src/lib/table-import/getSampleRows.ts
@@ -20,33 +20,3 @@ export const getSampleRows: GetSampleRowsMethod = async ({
 
   return out;
 };
-
-// export async function getSampleRows(
-//   connection,
-//   tableName,
-//   columns?
-// ): Promise<Array<{ [colName: string]: any }>> {
-//   const escapedTableName = tableName;
-//   const query = `SELECT * FROM \`${escapedTableName}\` LIMIT 10`;
-//   validateQuery(query);
-
-//   const options = { query };
-//   const [rows] = await connection.query(options);
-
-//   const response = [];
-//   if (rows.length > 0) {
-//     rows.map((row) => response.push(castRow(row)));
-//   } else {
-//     // use columns for preview
-//     if (!columns) {
-//       columns = await getColumns(connection, tableName);
-//       const sample = {};
-//       Object.keys(columns).forEach((colName) => {
-//         sample[colName] = castValue(null);
-//       });
-//       response.push(sample);
-//     }
-//   }
-
-//   return response;
-// }
--- plugins/@grouparoo/bigquery/src/lib/table-import/getTables.ts
+++ plugins/@grouparoo/bigquery/src/lib/table-import/getTables.ts
@@ -13,17 +13,3 @@ export const getTables: GetTablesMethod = async ({ connection }) => {
 
   return map;
 };
-
-// export const sourceOptions: SourceOptionsMethod = async ({ connection }) => {
-//   const response: SourceOptionsMethodResponse = {
-//     table: { type: "list", options: [] },
-//   };
-
-//   const [tables] = await connection.getTables();
-//   for (const i in tables) {
-//     const tableName = tables[i].id;
-//     response.table.options.push(tableName);
-//   }
-
-//   return response;
-// };
