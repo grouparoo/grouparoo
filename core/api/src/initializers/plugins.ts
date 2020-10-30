561cbc9b3578593a1690d655f09c6b44782d4679
Evan Tahler <evantahler@gmail.com>
2020-10-29 17:12:50 -0700

    valitate plugins

--- core/api/src/initializers/plugins.ts
+++ core/api/src/initializers/plugins.ts
@@ -158,6 +158,15 @@ export class Plugins extends Initializer {
           );
         }
         if (
+          connection.direction === "import" &&
+          !connection.methods.profileProperty &&
+          !connection.methods.profileProperties
+        ) {
+          errors.push(
+            `import connections must provide either connection.methods.profileProperty or connection.methods.profileProperties`
+          );
+        }
+        if (
           connection.direction === "export" &&
           !connection.methods.exportProfile &&
           !connection.methods.exportProfiles
