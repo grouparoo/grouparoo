5578069637493a6c506602d414c1f21904633855
Evan Tahler <evantahler@gmail.com>
2020-11-05 11:46:42 -0800

    update import tests

--- plugins/@grouparoo/spec-helper/src/lib/workflows/import.ts
+++ plugins/@grouparoo/spec-helper/src/lib/workflows/import.ts
@@ -1,30 +1,45 @@
+import { Profile } from "@grouparoo/core/api/src";
 import { specHelper } from "actionhero";
 
 export async function ImportWorkflow() {
+  const maxAttempts = 5;
+  let attempts = 0;
   let importTasks = [];
-  await specHelper.runTask("profileProperties:enqueue", {});
+  let pendingProfiles = 1;
 
-  // batch
-  importTasks = await specHelper.findEnqueuedTasks(
-    "profileProperty:importProfileProperties"
-  );
-  await Promise.all(
-    importTasks.map((t) =>
-      specHelper.runTask("profileProperty:importProfileProperties", t.args[0])
-    )
-  );
+  async function _import() {
+    await specHelper.runTask("profileProperties:enqueue", {});
 
-  // single
-  importTasks = await specHelper.findEnqueuedTasks(
-    "profileProperty:importProfileProperty"
-  );
-  await Promise.all(
-    importTasks.map((t) =>
-      specHelper.runTask("profileProperty:importProfileProperty", t.args[0])
-    )
-  );
+    // batch
+    importTasks = await specHelper.findEnqueuedTasks(
+      "profileProperty:importProfileProperties"
+    );
+    await Promise.all(
+      importTasks.map((t) =>
+        specHelper.runTask("profileProperty:importProfileProperties", t.args[0])
+      )
+    );
 
-  await specHelper.runTask("profiles:checkReady", {});
+    // single
+    importTasks = await specHelper.findEnqueuedTasks(
+      "profileProperty:importProfileProperty"
+    );
+    await Promise.all(
+      importTasks.map((t) =>
+        specHelper.runTask("profileProperty:importProfileProperty", t.args[0])
+      )
+    );
+
+    await specHelper.runTask("profiles:checkReady", {});
+
+    pendingProfiles = await Profile.count({ where: { state: "pending" } });
+  }
+
+  // we'll need to loop more than once to get dependent profiles first before secondary ones
+  while (pendingProfiles > 0 && attempts < maxAttempts) {
+    await _import();
+    attempts++;
+  }
 
   const completeTasks = await specHelper.findEnqueuedTasks(
     "profile:completeImport"
