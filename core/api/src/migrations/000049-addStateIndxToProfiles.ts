e9d6888a9ba874837b1da1a93f97ba114cb91e80
Evan Tahler <evantahler@gmail.com>
2020-11-05 12:23:31 -0800

    add index to profile#state and profileProperty#state

---
+++ core/api/src/migrations/000049-addStateIndxToProfiles.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  up: async function (migration) {
+    await migration.addIndex("profiles", ["state"], {
+      fields: ["state"],
+    });
+
+    await migration.addIndex("profileProperties", ["state"], {
+      fields: ["state"],
+    });
+  },
+
+  down: async function (migration) {
+    await migration.removeIndex("profiles", ["state"], {
+      fields: ["state"],
+    });
+
+    await migration.removeIndex("profileProperties", ["state"], {
+      fields: ["state"],
+    });
+  },
+};
