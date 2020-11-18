bae7b925f81545c67cf874e388b4132022defa66
Evan Tahler <evantahler@gmail.com>
2020-11-18 09:52:04 -0800

    Use Setting to configure Event assocation batch size

--- core/api/src/tasks/event/associateProfiles.ts
+++ core/api/src/tasks/event/associateProfiles.ts
@@ -1,4 +1,5 @@
 import { Task, task, log } from "actionhero";
+import { plugin } from "../../modules/plugin";
 import { Event } from "../../models/Event";
 
 export class EventsAssociateProfiles extends Task {
@@ -7,36 +8,31 @@ export class EventsAssociateProfiles extends Task {
     this.name = "event:associateProfiles";
     this.description =
       "ensure that events are associated to profiles, even if they were created outside of the Grouparoo API";
-    this.frequency = 60 * 1000;
+    this.frequency = 30 * 1000;
     this.queue = "events";
     this.inputs = {};
   }
 
   async run() {
-    const limit = 1000;
-    let offset = 0;
-    let events: Event[] = [];
+    const limit = parseInt(
+      (await plugin.readSetting("core", "runs-profile-batch-size")).value
+    );
 
-    while (events.length > 0 || offset === 0) {
-      events = await Event.findAll({
-        attributes: ["guid"],
-        where: { profileGuid: null },
-        limit,
-        offset,
-        order: [["createdAt", "asc"]],
+    const events = await Event.findAll({
+      attributes: ["guid"],
+      where: { profileGuid: null },
+      limit,
+      order: [["createdAt", "asc"]],
+    });
+
+    for (const i in events) {
+      await task.enqueue("event:associateProfile", {
+        eventGuid: events[i].guid,
       });
+    }
 
-      for (const i in events) {
-        await task.enqueue("event:associateProfile", {
-          eventGuid: events[i].guid,
-        });
-      }
-
-      if (events.length > 0) {
-        log(`enqueued ${events.length} events for association`);
-      }
-
-      offset = offset + limit;
+    if (events.length > 0) {
+      log(`enqueued ${events.length} events for association`);
     }
   }
 }
