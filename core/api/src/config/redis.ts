17b6cef3d7e3b2387a17d3d19f081a97fd7c5261
Evan Tahler <evantahler@gmail.com>
2020-11-17 17:45:45 -0800

    install ioredis-mock and update config

--- core/api/src/config/redis.ts
+++ core/api/src/config/redis.ts
@@ -1,66 +1,90 @@
 import { URL } from "url";
 
-let host = process.env.REDIS_HOST || "127.0.0.1";
-let port = process.env.REDIS_PORT || 6379;
-let db = process.env.REDIS_DB || process.env.JEST_WORKER_ID || "0";
-let password = process.env.REDIS_PASSWORD || null;
-const maxBackoff = 1000;
+export const DEFAULT = {
+  redis: () =>
+    !process.env.REDIS_URL || process.env.REDIS_URL.length === 0
+      ? MockRedisConfig()
+      : RealRedisConfig(),
+};
 
-if (process.env.REDIS_URL) {
-  const parsed = new URL(process.env.REDIS_URL);
-  if (parsed.password) {
-    password = parsed.password;
+function RealRedisConfig() {
+  const konstructor = require("ioredis");
+
+  let host = process.env.REDIS_HOST || "127.0.0.1";
+  let port = process.env.REDIS_PORT || 6379;
+  let db = process.env.REDIS_DB || process.env.JEST_WORKER_ID || "0";
+  let password = process.env.REDIS_PASSWORD || null;
+
+  if (process.env.REDIS_URL) {
+    const parsed = new URL(process.env.REDIS_URL);
+    if (parsed.password) password = parsed.password;
+    if (parsed.hostname) host = parsed.hostname;
+    if (parsed.port) port = parsed.port;
+    if (parsed.pathname) db = parsed.pathname.substring(1);
   }
-  if (parsed.hostname) {
-    host = parsed.hostname;
-  }
-  if (parsed.port) {
-    port = parsed.port;
-  }
-  if (parsed.pathname) {
-    db = parsed.pathname.substring(1);
-  }
+
+  const maxBackoff = 1000;
+  const commonArgs = {
+    port,
+    host,
+    password,
+    db: parseInt(db),
+    // you can learn more about retryStrategy @ https://github.com/luin/ioredis#auto-reconnect
+    retryStrategy: (times) => {
+      if (times === 1) {
+        console.error(
+          "Unable to connect to Redis - please check your Redis config!"
+        );
+        return 5000;
+      }
+      return Math.min(times * 50, maxBackoff);
+    },
+  };
+
+  return {
+    scanCount: 1000,
+
+    _toExpand: false,
+    client: {
+      konstructor,
+      args: [commonArgs],
+      buildNew: true,
+    },
+    subscriber: {
+      konstructor,
+      args: [commonArgs],
+      buildNew: true,
+    },
+    tasks: {
+      konstructor,
+      args: [commonArgs],
+      buildNew: true,
+    },
+  };
 }
 
-export const DEFAULT = {
-  redis: (config) => {
-    // konstructor: The redis client constructor method.  All redis methods must be promises
-    // args: The arguments to pass to the constructor
-    // buildNew: is it `new konstructor()` or just `konstructor()`?
+function MockRedisConfig() {
+  const MockIORedis = require("ioredis-mock");
+  const baseRedis = new MockIORedis();
 
-    const commonArgs = {
-      port,
-      host,
-      password,
-      db: parseInt(db),
-      // you can learn more about retryStrategy @ https://github.com/luin/ioredis#auto-reconnect
-      retryStrategy: (times) => {
-        if (times === 1) {
-          return 5000;
-        }
-        return Math.min(times * 50, maxBackoff);
-      },
-    };
+  return {
+    scanCount: 1000,
 
-    return {
-      enabled: true,
-
-      _toExpand: false,
-      client: {
-        konstructor: require("ioredis"),
-        args: [commonArgs],
-        buildNew: true,
-      },
-      subscriber: {
-        konstructor: require("ioredis"),
-        args: [commonArgs],
-        buildNew: true,
-      },
-      tasks: {
-        konstructor: require("ioredis"),
-        args: [commonArgs],
-        buildNew: true,
-      },
-    };
-  },
-};
+    _toExpand: false,
+    client: {
+      konstructor: () => baseRedis,
+      args: [],
+      buildNew: false,
+    },
+    subscriber: {
+      konstructor: () => baseRedis.createConnectedClient(),
+      args: [],
+      buildNew: false,
+    },
+    tasks: {
+      konstructor: () => baseRedis.createConnectedClient(),
+      args: [],
+      buildNew: false,
+    },
+  };
+}
