f8a50999992ec0b31a7a1065e95b884919435590
Evan Tahler <evantahler@gmail.com>
2020-12-01 10:46:04 -0800

    only remove compount indexes for SQLite

--- core/api/src/migrations/000053-removeCompoundindex.ts
+++ core/api/src/migrations/000053-removeCompoundindex.ts
@@ -1,65 +1,73 @@
+import { config } from "actionhero";
+
 module.exports = {
   up: async function (migration, DataTypes) {
-    await migration.removeIndex("groupMembers", ["profileGuid", "groupGuid"], {
-      unique: true,
-      fields: ["profileGuid", "groupGuid"],
-    });
+    if (config.sequelize.dialect === "sqlite") {
+      await migration.removeIndex(
+        "groupMembers",
+        ["profileGuid", "groupGuid"],
+        {
+          unique: true,
+          fields: ["profileGuid", "groupGuid"],
+        }
+      );
 
-    await migration.removeIndex(
-      "mappings",
-      ["ownerGuid", "profilePropertyRuleGuid"],
-      {
+      await migration.removeIndex(
+        "mappings",
+        ["ownerGuid", "profilePropertyRuleGuid"],
+        {
+          unique: true,
+          fields: ["ownerGuid", "profilePropertyRuleGuid"],
+        }
+      );
+
+      await migration.removeIndex("mappings", ["ownerGuid", "remoteKey"], {
         unique: true,
-        fields: ["ownerGuid", "profilePropertyRuleGuid"],
-      }
-    );
+        fields: ["ownerGuid", "remoteKey"],
+      });
 
-    await migration.removeIndex("mappings", ["ownerGuid", "remoteKey"], {
-      unique: true,
-      fields: ["ownerGuid", "remoteKey"],
-    });
-
-    await migration.removeIndex("options", ["ownerGuid", "key"], {
-      unique: true,
-      fields: ["ownerGuid", "key"],
-    });
-
-    await migration.removeIndex(
-      "profileProperties",
-      ["profileGuid", "profilePropertyRuleGuid", "position"],
-      {
+      await migration.removeIndex("options", ["ownerGuid", "key"], {
         unique: true,
-        fields: ["profileGuid", "profilePropertyRuleGuid", "position"],
-      }
-    );
+        fields: ["ownerGuid", "key"],
+      });
 
-    await migration.removeIndex("settings", ["pluginName", "key"], {
-      unique: true,
-      fields: ["pluginName", "key"],
-    });
+      await migration.removeIndex(
+        "profileProperties",
+        ["profileGuid", "profilePropertyRuleGuid", "position"],
+        {
+          unique: true,
+          fields: ["profileGuid", "profilePropertyRuleGuid", "position"],
+        }
+      );
 
-    await migration.removeIndex(
-      "destinationGroupMemberships",
-      ["destinationGuid", "groupGuid"],
-      {
+      await migration.removeIndex("settings", ["pluginName", "key"], {
         unique: true,
-        fields: ["destinationGuid", "groupGuid"],
-      }
-    );
+        fields: ["pluginName", "key"],
+      });
 
-    await migration.removeIndex(
-      "destinationGroupMemberships",
-      ["destinationGuid", "remoteKey"],
-      {
+      await migration.removeIndex(
+        "destinationGroupMemberships",
+        ["destinationGuid", "groupGuid"],
+        {
+          unique: true,
+          fields: ["destinationGuid", "groupGuid"],
+        }
+      );
+
+      await migration.removeIndex(
+        "destinationGroupMemberships",
+        ["destinationGuid", "remoteKey"],
+        {
+          unique: true,
+          fields: ["destinationGuid", "remoteKey"],
+        }
+      );
+
+      await migration.removeIndex("permissions", ["ownerGuid", "topic"], {
         unique: true,
-        fields: ["destinationGuid", "remoteKey"],
-      }
-    );
-
-    await migration.removeIndex("permissions", ["ownerGuid", "topic"], {
-      unique: true,
-      fields: ["ownerGuid", "topic"],
-    });
+        fields: ["ownerGuid", "topic"],
+      });
+    }
   },
 
   down: async function () {
