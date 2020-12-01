6a2b7ad997e7509bb2d49e3689148968de1871f4
Brian Leonard <brian@bleonard.com>
2020-12-01 09:51:35 -0800

    Use shared batch code in marketo

--- plugins/@grouparoo/marketo/src/lib/export/exportProfiles.ts
+++ plugins/@grouparoo/marketo/src/lib/export/exportProfiles.ts
@@ -1,243 +1,103 @@
 import {
-  ExportProfilesPluginMethod,
-  ErrorWithProfileGuid,
-  ExportedProfile,
-  SimpleAppOptions,
-} from "@grouparoo/core";
+  buildBatchExports,
+  BatchExport,
+  exportProfilesInBatch,
+  BatchGroupMode,
+  BatchMethodGetClient,
+  BatchMethodFindAndSetDestinationIds,
+  BatchMethodDeleteByDestinationIds,
+  BatchMethodUpdateByDestinationIds,
+  BatchMethodCreateByForeignKeyAndSetDestinationIds,
+  BatchMethodAddToGroups,
+  GroupNameListMap,
+  DestinationIdMap,
+  BatchMethodNormalizeForeignKeyValue,
+  BatchMethodNormalizeGroupName,
+  BatchMethodRemoveFromGroups,
+} from "@grouparoo/app-templates/src/destination/batch";
+import { ExportProfilesPluginMethod, SimpleAppOptions } from "@grouparoo/core";
 import { connect } from "../connect";
 import { getListId, MarketoCacheData } from "./listMethods";
 
-enum MarketoAction {
-  Delete = "DELETE",
-  EmailChange = "EMAILCHANGE",
-  Update = "UPDATE",
-}
-export interface MarketoExport extends ExportedProfile {
-  marketoId?: number;
-  action?: MarketoAction;
-  result?: any; // result from find
-  processed?: boolean;
-  error?: any;
-}
-export interface ExportBatchMethod {
-  (argument: {
-    // connection: any;
-    appGuid: string;
-    appOptions: SimpleAppOptions;
-    exports: MarketoExport[];
-  }): Promise<{
-    success: boolean;
-    retryDelay?: number;
-    errors?: ErrorWithProfileGuid[];
-  }>;
-}
-declare type MarketoEmailMap = { [email: string]: MarketoExport };
-
-export async function exportBatch({ appGuid, appOptions, exports }) {
-  if (exports.length === 0) {
-    return { success: true };
-  }
-
-  const client = await connect(appOptions);
-  const cacheData: MarketoCacheData = { appGuid, appOptions };
-
-  const emailMap: MarketoEmailMap = {};
-  for (const exportedProfile of exports) {
-    try {
-      sortExport(exportedProfile, emailMap);
-    } catch (error) {
-      // if just one of them is missing email or something, move on
-      exportedProfile.error = error;
-    }
-  }
-
-  await setMarketoIds(client, emailMap);
-
-  await deleteLeads(client, exports);
-  await updateByIds(client, exports);
-  await updateByEmail(client, exports);
-
-  // so now, all the exports that don't have an error and where not deleted should have a marketoId
-  // use those ids to update the groups
-  await updateGroups(client, cacheData, exports);
-
-  // assuming semantics here of success is only true if there are zero errors
-  let errors: ErrorWithProfileGuid[] = null; // for ones that go wrong
-  let success = true;
-  for (const exportedProfile of exports) {
-    let { error } = exportedProfile;
-    if (error) {
-      success = false;
-      errors = errors || [];
-      if (typeof error === "string") {
-        error = new Error(error);
-      }
-      error.profileGuid = exportedProfile.profileGuid;
-      errors.push(error);
-    }
-  }
-
-  return { success, errors };
+export interface MarketoData {
+  cacheData: MarketoCacheData;
 }
 
-async function updateGroups(
+// return an object that you can connect with
+const getClient: BatchMethodGetClient = async ({ config }) => {
+  return connect(config.appOptions);
+};
+
+// fetch using the keys to set destinationId and result on BatchExports
+// use the getByForeignKey to lookup results
+const findAndSetDestinationIds: BatchMethodFindAndSetDestinationIds = async ({
   client,
-  cacheData: MarketoCacheData,
-  exports: MarketoExport[]
-) {
-  const removal: { [groupName: string]: MarketoExport[] } = {};
-  const addition: { [groupName: string]: MarketoExport[] } = {};
-  for (const exportedProfile of exports) {
-    if (exportedProfile.error) {
-      continue;
-    }
-    if (exportedProfile.action === MarketoAction.Delete) {
-      continue;
-    }
+  foreignKeys,
+  getByForeignKey,
+}) => {
+  // search for these email addresses
+  const filterValues = foreignKeys;
+  const filterType = "email";
 
-    const { marketoId } = exportedProfile;
-    if (!marketoId) {
-      try {
-        throw new Error(
-          `profile does not have a marketo id: ${exportedProfile?.newProfileProperties?.email}`
-        );
-      } catch (error) {
-        exportedProfile.error = error;
-      }
-    }
-
-    // build up groups situation of group names to addition and removal
-    const { oldGroups, newGroups } = exportedProfile;
-    const removedLists = oldGroups.filter((k) => !newGroups.includes(k));
-    const addedLists = newGroups;
-
-    for (const list of removedLists) {
-      removal[list] = removal[list] || [];
-      removal[list].push(exportedProfile);
-    }
-    for (const list of addedLists) {
-      addition[list] = addition[list] || [];
-      addition[list].push(exportedProfile);
-    }
-  }
-
-  for (const listName in addition) {
-    await updateList(
-      client,
-      ListAction.Add,
-      cacheData,
-      listName,
-      addition[listName]
-    );
-  }
-  for (const listName in removal) {
-    await updateList(
-      client,
-      ListAction.Remove,
-      cacheData,
-      listName,
-      removal[listName]
-    );
-  }
-}
-
-enum ListAction {
-  Add = "ADD",
-  Remove = "REMOVE",
-}
-async function updateList(
-  client,
-  action: ListAction,
-  cacheData: MarketoCacheData,
-  listName: string,
-  users: MarketoExport[]
-) {
-  if (users.length === 0) {
-    return;
-  }
-  const id = await getListId(client, cacheData, listName);
-  const idMap: { [marketoId: number]: MarketoExport } = {};
-  const marketoIds: any[] = [];
-  for (const user of users) {
-    idMap[user.marketoId] = user;
-    //marketoIds.push({ id: user.marketoId });
-    marketoIds.push(user.marketoId);
-  }
-  let response;
-  if (action === ListAction.Add) {
-    response = await client.list.addLeadsToList(id, marketoIds);
-  } else if (action === ListAction.Remove) {
-    // DELETE /rest/v1/lists/{listId}/leads.json?id=318603&id=318595&id=999999
-    const query = marketoIds.join("&id=");
-    const path = `/v1/lists/${id}/leads.json?id=${query}`;
-
-    //client.list.removeLeadsToList has issue
-    response = await client._connection.del(path, {
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+  // TODO: there might be size limit for this GET.
+  // The library commented out // _method: 'GET' and post approach
+  // That would have worked around that. I wonder why.
+  const response = await client.lead.find(filterType, filterValues);
   if (!response.success) {
-    throw new Error(`Marketo list error: ${listName}`);
+    throw new Error(`Marketo email find error`);
   }
-  const results = response.result || [];
-  if (results.length === 0) {
-    throw new Error(`Marketo empty results: ${listName}`);
-  }
+  const results = response.result;
   for (const result of results) {
-    const { id } = result;
-    if (result.reasons) {
-      if (action === ListAction.Remove && result.status === "skipped") {
-        // this is ok, not in the  list
-      } else {
-        const message = result.reasons.map((r) => r.message).join(", ");
-        const user = idMap[id];
-        if (!user) {
-          throw new Error(`Unknown user id in list: ${id}`);
-        }
-        user.error =
-          user.error || new Error(`could update list ${listName}: ${message}`);
-      }
+    const found = getByForeignKey(result.email);
+    if (found) {
+      found.destinationId = result.id;
+      found.result = result;
+    } else {
+      // Marketo result found but didn't have email. not sure what that means
     }
   }
-}
+};
 
-async function updateByIds(client, exports: MarketoExport[]) {
-  const users: MarketoExport[] = [];
-  for (const exportedProfile of exports) {
-    if (exportedProfile.processed || exportedProfile.error) {
-      continue;
-    }
-    if (!exportedProfile.marketoId) {
-      continue;
-    }
-    users.push(exportedProfile);
+// delete the given destinationIds
+const deleteByDestinationIds: BatchMethodDeleteByDestinationIds = async ({
+  client,
+  users,
+}) => {
+  const marketoIds = users.map((user) => {
+    return {
+      id: user.destinationId,
+    };
+  });
+  const path = "/v1/leads.json?_method=DELETE";
+  const data = { input: marketoIds };
+  const response = await client.lead._connection.postJson(path, data);
+  if (!response.success) {
+    throw new Error(`Marketo delete error`);
   }
+};
 
+// update these users by destinationId
+const updateByDestinationIds: BatchMethodUpdateByDestinationIds = async ({
+  client,
+  users,
+}) => {
   const options = { lookupField: "id", action: "updateOnly" };
   await updateUsers(client, users, options);
-}
+};
 
-async function updateByEmail(client, exports: MarketoExport[]) {
-  const users: MarketoExport[] = [];
-  for (const exportedProfile of exports) {
-    if (exportedProfile.processed || exportedProfile.error) {
-      continue;
-    }
-    users.push(exportedProfile);
-  }
-
+// usually this is creating them. ideally upsert. set the destinationId on each when done
+const createByForeignKeyAndSetDestinationIds: BatchMethodCreateByForeignKeyAndSetDestinationIds = async ({
+  client,
+  users,
+}) => {
   const options = { lookupField: "email", action: "createOrUpdate" };
   await updateUsers(client, users, options);
-}
+};
 
-async function updateUsers(client, users: MarketoExport[], options) {
+async function updateUsers(client, users: BatchExport[], options) {
   const input: any[] = [];
   for (const exportedProfile of users) {
-    exportedProfile.processed = true;
     input.push(buildPayload(exportedProfile));
-  }
-  if (input.length === 0) {
-    return;
   }
   const response = await client.lead.createOrUpdate(input, options);
   if (!response.success) {
@@ -260,30 +120,31 @@ async function updateUsers(client, users: MarketoExport[], options) {
           `Marketo profile error: ${user.profileGuid} ${JSON.stringify(result)}`
         );
       }
-      if (user.marketoId && user.marketoId !== result.id) {
+      if (user.destinationId && user.destinationId !== result.id) {
         throw new Error(
-          `user id does not match updated one: ${user.marketoId} -> ${result.id}`
+          `user id does not match updated one: ${user.destinationId} -> ${result.id}`
         );
       }
     } catch (error) {
       user.error = error;
     }
 
-    user.marketoId = result.id;
+    user.destinationId = result.id;
   }
 }
 
-function buildPayload(exportedProfile: MarketoExport): any {
+function buildPayload(exportedProfile: BatchExport): any {
   const user: any = {};
   const {
-    marketoId,
+    destinationId,
     oldProfileProperties,
     newProfileProperties,
+    foreignKeyValue,
   } = exportedProfile;
 
-  user.email = newProfileProperties.email.toLowerCase().trim();
-  if (marketoId) {
-    user.id = marketoId;
+  user.email = foreignKeyValue; // email
+  if (destinationId) {
+    user.id = destinationId;
   }
 
   // set profile properties, including old ones
@@ -312,109 +173,137 @@ function formatVar(value) {
   }
 }
 
-async function deleteLeads(client, exports: MarketoExport[]) {
-  const marketoIds: any[] = [];
-  for (const exportedProfile of exports) {
-    if (exportedProfile.processed || exportedProfile.error) {
-      continue;
-    }
-    if (exportedProfile.action !== MarketoAction.Delete) {
-      continue;
-    }
-    exportedProfile.processed = true;
+// make sure these user are in these groups (keys of map are group names)
+const addToGroups: BatchMethodAddToGroups = async ({
+  client,
+  groupMap,
+  destIdMap,
+  config,
+}) => {
+  const { cacheData } = config.data;
+  return updateList(client, ListAction.Add, groupMap, destIdMap, cacheData);
+};
 
-    const id = exportedProfile.marketoId;
-    if (id) {
-      marketoIds.push({ id });
-    } else {
-      // they aren't there anyway. let it go.
-    }
-  }
+const removeFromGroups: BatchMethodRemoveFromGroups = async ({
+  client,
+  groupMap,
+  destIdMap,
+  config,
+}) => {
+  const { cacheData } = config.data;
+  return updateList(client, ListAction.Remove, groupMap, destIdMap, cacheData);
+};
 
-  if (marketoIds.length === 0) {
-    return;
-  }
-  const path = "/v1/leads.json?_method=DELETE";
-  const data = { input: marketoIds };
-  const response = await client.lead._connection.postJson(path, data);
-  if (!response.success) {
-    throw new Error(`Marketo delete error`);
+enum ListAction {
+  Add = "ADD",
+  Remove = "REMOVE",
+}
+async function updateList(
+  client,
+  action: ListAction,
+  groupMap: GroupNameListMap,
+  destIdMap: DestinationIdMap,
+  cacheData: MarketoCacheData
+) {
+  for (const listName in groupMap) {
+    const id = await getListId(client, cacheData, listName);
+    const users = groupMap[listName];
+    const marketoIds: string[] = users.map((user) => user.destinationId);
+
+    let response;
+    if (action === ListAction.Add) {
+      response = await client.list.addLeadsToList(id, marketoIds);
+    } else if (action === ListAction.Remove) {
+      // DELETE /rest/v1/lists/{listId}/leads.json?id=318603&id=318595&id=999999
+      const query = marketoIds.join("&id=");
+      const path = `/v1/lists/${id}/leads.json?id=${query}`;
+
+      //client.list.removeLeadsToList has issue
+      response = await client._connection.del(path, {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    if (!response.success) {
+      throw new Error(`Marketo list error: ${listName}`);
+    }
+    const results = response.result || [];
+    if (results.length === 0) {
+      throw new Error(`Marketo empty results: ${listName}`);
+    }
+    for (const result of results) {
+      const { id } = result;
+      if (result.reasons) {
+        if (action === ListAction.Remove && result.status === "skipped") {
+          // this is ok, not in the  list
+        } else {
+          const message = result.reasons.map((r) => r.message).join(", ");
+          const user = destIdMap[id];
+          if (!user) {
+            throw new Error(`Unknown user id in list: ${id}`);
+          }
+          user.error =
+            user.error ||
+            new Error(`could update list ${listName}: ${message}`);
+        }
+      }
+    }
   }
 }
 
-async function setMarketoIds(client, emailMap: MarketoEmailMap) {
-  // search for these email addresses
-  const filterValues = Object.keys(emailMap);
-  const filterType = "email";
-
-  // TODO: there might be size limit for this GET.
-  // The library commented out // _method: 'GET' and post approach
-  // That would have worked around that. I wonder why.
-  const response = await client.lead.find(filterType, filterValues);
-  if (!response.success) {
-    throw new Error(`Marketo email find error`);
+// mess with the keys (lowercase emails, for example)
+const normalizeForeignKeyValue: BatchMethodNormalizeForeignKeyValue = ({
+  keyValue,
+}) => {
+  if (!keyValue) {
+    return null;
   }
-  const results = response.result;
-  for (const result of results) {
-    const email = result.email.toLowerCase().trim();
-    const found = emailMap[email];
-    if (found) {
-      found.marketoId = result.id;
-      found.result = result;
-    } else {
-      // Marketo result found but didn't have email. not sure what that means
+  return keyValue.toString().toLowerCase().trim();
+};
+// mess with the names of groups (tags with no spaces, for example)
+const normalizeGroupName: BatchMethodNormalizeGroupName = ({ groupName }) => {
+  return groupName.toString().trim();
+};
+
+export async function exportBatch({ appGuid, appOptions, exports }) {
+  const cacheData = { appGuid, appOptions };
+  const batchSize = 300;
+  const findSize = 300;
+  const data: MarketoData = { cacheData };
+
+  return exportProfilesInBatch(
+    exports,
+    {
+      findSize,
+      batchSize,
+      groupMode: BatchGroupMode.WithinGroup,
+      appOptions,
+      data,
+      foreignKey: "email",
+    },
+    {
+      getClient,
+      findAndSetDestinationIds,
+      deleteByDestinationIds,
+      updateByDestinationIds,
+      createByForeignKeyAndSetDestinationIds,
+      addToGroups,
+      removeFromGroups,
+      normalizeForeignKeyValue,
+      normalizeGroupName,
     }
-  }
-}
-
-// returns what to do for each case
-function sortExport(exportedProfile: MarketoExport, emailMap: MarketoEmailMap) {
-  const {
-    oldProfileProperties,
-    newProfileProperties,
-    toDelete,
-  } = exportedProfile;
-
-  const email = newProfileProperties.email?.toLowerCase()?.trim();
-  const oldEmail = oldProfileProperties.email?.toLowerCase()?.trim();
-  if (!email) {
-    throw new Error(`newProfileProperties[email] is a required mapping`);
-  }
-
-  emailMap[email] = exportedProfile;
-
-  // record other one if applicable
-  if (oldEmail && oldEmail !== email) {
-    emailMap[oldEmail] = exportedProfile;
-    exportedProfile.action = MarketoAction.EmailChange;
-  }
-
-  if (toDelete) {
-    exportedProfile.action = MarketoAction.Delete;
-  }
-
-  if (!exportedProfile.action) {
-    exportedProfile.action = MarketoAction.Update;
-  }
-
-  return exportedProfile;
+  );
 }
 
 export const exportProfiles: ExportProfilesPluginMethod = async ({
-  appOptions,
-  exports,
   appGuid,
+  appOptions,
+  destinationOptions,
+  exports: profilesToExport,
 }) => {
-  // TODO: marketo can do batches of 300 at a time, it seems.
-  // developers.marketo.com/rest-api/marketo-integration-best-practices
-  // do we need to switch this even one more level up?
-  // i'm making a separate function to enable that loop
-  // the separate function also enabled testing (doesn't need profile model)
-
-  const batchExports: MarketoExport[] = [];
-  for (const exportedProfile of exports) {
-    const marketo: MarketoExport = Object.assign({}, exportedProfile);
-    batchExports.push(marketo);
-  }
-  return exportBatch({ appGuid, appOptions, exports: batchExports });
+  const batchExports = buildBatchExports(profilesToExport);
+  return exportBatch({
+    appGuid,
+    appOptions,
+    exports: batchExports,
+  });
 };
