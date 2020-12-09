0cb72299bbd1c9ab0fba84168aa696c725162f5c
Evan Tahler <evantahler@gmail.com>
2020-12-08 18:58:25 -0800

    move init log messages to initializer

--- core/src/config/environment.ts
+++ core/src/config/environment.ts
@@ -6,10 +6,12 @@ import { getParentPath } from "../utils/pluginDetails";
 // this needs to be done before requiring any part of actionhero
 const env = process.env.NODE_ENV || "development";
 
-if (env === "development") {
-  const envFile = path.resolve(path.join(getParentPath(), ".env"));
+if (env === "development" || process.env.GROUPAROO_ENV_CONFIG_FILE) {
+  const envFile =
+    process.env.GROUPAROO_ENV_CONFIG_FILE ||
+    path.resolve(path.join(getParentPath(), ".env"));
   if (fs.existsSync(envFile)) {
     require("dotenv").config({ path: envFile });
-    console.log(`modified your runtime environment with ${envFile}`);
+    process.env.GROUPAROO_ENV_CONFIG_FILE = envFile;
   }
 }
