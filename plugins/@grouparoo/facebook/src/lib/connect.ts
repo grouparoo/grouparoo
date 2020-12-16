2ee6b574f26c1878f7c821685d5e5cb696c1b0b9
Brian Leonard <brian@bleonard.com>
2020-12-15 22:15:24 -0800

    Compile fix

--- plugins/@grouparoo/facebook/src/lib/connect.ts
+++ plugins/@grouparoo/facebook/src/lib/connect.ts
@@ -1,6 +1,5 @@
 import { SimpleAppOptions } from "@grouparoo/core";
 const bizSdk = require("facebook-nodejs-business-sdk");
-const { FacebookAdsApi, AdAccount, CustomAudience } = bizSdk;
 
 export async function connect(appOptions: SimpleAppOptions): Promise<Client> {
   return new Client(appOptions);
@@ -10,7 +9,7 @@ export class Client {
   sdk: typeof bizSdk;
   accessToken: string;
   adAccountId: string;
-  _api: typeof FacebookAdsApi;
+  _api: any;
 
   constructor(appOptions: SimpleAppOptions) {
     const { accessToken, adAccountId } = appOptions;
@@ -23,6 +22,7 @@ export class Client {
     if (this._api) {
       return this._api;
     }
+    const { FacebookAdsApi } = this.sdk;
     this._api = FacebookAdsApi.init(this.accessToken);
     // make sure this is not shared
     FacebookAdsApi.setDefaultApi(null);
@@ -36,10 +36,12 @@ export class Client {
   //   api: ?FacebookAdsApi)
 
   adAccount() {
+    const { AdAccount } = this.sdk;
     return new AdAccount(this.adAccountId, {}, null, this.getApi());
   }
 
   audience(id: string) {
+    const { CustomAudience } = this.sdk;
     return new CustomAudience(id, {}, null, this.getApi());
   }
 }
