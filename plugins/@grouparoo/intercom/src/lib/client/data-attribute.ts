104631a5459688df6eee99a08154e27f23d6c3ed
Brian Leonard <brian@bleonard.com>
2021-01-03 16:34:54 -0800

    Not in use

--- plugins/@grouparoo/intercom/src/lib/client/data-attribute.ts
+++ plugins/@grouparoo/intercom/src/lib/client/data-attribute.ts
@@ -4,12 +4,6 @@ export default class DataAttribute {
   constructor(client) {
     this.client = client;
   }
-  create(data, f) {
-    return this.client.post("/data_attributes", data, f);
-  }
-  update(params, f) {
-    return this.client.post("/data_attributes", params, f);
-  }
   list(params, f) {
     return this.client.get("/data_attributes", params, f);
   }
