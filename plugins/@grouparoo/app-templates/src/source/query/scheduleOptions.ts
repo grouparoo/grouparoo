a943edefd2b57678665acde286ec45a8bc8cccbe
Evan Tahler <evantahler@gmail.com>
2021-01-04 09:05:35 -0800

    Add more description to query option

--- plugins/@grouparoo/app-templates/src/source/query/scheduleOptions.ts
+++ plugins/@grouparoo/app-templates/src/source/query/scheduleOptions.ts
@@ -5,7 +5,8 @@ export const getScheduleOptions = () => {
     {
       key: "query",
       required: true,
-      description: "What query should I run?",
+      description:
+        "What query should I run?  Do not include LIMIT or OFFSET, as theses will be controlled automatically according to your settings.",
       type: "textarea",
       options: async () => {
         return [];
