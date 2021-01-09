412763766a86c59c9d8a394fe7dbca5923185f62
Evan Tahler <evantahler@gmail.com>
2021-01-08 16:26:00 -0800

    extract mustache utils

---
+++ core/src/modules/mustacheUtils.ts
@@ -0,0 +1,36 @@
+import Mustache from "mustache";
+
+export namespace MustacheUtils {
+  export interface MustacheArgs {
+    [key: string]: any;
+  }
+
+  export function strictlyRender(
+    string: string,
+    data: MustacheArgs,
+    errorPrefix?: string
+  ) {
+    validateMustacheVariables(string, data, errorPrefix);
+    return render(string, data);
+  }
+
+  export function render(string: string, data: MustacheArgs) {
+    return Mustache.render(string, data);
+  }
+
+  export function validateMustacheVariables(
+    string: string,
+    data: MustacheArgs,
+    errorPrefix = "missing mustache key"
+  ) {
+    Mustache.parse(string)
+      .filter((chunk) => chunk[0] === "name")
+      .map((chunk) => chunk[1])
+      .map((key) => {
+        const value = key.split(".").reduce((o, i) => o[i], data);
+        if (value === undefined || value === null) {
+          throw new Error(`${errorPrefix} "${key}"`);
+        }
+      });
+  }
+}
