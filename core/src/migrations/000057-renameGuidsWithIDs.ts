41176282e0a5b51696d5851e97c0803f8a411a9b
Evan Tahler <evantahler@gmail.com>
2021-02-02 16:03:54 -0800

    migration

---
+++ core/src/migrations/000057-renameGuidsWithIDs.ts
@@ -0,0 +1,56 @@
+const tables = {
+  apiKeys: [],
+  apps: [],
+  destinationGroupMemberships: ["destinationGuid", "groupGuid"],
+  destinations: ["appGuid"],
+  eventData: ["eventGuid"],
+  events: ["producerGuid", "profileGuid"],
+  exports: ["profileGuid", "destinationGuid"],
+  files: [],
+  groupMembers: ["profileGuid", "groupGuid"],
+  groupRules: ["groupGuid", "propertyGuid"],
+  groups: [],
+  imports: ["creatorGuid", "profileGuid", "oldGroupGuids", "newGroupGuids"],
+  logs: ["ownerGuid"],
+  mappings: ["ownerGuid", "propertyGuid"],
+  notifications: [],
+  options: ["ownerGuid"],
+  permissions: ["ownerGuid"],
+  profileProperties: ["profileGuid", "propertyGuid"],
+  profiles: [],
+  properties: ["sourceGuid"],
+  propertyFilters: ["propertyGuid"],
+  runs: ["creatorGuid"],
+  schedules: ["sourceGuid"],
+  settings: [],
+  setupSteps: [],
+  sources: ["appGuid"],
+  teamMembers: ["teamGuid"],
+  teams: [],
+};
+
+export default {
+  up: async function (migration) {
+    for (const table in tables) {
+      await migration.renameColumn(table, "guid", "id");
+      const cols = tables[table];
+      for (const j in cols) {
+        const oldName = cols[j];
+        const newName = oldName.replace("Guid", "Id");
+        await migration.renameColumn(table, oldName, newName);
+      }
+    }
+  },
+
+  down: async function (migration) {
+    for (const table in tables) {
+      await migration.renameColumn(table, "id", "guid");
+      const cols = tables[table];
+      for (const j in cols) {
+        const newName = cols[j];
+        const oldName = newName.replace("Guid", "Id");
+        await migration.renameColumn(table, oldName, newName);
+      }
+    }
+  },
+};
