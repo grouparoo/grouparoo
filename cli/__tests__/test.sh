f5b9337e2511fb3ddf246123b305a7e717b4e13f
Evan Tahler <evantahler@gmail.com>
2021-02-12 16:16:42 -0800

    update tests

--- cli/__tests__/test.sh
+++ cli/__tests__/test.sh
@@ -3,10 +3,13 @@
 set -e
 
 TIME=`date +%s`
+cd "$(dirname "$0")/../../"
+CORE_VERSION=`cat lerna.json | jq '.version'`
+cd "./cli/__tests__"
 WORKDIR="/tmp/grouparoo-$TIME"
 export INIT_CWD=$WORKDIR
 
-echo " >>> testing with WORKDIR=$WORKDIR <<< "
+echo " >>> testing with WORKDIR=$WORKDIR against CORE_VERSION=$CORE_VERSION <<< "
 
 ## to get around premission issues on CI
 ## https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
@@ -14,8 +17,8 @@ mkdir -p ~/.npm-global
 export NPM_CONFIG_PREFIX=~/.npm-global
 export PATH="$HOME/.npm-global/bin:$PATH"
 
-## use /this/ version of the "grouparoo" package with NPX
-npm link
+## use /this/ version of the "grouparoo" package with
+npm link ../
 
 echo ""
 echo "--- npm linked ---"
@@ -23,21 +26,21 @@ echo "--- npm linked ---"
 ## try the version command
 echo ""
 echo "--- test: version ---"
-npx grouparoo --version
+grouparoo --version
 
 ## the help command should be the default
 # echo "--- test: (empty) ---"
-# npx grouparoo (we can't test this because the output is actually stderr)
+# grouparoo (we can't test this because the output is actually stderr)
 
 ## the help command should work too
 echo ""
 echo "--- test: help ---"
-npx grouparoo --help
+grouparoo --help
 
 ## try the init command
 echo ""
 echo "--- test: init ---"
-npx grouparoo init $WORKDIR
+grouparoo init $WORKDIR
 
 if [ -f "$WORKDIR/package.json" ]; then
     echo "✅ package.json exists."
@@ -63,20 +66,30 @@ fi
 ## try the update command
 echo ""
 echo "--- test: update ---"
-cd $WORKDIR && npx grouparoo update
+cd $WORKDIR && grouparoo update
 
 ## try the install command
 echo ""
 echo "--- test: install ---"
-cd $WORKDIR && npx grouparoo install @grouparoo/logger@next
+cd $WORKDIR && grouparoo install @grouparoo/logger
 echo ""
 echo ""
 cd $WORKDIR && cat package.json
 echo ""
 echo ""
 
-## reset the NPM/NPX link
-npm unlink
+## try the uninstall command
+echo ""
+echo "--- test: uninstall ---"
+cd $WORKDIR && grouparoo uninstall @grouparoo/logger
+echo ""
+echo ""
+cd $WORKDIR && cat package.json
+echo ""
+echo ""
+
+## reset the NPM link
+npm unlink ../
 echo "--- npm unlinked ---"
 
 echo ""
