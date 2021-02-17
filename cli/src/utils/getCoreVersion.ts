f309062f3742d759daeaeee99bec2dd4ed796158
Evan Tahler <evan.tahler@grouparoo.com>
2021-02-17 10:23:53 -0800

    Get core version exactly from installed package.json for `grouparoo install` (#1315)
    * Share JSON spacer
    
    * Get core version exacrly from installed package.json version if possible

--- cli/src/utils/getCoreVersion.ts
+++ cli/src/utils/getCoreVersion.ts
@@ -2,13 +2,27 @@ import path from "path";
 import fs from "fs";
 import { readPackageJSON } from "./readPackageJSON";
 
+/**
+ * Get the exact version of @grouparoo/core installed in this project.  Defaults to 'latest'
+ * @param workDir
+ */
 export function getCoreVersion(workDir: string) {
   let coreVersion = "latest";
-  const packageFile = path.join(workDir, "package.json");
-  if (fs.existsSync(packageFile)) {
-    let pkgJSONContents = readPackageJSON(packageFile);
-    if (pkgJSONContents.dependencies["@grouparoo/core"]) {
-      coreVersion = pkgJSONContents.dependencies["@grouparoo/core"].trim();
+
+  try {
+    // prefer to get the exact version installed
+    const corePath = require.resolve("@grouparoo/core", { paths: [workDir] });
+    const corePackageFile = path.join(corePath, "..", "..", "package.json");
+    const corePkgJSONContents = readPackageJSON(corePackageFile);
+    coreVersion = corePkgJSONContents.version;
+  } catch (error) {
+    // otherwise get the version listed in the top-level package.json
+    const localPackageFile = path.join(workDir, "package.json");
+    if (fs.existsSync(localPackageFile)) {
+      let pkgJSONContents = readPackageJSON(localPackageFile);
+      if (pkgJSONContents.dependencies["@grouparoo/core"]) {
+        coreVersion = pkgJSONContents.dependencies["@grouparoo/core"].trim();
+      }
     }
   }
 
