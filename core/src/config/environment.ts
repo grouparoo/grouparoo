59a517bff25afff26b93570a44e314bb1c2beb60
Evan Tahler <evan.tahler@grouparoo.com>
2021-02-23 18:26:15 -0800

    If a .env file is present, load it in all environments + Testing Updates (#1356)
    * If a .env file is present, load it in all environments
    
    * setup staging apps for client testing
    
    * better test configuration
    
    * always log test code-config errors

--- core/src/config/environment.ts
+++ core/src/config/environment.ts
@@ -2,16 +2,19 @@ import fs from "fs";
 import path from "path";
 import { getParentPath } from "../utils/pluginDetails";
 
-// load env from .env in development
-// this needs to be done before requiring any part of actionhero
-const env = process.env.NODE_ENV || "development";
-
-if (env === "development" || process.env.GROUPAROO_ENV_CONFIG_FILE) {
-  const envFile =
-    process.env.GROUPAROO_ENV_CONFIG_FILE ||
-    path.resolve(path.join(getParentPath(), ".env"));
-  if (fs.existsSync(envFile)) {
-    require("dotenv").config({ path: envFile });
-    process.env.GROUPAROO_ENV_CONFIG_FILE = envFile;
-  }
+const envFileAttempts = [path.resolve(path.join(getParentPath(), ".env"))];
+if (process.env.GROUPAROO_ENV_CONFIG_FILE) {
+  envFileAttempts.unshift(process.env.GROUPAROO_ENV_CONFIG_FILE);
+  envFileAttempts.unshift(
+    path.join(getParentPath(), process.env.GROUPAROO_ENV_CONFIG_FILE)
+  );
 }
+
+let found = false;
+envFileAttempts.forEach((file) => {
+  if (!found && fs.existsSync(file)) {
+    require("dotenv").config({ path: file });
+    process.env.GROUPAROO_ENV_CONFIG_FILE = file;
+    found = true;
+  }
+});
