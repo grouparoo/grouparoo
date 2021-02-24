0fc4e15aa8ef8bd16bdce755ef6c714bce38f46a
Evan Tahler <evantahler@gmail.com>
2021-02-24 13:51:32 -0800

    import:associateProfiles

---
+++ core/src/tasks/import/associateProfiles.ts
@@ -0,0 +1,57 @@
+import { log } from "actionhero";
+import { plugin } from "../../modules/plugin";
+import { Import } from "../../models/Import";
+import { Run } from "../../models/Run";
+import { CLSTask } from "../../classes/tasks/clsTask";
+import { CLS } from "../../modules/cls";
+import { Op } from "sequelize";
+
+export class ImportAssociateProfiles extends CLSTask {
+  constructor() {
+    super();
+    this.name = "import:associateProfiles";
+    this.description = "ensure that imports are associated to profiles";
+    this.frequency = 30 * 1000;
+    this.queue = "imports";
+    this.inputs = {};
+  }
+
+  async runWithinTransaction() {
+    const limit = parseInt(
+      (await plugin.readSetting("core", "runs-profile-batch-size")).value
+    );
+
+    const imports = await Import.findAll({
+      where: { profileId: null },
+      limit,
+      order: [["createdAt", "asc"]],
+    });
+
+    const runIds: string[] = [];
+    console.log(imports);
+    for (const i in imports) {
+      const _import = imports[i];
+      await CLS.enqueueTask("import:associateProfile", {
+        importId: _import.id,
+      });
+
+      if (
+        _import.creatorType === "run" &&
+        !runIds.includes(_import.creatorId)
+      ) {
+        runIds.push(_import.creatorId);
+      }
+    }
+
+    if (runIds.length > 0) {
+      await Run.update(
+        { state: "running" },
+        { where: { state: "complete", id: { [Op.in]: runIds } }, logging: true }
+      );
+    }
+
+    if (imports.length > 0) {
+      log(`enqueued ${imports.length} imports for association`);
+    }
+  }
+}
