2ca2fb3598681b0a929181e834009f26afef11b0
Evan Tahler <evantahler@gmail.com>
2021-02-23 17:19:44 -0800

    If a .env file is present, load it in all environments

--- core/src/config/environment.ts
+++ core/src/config/environment.ts
@@ -2,16 +2,10 @@ import fs from "fs";
 import path from "path";
 import { getParentPath } from "../utils/pluginDetails";
 
-// load env from .env in development
-// this needs to be done before requiring any part of actionhero
-const env = process.env.NODE_ENV || "development";
-
-if (env === "development" || process.env.GROUPAROO_ENV_CONFIG_FILE) {
-  const envFile =
-    process.env.GROUPAROO_ENV_CONFIG_FILE ||
-    path.resolve(path.join(getParentPath(), ".env"));
-  if (fs.existsSync(envFile)) {
-    require("dotenv").config({ path: envFile });
-    process.env.GROUPAROO_ENV_CONFIG_FILE = envFile;
-  }
+const envFile =
+  process.env.GROUPAROO_ENV_CONFIG_FILE ||
+  path.resolve(path.join(getParentPath(), ".env"));
+if (fs.existsSync(envFile)) {
+  require("dotenv").config({ path: envFile });
+  process.env.GROUPAROO_ENV_CONFIG_FILE = envFile;
 }
