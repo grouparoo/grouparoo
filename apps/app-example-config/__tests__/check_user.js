6352dff9a71b35a2a5cf01f942b5d46f207f3a9e
Sean C Davis <sean.davis@grouparoo.com>
2021-03-03 14:31:33 -0500

    Add app-example-config as an example app

---
+++ apps/app-example-config/__tests__/check_user.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment node
+ */
+
+process.env.GROUPAROO_ENV_CONFIG_FILE = `${__dirname}/.env`;
+
+const { helper } = require("@grouparoo/spec-helper");
+
+describe("snapshot", () => {
+  helper.grouparooTestServer({ truncate: true });
+
+  test("Thomas Murrhardt is imported properly", async () => {
+    const { snapshot } = await helper.getProfile({
+      email: "tmurrhardtet@demo.com",
+      user_id: 534,
+    });
+
+    console.log(
+      Object.entries(snapshot.properties).map(([key, { values }]) => [
+        key,
+        values,
+      ])
+    );
+
+    expect(snapshot.properties.email.values[0]).toBe("tmurrhardtet@demo.com");
+  });
+});
