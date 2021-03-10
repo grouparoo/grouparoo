56da2a626e43535712a594ae868b961887ccf1e7
Pedro Lopez <pedroslopez@me.com>
2021-03-10 15:58:39 -0400

    fix: use recorded nock fixture

--- plugins/@grouparoo/pardot/__tests__/export/export-profiles.ts
+++ plugins/@grouparoo/pardot/__tests__/export/export-profiles.ts
@@ -8,11 +8,11 @@ import PardotClient from "../../src/lib/client";
 const nockFile = path.join(__dirname, "../", "fixtures", "export-profiles.js");
 
 // these comments to use nock
-// const newNock = false;
-// require("./../fixtures/export-profiles");
+const newNock = false;
+require("./../fixtures/export-profiles");
 // or these to make it true
-const newNock = true;
-helper.recordNock(nockFile, updater);
+// const newNock = true;
+// helper.recordNock(nockFile, updater);
 
 const appOptions = loadAppOptions(newNock);
 const appId = "app_a0bb07d8-0a4e-49b5-ad42-545f2e8662e6";
