73aba860566338f7de99c923f600e04f79b2b997
Pedro Lopez <pedroslopez@me.com>
2021-03-12 15:39:56 -0400

    pipedrive test method with better messages

--- plugins/@grouparoo/pipedrive/src/lib/test.ts
+++ plugins/@grouparoo/pipedrive/src/lib/test.ts
@@ -4,11 +4,13 @@ import { connect } from "./connect";
 export const test: TestPluginMethod = async ({ appOptions }) => {
   const client = await connect(appOptions);
 
-  const user = await client.UsersController.getCurrentUserData();
-  console.log(user);
-
-  return {
-    success: true,
-    message: "Connected.",
-  };
+  try {
+    const user = await client.UsersController.getCurrentUserData();
+    return {
+      success: true,
+      message: `Logged in as ${user.data.name} at ${user.data.company_name}.`,
+    };
+  } catch (err) {
+    throw err.errorMessage || err;
+  }
 };
