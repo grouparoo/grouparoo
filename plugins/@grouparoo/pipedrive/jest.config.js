1a8ef2883cbb22855ede43b3d9bd70a77b0a593b
Pedro Lopez <pedroslopez@me.com>
2021-03-22 10:32:05 -0400

    fix: use default timeout for tests

--- plugins/@grouparoo/pipedrive/jest.config.js
+++ plugins/@grouparoo/pipedrive/jest.config.js
@@ -1,6 +1,8 @@
+const { helper } = require("@grouparoo/spec-helper");
+
 module.exports = {
   testEnvironment: "node",
-  testTimeout: 240000,
+  testTimeout: helper.defaultTime,
   setupFiles: ["<rootDir>/jest.setup.js"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
