2d36f8d61d9d5988a5048aa629a117a7a1b3a051
Evan Tahler <evan.tahler@grouparoo.com>
2021-03-24 10:33:29 -0700

    Associate Profile tasks should use transactions w/ custom error handling

--- core/src/tasks/import/associateProfile.ts
+++ core/src/tasks/import/associateProfile.ts
@@ -1,8 +1,10 @@
 import { log, env, Task } from "actionhero";
 import { Import } from "../../models/Import";
+import { CLS } from "../../modules/cls";
 
 export class ImportAssociateProfile extends Task {
   // This Task extends Task rather than CLSTask as we want to be able to view newly created profiles happening in parallel to this task/transaction
+  // We still want things to be in a transaction, so we wrap the run method custom
   // This Task has no side effects
   constructor() {
     super();
@@ -19,22 +21,26 @@ export class ImportAssociateProfile extends Task {
 
   async run(params) {
     const { importId } = params;
-    const _import = await Import.findById(importId);
-    if (_import.profileId) return;
+    let _import: Import;
 
     try {
-      const { profile, isNew } = await _import.associateProfile();
+      await CLS.wrap(async () => {
+        _import = await Import.findById(importId);
+        if (_import.profileId) return;
 
-      const oldProfileProperties = await profile.simplifiedProperties();
-      const oldGroups = await profile.$get("groups");
+        const { profile, isNew } = await _import.associateProfile();
 
-      await _import.update({
-        createdProfile: isNew,
-        oldProfileProperties,
-        oldGroupIds: oldGroups.map((g) => g.id),
+        const oldProfileProperties = await profile.simplifiedProperties();
+        const oldGroups = await profile.$get("groups");
+
+        await _import.update({
+          createdProfile: isNew,
+          oldProfileProperties,
+          oldGroupIds: oldGroups.map((g) => g.id),
+        });
+
+        await profile.markPending();
       });
-
-      await profile.markPending();
     } catch (error) {
       if (env !== "test") log(`[ASSOCIATE PROFILE ERROR] ${error}`, "alert");
       await _import.setError(error, this.name);
