6944891ba8faf539d3fdbc7651679ce255f970a0
Sean C Davis <sean.davis@grouparoo.com>
2021-03-24 06:59:15 -0400

    Fix in-memory error notice on connect

--- plugins/@grouparoo/sqlite/src/lib/connect.ts
+++ plugins/@grouparoo/sqlite/src/lib/connect.ts
@@ -9,13 +9,15 @@ export const connect: ConnectPluginAppMethod = async ({ appOptions }) => {
 
   let dbPath = formattedOptions.file;
 
-  if (!path.isAbsolute(dbPath)) dbPath = path.join(getParentPath(), dbPath);
-
-  // Don't allow connections unless the file already exists. This avoids
-  // in-memory storage and creating the file during instantiation.
+  // Don't allow running with memory only.
   if (dbPath === ":memory:") {
     throw new Error(`The SQLite plugin does not support in-memory database.`);
   }
+
+  if (!path.isAbsolute(dbPath)) dbPath = path.join(getParentPath(), dbPath);
+
+  // Don't allow connections unless the file already exists. This avoids
+  // creating the file during instantiation.
   if (!fs.existsSync(dbPath) || fs.lstatSync(dbPath).isDirectory()) {
     throw new Error(`Could not find SQLite database: ${dbPath}`);
   }
