50e91552663053fecba6c7ec02f3d3a20444d298
Pedro Lopez <pedroslopez@me.com>
2021-04-01 18:28:24 -0400

    proper migrations for pipedrive and intercom

--- core/src/migrations/000062-addSyncModeToDestination.ts
+++ core/src/migrations/000062-addSyncModeToDestination.ts
@@ -6,14 +6,55 @@ export default {
         allowNull: true,
       });
 
+      // Pipedrive
       await migration.sequelize.query(
-        `UPDATE "destinations" SET "syncMode"='sync' WHERE "state"='ready'`
+        `UPDATE "destinations" SET "syncMode"='sync' WHERE "state"='ready' AND "type"='pipedrive-export'`
       );
 
-      // TODO Intercom option migration
-      // creationMode Enrich, removalMode Skip => Enrich
-      // creationMode != Enrich, removalMode Skip => Additive
-      // creationMode Enrich, removalMode != Skip => ???? Only update and delete
+      // Intercom
+      const [intercomOptions] = await migration.sequelize.query(
+        `SELECT "ownerId" AS "id", "key", "value" FROM "options" o JOIN "destinations" d ON o."ownerId"=d.id WHERE "state"='ready' AND d.type='intercom-export-contacts'`
+      );
+
+      const intercomDestinations = intercomOptions.reduce((dest, opt) => {
+        if (!dest[opt.id]) {
+          dest[opt.id] = {};
+        }
+        dest[opt.id][opt.key] = opt.value;
+        return dest;
+      }, {});
+
+      for (const destId in intercomDestinations) {
+        const dest = intercomDestinations[destId];
+        let syncMode;
+
+        if (dest.creationMode !== "Enrich" && dest.removalMode !== "Skip") {
+          // Allows creating and removing profiles
+          syncMode = "sync";
+        } else if (dest.removalMode === "Skip") {
+          // Allows creating, but not removing profiles
+          syncMode = "additive";
+        } else {
+          // Does not allow creating or removing profiles
+          syncMode = "enrich";
+        }
+
+        await migration.sequelize.query(
+          `UPDATE "destinations" SET "syncMode"=? WHERE "id"=?`,
+          {
+            logging: true,
+            replacements: [syncMode, destId],
+          }
+        );
+      }
+
+      await migration.sequelize.query(
+        `UPDATE "options" SET "value"='' WHERE ("value"='Skip' OR "value"='Enrich') AND "ownerId" IN(?) AND "key" IN ('creationMode', 'removalMode')`,
+        {
+          logging: true,
+          replacements: [Object.keys(intercomDestinations)],
+        }
+      );
     });
   },
 
