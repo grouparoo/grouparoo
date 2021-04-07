28dc33b1302cfedea7b1c26d10ccf196582cad56
Sean C Davis <scdavis41@gmail.com>
2021-04-07 17:58:08 -0400

    Fix type on Google Sheets Source template (#1608)
    * Fix type on gsheet source template
    
    * [WIP] Testing for CLI commands within plugins
    
    * locally validate newly generated config
    
    * fixup env logging and test path
    
    * make symlinks manualy to skip NPM
    
    Co-authored-by: Evan Tahler <evan.tahler@grouparoo.com>

--- plugins/@grouparoo/spec-helper/src/index.ts
+++ plugins/@grouparoo/spec-helper/src/index.ts
@@ -2,3 +2,4 @@ export * from "./lib/specHelper";
 export * from "./lib/workflows/import";
 export * from "./lib/relaxedSnapshot";
 export * from "./lib/integrationSpecHelper";
+export * from "./lib/cliSpecHelper";
---
+++ plugins/@grouparoo/spec-helper/src/lib/cliSpecHelper.ts
@@ -0,0 +1,181 @@
+import path from "path";
+import fs from "fs-extra";
+import { spawn } from "child_process";
+import os from "os";
+
+export namespace CLISpecHelper {
+  const jestId = parseInt(process.env.JEST_WORKER_ID || "1");
+
+  export async function spawnPromise(
+    command: string,
+    args: Array<string> = [],
+    cwd: string = process.cwd(),
+    extraEnv = {}
+  ): Promise<{ exitCode: number; stderr: string; stdout: string }> {
+    return new Promise((resolve, reject) => {
+      const env = process.env;
+      let stdout = "";
+      let stderr = "";
+
+      const spawnProcess = spawn(command, args, {
+        cwd,
+        env: Object.assign(extraEnv, env),
+      });
+
+      spawnProcess.stdout.on("data", (data) => {
+        stdout += String(data);
+      });
+
+      spawnProcess.stderr.on("data", (data) => {
+        stderr += String(data);
+      });
+
+      spawnProcess.on("close", (code) => {
+        if (code !== 0) return reject(new Error(stderr));
+        resolve({ stdout, stderr, exitCode: code });
+      });
+    });
+  }
+
+  export async function sleep(timeMs = 1000) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, timeMs);
+    });
+  }
+
+  export async function clear(projectDir: string) {
+    if (!fs.existsSync(projectDir)) fs.mkdirpSync(projectDir);
+    await fs.emptyDir(projectDir);
+  }
+
+  export function corePath() {
+    return path.join(require.resolve("@grouparoo/core"), "..", "..");
+  }
+
+  export async function install(
+    pluginName: string,
+    pluginPath: string,
+    projectPath: string
+  ) {
+    const template = `
+{
+  "name": "test",
+  "description": "A Grouparoo Deployment",
+  "version": "0.0.1",
+  "license": "MPL-2.0",
+  "private": true,
+  "dependencies": {
+    "@grouparoo/core": "file:${corePath()}",
+    "${pluginName}": "file:${pluginPath}"
+  },
+  "scripts": {},
+  "grouparoo": {
+    "plugins": ["${pluginName}"]
+  }
+}
+    `;
+
+    fs.writeFileSync(path.join(projectPath, "package.json"), template);
+
+    // we don't want to use NPM because it will try to re-run scripts (even with --ignore-scripts)
+    // npm would also be slow
+    fs.mkdirpSync(path.join(projectPath, "node_modules", "@grouparoo"));
+    fs.createSymlinkSync(
+      corePath(),
+      path.join(projectPath, "node_modules", "@grouparoo", "core"),
+      "dir"
+    );
+    fs.createSymlinkSync(
+      pluginPath,
+      path.join(projectPath, "node_modules", pluginName),
+      "dir"
+    );
+  }
+
+  function writeEnvFile(projectPath: string) {
+    const template = `
+PORT=3000
+WEB_URL=http://localhost:3000
+WEB_SERVER=true
+WORKERS=1
+SERVER_TOKEN=my-server-token
+
+# GROUPAROO_CONFIG_DIR=/path/to/config
+
+GROUPAROO_LOG_LEVEL=info
+# GROUPAROO_LOGS_PATH="logs"
+GROUPAROO_LOGS_STDOUT_DISABLE_COLOR=true
+# GROUPAROO_LOGS_STDOUT_DISABLE_TIMESTAMP=true
+
+REDIS_URL="redis://mock"
+DATABASE_URL="sqlite://grouparoo_test.sqlite"
+    `;
+
+    fs.writeFileSync(path.join(projectPath, ".env"), template);
+  }
+
+  export function enableBootstrappedProperty(
+    projectPath: string,
+    sourceName: string
+  ) {
+    const file = path.join(
+      projectPath,
+      "config",
+      "sources",
+      `${sourceName}.js`
+    );
+    const contents = fs.readFileSync(file).toString();
+    const lines = contents.split(os.EOL);
+    const updatedLines: string[] = [];
+
+    let inBootstrap = false;
+    for (const line of lines) {
+      if (line.includes("bootstrappedProperty: {")) inBootstrap = true;
+      if (inBootstrap && line === "") inBootstrap = false;
+
+      if (!inBootstrap) {
+        updatedLines.push(line);
+      } else {
+        updatedLines.push(line.replace("// ", ""));
+      }
+    }
+
+    fs.writeFileSync(file, updatedLines.join(os.EOL));
+  }
+
+  export function prepareForCLITest(
+    pluginName: string,
+    pluginPath: string,
+    projectPath: string = `${os.tmpdir()}/grouparoo_test/cli/${pluginName}/${jestId}`
+  ) {
+    // TODO: This only works within the monorepo
+    const cliRoot = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "..",
+      "cli",
+      "dist"
+    );
+    const cliBin = path.join(cliRoot, "grouparoo.js");
+
+    beforeAll(async () => {
+      console.log(`testing ${pluginName} CLI @ ${projectPath}`);
+      await clear(projectPath);
+      await install(pluginName, pluginPath, projectPath);
+      writeEnvFile(projectPath);
+    }, 1000 * 60);
+
+    async function runCliCommand(args: string[]) {
+      const cliEnv = Object.assign({}, process.env, {
+        GROUPAROO_PARENT_PATH: projectPath,
+        JEST_WORKER_ID: jestId,
+      });
+      return spawnPromise(cliBin, args, projectPath, cliEnv);
+    }
+
+    return { cliBin, projectPath, runCliCommand, spawnPromise };
+  }
+}
