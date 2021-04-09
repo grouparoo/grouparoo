3f3626e839c34cdc48cbfdf40a19fd36da873869
Pedro Lopez <pedroslopez@me.com>
2021-04-09 11:29:47 -0400

    remove "down" migration

--- plugins/@grouparoo/salesforce/src/migrations/000001-plugin-salesforce-syncMode.ts
+++ plugins/@grouparoo/salesforce/src/migrations/000001-plugin-salesforce-syncMode.ts
@@ -45,43 +45,7 @@ export default {
   },
 
   down: async function (migration) {
-    // TODO: test this to make sure it works!!
-    await migration.sequelize.transaction(async () => {
-      // Get sync modes
-      const [destinations] = await migration.sequelize.query(
-        `SELECT "id", "syncMode" FROM "destinations" WHERE "type"='salesforce-objects-export' AND "syncMode" IS NOT NULL AND "locked" IS NULL`
-      );
-
-      // Add corresponding "syncMode" option for each destination
-      const newOptions = [];
-      for (const dest of destinations) {
-        let syncMode: string;
-        switch (dest.syncMode) {
-          case "sync":
-            syncMode = "Sync";
-            break;
-          case "enrich":
-            syncMode = "Enrich";
-            break;
-          case "additive":
-            syncMode = "Additive";
-            break;
-          default:
-            continue;
-        }
-
-        newOptions.push({
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          ownerId: dest.id,
-          ownerType: "destination",
-          key: "syncMode",
-          value: syncMode,
-          type: "string",
-        });
-      }
-
-      await migration.bulkInsert("options", newOptions);
-    });
+    // TODO: We could do better here by creating the options based on the destination's syncMode
+    throw new Error("irreversible migration");
   },
 };
