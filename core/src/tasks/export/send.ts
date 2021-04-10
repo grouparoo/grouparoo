9d6d416583b4f34752060f664b36657139b3b41d
Evan Tahler <evantahler@gmail.com>
2021-04-10 10:50:18 -0700

    export:send tasks should not roll back transaction of there's a returned error

--- core/src/tasks/export/send.ts
+++ core/src/tasks/export/send.ts
@@ -1,9 +1,12 @@
-import { RetryableTask } from "../../classes/tasks/retryableTask";
+import { Task } from "actionhero";
 import { Destination } from "../../models/Destination";
 import { Export } from "../../models/Export";
+import { App } from "../../models/App";
+import { DestinationOps } from "../../modules/ops/destination";
 import { CLS } from "../../modules/cls";
+import { AsyncReturnType } from "type-fest";
 
-export class ExportSend extends RetryableTask {
+export class ExportSend extends Task {
   constructor() {
     super();
     this.name = "export:send";
@@ -16,31 +19,35 @@ export class ExportSend extends RetryableTask {
     };
   }
 
-  async runWithinTransaction(params) {
-    const destination = await Destination.scope(null).findOne({
-      where: { id: params.destinationId },
+  async run(params) {
+    let response: AsyncReturnType<typeof DestinationOps["sendExport"]>;
+    let app: App;
+
+    await CLS.wrap(async () => {
+      const destination = await Destination.scope(null).findOne({
+        where: { id: params.destinationId },
+      });
+      if (!destination) return;
+      app = await destination.$get("app");
+      const _export = await Export.findOne({
+        where: { id: params.exportId },
+      });
+      if (!_export) return; // the export was deleted
+      if (_export.completedAt) return; // be sure not to export twice
+
+      response = await destination.sendExport(_export);
     });
-    if (!destination) return;
-    const _export = await Export.findOne({ where: { id: params.exportId } });
-    if (!_export) return; // the export was deleted
-    if (_export.completedAt) return; // be sure not to export twice
 
-    const { success, retryDelay, error } = await destination.sendExport(
-      _export
-    );
-
-    if (!success) {
-      if (retryDelay) {
-        const app = await destination.$get("app");
+    if (!response.success) {
+      if (response.retryDelay) {
         return CLS.enqueueTaskIn(
-          retryDelay,
+          response.retryDelay,
           "export:send",
           params,
           `exports:${app.type}`
         );
       } else {
-        // auto retry
-        throw error;
+        throw response.error; // auto retry
       }
     }
   }
