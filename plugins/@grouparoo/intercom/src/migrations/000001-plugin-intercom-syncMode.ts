1e98c3348c55bd514c45b4de51f3fb8998cab857
Pedro Lopez <pedroslopez@me.com>
2021-04-12 15:43:16 -0400

    add migration rollback for intercom

--- plugins/@grouparoo/intercom/src/migrations/000001-plugin-intercom-syncMode.ts
+++ plugins/@grouparoo/intercom/src/migrations/000001-plugin-intercom-syncMode.ts
@@ -1,23 +1,37 @@
+const getDestinations = async (migration) => {
+  const [destinationOptions] = await migration.sequelize.query(
+    `SELECT "ownerId" AS "id", "key", "value", "syncMode" FROM "options" o JOIN "destinations" d ON o."ownerId"=d.id WHERE "state"='ready' AND d.type='intercom-export-contacts' AND "locked" IS NULL`
+  );
+
+  const destinations: {
+    [key: string]: {
+      syncMode?: string;
+      creationMode: string;
+      removalMode: string;
+    };
+  } = destinationOptions.reduce((dest, opt) => {
+    if (!dest[opt.id]) {
+      dest[opt.id] = {
+        syncMode: opt.syncMode,
+      };
+    }
+    dest[opt.id][opt.key] = opt.value;
+    return dest;
+  }, {});
+
+  return destinations;
+};
+
 export default {
   up: async function (migration) {
     await migration.sequelize.transaction(async () => {
       // Get current options for intercom destinations
-      const [intercomOptions] = await migration.sequelize.query(
-        `SELECT "ownerId" AS "id", "key", "value" FROM "options" o JOIN "destinations" d ON o."ownerId"=d.id WHERE "state"='ready' AND d.type='intercom-export-contacts' AND "locked" IS NULL`
-      );
-
-      const intercomDestinations = intercomOptions.reduce((dest, opt) => {
-        if (!dest[opt.id]) {
-          dest[opt.id] = {};
-        }
-        dest[opt.id][opt.key] = opt.value;
-        return dest;
-      }, {});
+      const intercomDestinations = await getDestinations(migration);
 
       // determine and update correct sync mode based on options
       for (const destId in intercomDestinations) {
         const dest = intercomDestinations[destId];
-        let syncMode;
+        let syncMode: string;
 
         if (dest.creationMode === "Enrich") {
           // Don't create people
@@ -52,6 +66,53 @@ export default {
   },
 
   down: async function (migration) {
-    throw new Error("cannot recover data for this migration");
+    // Get sync modes
+    const destinations = await getDestinations(migration);
+
+    const noCreateIds = [];
+    const noDeleteIds = [];
+    for (const destId in destinations) {
+      const { syncMode } = destinations[destId];
+      if (!syncMode) continue;
+
+      if (["enrich", "additive"].includes(syncMode)) {
+        noDeleteIds.push(destId);
+      }
+
+      if (syncMode === "enrich") {
+        noCreateIds.push(destId);
+      }
+    }
+
+    // Set removalMode="Skip" for destinations that don't delete
+    await migration.bulkUpdate(
+      "options",
+      {
+        value: "Skip",
+      },
+      {
+        ownerId: noDeleteIds,
+        key: "removalMode",
+      }
+    );
+
+    // Set creationMode="Enrich" for destinations that don't create
+    await migration.bulkUpdate(
+      "options",
+      { value: "Enrich" },
+      {
+        ownerId: noCreateIds,
+        key: "creationMode",
+      }
+    );
+
+    // Remove syncMode value
+    await migration.bulkUpdate(
+      "destinations",
+      {
+        syncMode: null,
+      },
+      { id: Object.keys(destinations) }
+    );
   },
 };
