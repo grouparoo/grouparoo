11264cec240c9c33f01e95a14683bedbfd9d19d2
Evan Tahler <evan.tahler@grouparoo.com>
2021-04-12 17:15:59 -0700

    Destroy sessions in a transaction within middleware

--- core/src/modules/middleware/authentication.ts
+++ core/src/modules/middleware/authentication.ts
@@ -28,13 +28,11 @@ export const AuthenticatedActionMiddleware: action.ActionMiddleware = {
   global: false,
   priority: 1000,
   preProcessor: async (data) => {
-    await CLS.wrap(async () => {
-      if (data.params.apiKey) {
-        return authenticateApiKey(data);
-      } else {
-        return authenticateTeamMember(data, false);
-      }
-    });
+    if (data.params.apiKey) {
+      return authenticateApiKey(data);
+    } else {
+      return authenticateTeamMember(data, false);
+    }
   },
 };
 
@@ -43,27 +41,41 @@ export const OptionallyAuthenticatedActionMiddleware: action.ActionMiddleware = 
   global: false,
   priority: 1000,
   preProcessor: async (data) => {
-    await CLS.wrap(async () => {
-      if (data.params.apiKey) {
-        return authenticateApiKey(data);
-      } else {
-        return authenticateTeamMember(data, true);
-      }
-    });
+    if (data.params.apiKey) {
+      return authenticateApiKey(data);
+    } else {
+      return authenticateTeamMember(data, true);
+    }
   },
 };
 
-export const ModelChatRoomMiddleware: chatRoom.ChatMiddleware = {
+export const ChatRoomMiddleware: chatRoom.ChatMiddleware = {
   name: "model chat room middleware",
+  priority: 1000,
   join: async (connection: Connection, room: string) => {
-    await CLS.wrap(async () => {
-      if (!room.match(/^model:/)) return;
+    return authenticateTeamMemberInRoom(connection, room);
+  },
+};
 
-      const topic = room.split(":")[1];
-      const mode = "read";
-      const session = await api.session.load(connection);
+// authenticate a web user with session cookie & csrfToken
+async function authenticateTeamMember(
+  data: { [key: string]: any },
+  optional: boolean
+) {
+  const error: Error = await CLS.wrap(
+    async () => {
+      const session = await api.session.load(data.connection);
+      if (!session && optional) return;
       if (!session || !session.id) {
         throw new AuthenticationError("Please log in to continue");
+      } else if (
+        (data.params.csrfToken && data.params.csrfToken !== session.id) ||
+        (!data.params.csrfToken &&
+          data.connection.rawConnection?.req?.headers[
+            "x-grouparoo-server-token"
+          ] !== config.general.serverToken)
+      ) {
+        throw new AuthenticationError("CSRF error");
       } else {
         const teamMember = await TeamMember.findOne({
           where: { id: session.teamMemberId },
@@ -73,45 +85,45 @@ export const ModelChatRoomMiddleware: chatRoom.ChatMiddleware = {
         if (!teamMember) throw new AuthenticationError("Team member not found");
 
         const team = await teamMember.$get("team");
-        const authorized = await team.authorizeAction(topic, "read");
+        const authorized = await team.authorizeAction(
+          data.actionTemplate.permission.topic,
+          data.actionTemplate.permission.mode
+        );
         if (!authorized) {
-          throw new AuthorizationError(mode, topic);
+          throw new AuthorizationError(
+            data.actionTemplate.permission.mode,
+            data.actionTemplate.permission.topic
+          );
         }
+
+        data.session.data = session;
+        data.session.teamMember = teamMember;
       }
-    });
-  },
-};
+    },
+    { catchError: true }
+  );
 
-// authenticate a web user with session cookie & csrfToken
-async function authenticateTeamMember(
-  data: { [key: string]: any },
-  optional: boolean
-) {
-  const session = await api.session.load(data.connection);
-  if (!session && optional) return;
-  if (!session || !session.id) {
-    throw new AuthenticationError("Please log in to continue");
-  } else if (
-    (data.params.csrfToken && data.params.csrfToken !== session.id) ||
-    (!data.params.csrfToken &&
-      data.connection.rawConnection?.req?.headers[
-        "x-grouparoo-server-token"
-      ] !== config.general.serverToken)
-  ) {
-    await api.session.destroy(data.connection);
-    throw new AuthenticationError("CSRF error");
-  } else {
-    const teamMember = await TeamMember.findOne({
-      where: { id: session.teamMemberId },
-      include: [Team],
-    });
-
-    if (!teamMember) {
-      throw new AuthenticationError("Team member not found");
+  if (error) {
+    if (error["code"] === "AUTHENTICATION_ERROR") {
+      await CLS.wrap(async () => {
+        await api.session.destroy(data.connection);
+      });
     }
 
-    const team = await teamMember.$get("team");
-    const authorized = await team.authorizeAction(
+    throw error;
+  }
+}
+
+// authenticate an API Request
+async function authenticateApiKey(data: { [key: string]: any }) {
+  await CLS.wrap(async () => {
+    const apiKey = await ApiKey.findOne({
+      where: { apiKey: data.params.apiKey },
+    });
+
+    if (!apiKey) throw new AuthenticationError("apiKey not found");
+
+    const authorized = await apiKey.authorizeAction(
       data.actionTemplate.permission.topic,
       data.actionTemplate.permission.mode
     );
@@ -122,28 +134,35 @@ async function authenticateTeamMember(
       );
     }
 
-    data.session.data = session;
-    data.session.teamMember = teamMember;
-  }
+    data.session.apiKey = apiKey;
+  });
 }
 
-// authenticate an API Request
-async function authenticateApiKey(data: { [key: string]: any }) {
-  const apiKey = await ApiKey.findOne({
-    where: { apiKey: data.params.apiKey },
+// authenticate a teamMember against an already-existing session in a WS room
+async function authenticateTeamMemberInRoom(
+  connection: Connection,
+  room: string
+) {
+  if (!room.match(/^model:/)) return;
+
+  const topic = room.split(":")[1];
+  const mode = "read";
+
+  await CLS.wrap(async () => {
+    const session = await api.session.load(connection);
+    if (!session || !session.id) {
+      throw new AuthenticationError("Please log in to continue");
+    } else {
+      const teamMember = await TeamMember.findOne({
+        where: { id: session.teamMemberId },
+        include: [Team],
+      });
+
+      if (!teamMember) throw new AuthenticationError("Team member not found");
+
+      const team = await teamMember.$get("team");
+      const authorized = await team.authorizeAction(topic, "read");
+      if (!authorized) throw new AuthorizationError(mode, topic);
+    }
   });
-  if (!apiKey) throw new AuthenticationError("apiKey not found");
-
-  const authorized = await apiKey.authorizeAction(
-    data.actionTemplate.permission.topic,
-    data.actionTemplate.permission.mode
-  );
-  if (!authorized) {
-    throw new AuthorizationError(
-      data.actionTemplate.permission.mode,
-      data.actionTemplate.permission.topic
-    );
-  }
-
-  data.session.apiKey = apiKey;
 }
