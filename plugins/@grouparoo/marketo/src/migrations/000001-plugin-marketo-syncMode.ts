6f07ebd8a8d61443760b95daa422b5e83ae800de
Pedro Lopez <pedroslopez@me.com>
2021-04-13 17:55:35 -0400

    use bulkUpdate in migration to be more consistent

--- plugins/@grouparoo/marketo/src/migrations/000001-plugin-marketo-syncMode.ts
+++ plugins/@grouparoo/marketo/src/migrations/000001-plugin-marketo-syncMode.ts
@@ -2,8 +2,16 @@ export default {
   up: async function (migration) {
     await migration.sequelize.transaction(async () => {
       // Set all existing instances to "sync" to maintain same behavior
-      await migration.sequelize.query(
-        `UPDATE "destinations" SET "syncMode"='sync' WHERE "state"='ready' AND "type"='marketo-export' AND "locked" IS NULL`
+      await migration.bulkUpdate(
+        "destinations",
+        {
+          syncMode: "sync",
+        },
+        {
+          state: "ready",
+          type: "marketo-export",
+          locked: null,
+        }
       );
     });
   },
