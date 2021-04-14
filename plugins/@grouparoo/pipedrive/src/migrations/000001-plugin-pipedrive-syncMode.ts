a1f374100cd710e80851072b1da6925f52423f79
Pedro Lopez <pedroslopez@me.com>
2021-04-14 18:28:42 -0400

    make pipedrive migration / rollback consistent

--- plugins/@grouparoo/pipedrive/src/migrations/000001-plugin-pipedrive-syncMode.ts
+++ plugins/@grouparoo/pipedrive/src/migrations/000001-plugin-pipedrive-syncMode.ts
@@ -2,8 +2,16 @@ export default {
   up: async function (migration) {
     await migration.sequelize.transaction(async () => {
       // Set all existing instances to "sync" to maintain same behavior
-      await migration.sequelize.query(
-        `UPDATE "destinations" SET "syncMode"='sync' WHERE "state"='ready' AND "type"='pipedrive-export' AND "locked" IS NULL`
+      await migration.bulkUpdate(
+        "destinations",
+        {
+          syncMode: "sync",
+        },
+        {
+          state: "ready",
+          type: "pipedrive-export",
+          locked: null,
+        }
       );
     });
   },
