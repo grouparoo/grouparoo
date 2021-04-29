66c2ca59a45f048aac36deede53989d1e07255b1
Pedro S. Lopez <pedroslopez@me.com>
2021-04-29 19:05:50 -0400

    fix test for google sheets source generation

--- plugins/@grouparoo/google-sheets/__tests__/integration/cli-generate-validate.ts
+++ plugins/@grouparoo/google-sheets/__tests__/integration/cli-generate-validate.ts
@@ -3,5 +3,10 @@ import { CLISpecHelper } from "@grouparoo/spec-helper";
 
 CLISpecHelper.validateGenerators(
   "@grouparoo/google-sheets",
-  join(__dirname, "..", "..")
+  join(__dirname, "..", ".."),
+  {
+    bootstrappedPropertyOptions: {
+      column: "id",
+    },
+  }
 );
