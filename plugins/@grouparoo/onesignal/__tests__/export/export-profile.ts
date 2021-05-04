813a3f8239bc0ce7de66f40b4673b8d97a7a3e42
Paulo Ouriques <ourixilva@gmail.com>
2021-05-04 15:10:58 -0300

    fix: newNock issue.

--- plugins/@grouparoo/onesignal/__tests__/export/export-profile.ts
+++ plugins/@grouparoo/onesignal/__tests__/export/export-profile.ts
@@ -26,11 +26,11 @@ const weirdGroupNormalized = "__high_value____";
 const nockFile = path.join(__dirname, "../", "fixtures", "export-profile.js");
 
 // these comments to use nock
-// const newNock = false;
-// require("./../fixtures/export-profile");
+const newNock = false;
+require("./../fixtures/export-profile");
 // or these to make it true
-const newNock = true;
-helper.recordNock(nockFile, updater);
+// const newNock = true;
+// helper.recordNock(nockFile, updater);
 
 const appOptions = loadAppOptions(newNock);
 
