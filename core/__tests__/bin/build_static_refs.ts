c3c6755d6006f399e22eb824e102f54c7f4bcfc6
Teal Larson <teal.larson@grouparoo.com>
2021-06-03 16:14:31 -0700

    removed core/bin tests for build_static_refs

--- core/__tests__/bin/build_static_refs.ts
+++ core/__tests__/bin/build_static_refs.ts
@@ -10,41 +10,13 @@ import path from "path";
 
 const outputDir = path.join(__dirname, "../../bin/static_refs");
 
-const pgFilePath = path.join(
-  outputDir,
-  "property-ops-dictionary--postgres.json"
-);
-const sqliteFilePath = path.join(
-  outputDir,
-  "property-ops-dictionary--sqlite.json"
-);
 const cliFilePath = path.join(outputDir, "cli-commands.json");
 
 const scriptPath = path.join(__dirname, "../../bin/build_static_refs");
 
 describe("bin/build_static_refs", () => {
   beforeEach(() => {
-    if (fs.existsSync(pgFilePath)) fs.unlinkSync(pgFilePath);
-    if (fs.existsSync(sqliteFilePath)) fs.unlinkSync(sqliteFilePath);
     if (fs.existsSync(cliFilePath)) fs.unlinkSync(cliFilePath);
-  });
-
-  test("writes two files with the appropriate data", () => {
-    expect(fs.existsSync(pgFilePath)).toBe(false);
-    expect(fs.existsSync(sqliteFilePath)).toBe(false);
-    cp.execSync(`node ${scriptPath}`);
-    const pgData = buildPropertyOpsDictionary({
-      sequelize: { dialect: "postgres" },
-    });
-    expect(fs.readFileSync(pgFilePath).toString()).toBe(
-      JSON.stringify(pgData, null, 2)
-    );
-    const sqliteData = buildPropertyOpsDictionary({
-      sequelize: { dialect: "sqlite" },
-    });
-    expect(fs.readFileSync(sqliteFilePath).toString()).toBe(
-      JSON.stringify(sqliteData, null, 2)
-    );
   });
 
   test("generates CLI data", async () => {
