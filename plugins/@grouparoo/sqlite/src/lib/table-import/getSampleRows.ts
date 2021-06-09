3d58f2af53f11b5226b032140b209673dd08c288
Sean C Davis <sean.davis@grouparoo.com>
2021-06-09 13:15:46 -0400

    Don't assume database has an id column

--- plugins/@grouparoo/sqlite/src/lib/table-import/getSampleRows.ts
+++ plugins/@grouparoo/sqlite/src/lib/table-import/getSampleRows.ts
@@ -7,7 +7,7 @@ export const getSampleRows: GetSampleRowsMethod = async ({
   connection,
   tableName,
 }) => {
-  const query = `SELECT * FROM ${tableName} WHERE id IN (SELECT id FROM ${tableName} ORDER BY RANDOM() LIMIT 10)`;
+  const query = `SELECT * FROM "${tableName}" ORDER BY RANDOM() LIMIT 10);`;
   const out: DataResponseRow[] = await connection.asyncQuery(query);
 
   return out;
