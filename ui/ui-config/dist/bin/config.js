19fbc13cecd1daf0ec92a658290222e72ca6daa4
Sean C Davis <scdavis41@gmail.com>
2021-06-11 15:02:41 -0400

    Move config CLI command to ui-config (#1916)
    * Move config command to ui-config
    
    * Make actionhero a dependency and remove src dir for ui-config
    
    * Add config dist script directly
    Continue to track

---
+++ ui/ui-config/dist/bin/config.js
@@ -0,0 +1,34 @@
+const cli = require("@grouparoo/core/dist/modules/cli");
+const actionhero = require("actionhero");
+
+class Apply extends actionhero.CLI {
+  constructor() {
+    super();
+    this.preInitialize = () => {
+      cli.GrouparooCLI.setGrouparooRunMode(this);
+      cli.GrouparooCLI.setNextDevelopmentMode();
+      // use temporary databases
+      process.env.DATABASE_URL =
+        process.env.CONFIG_DATABASE_URL || "sqlite://memory";
+      process.env.REDIS_URL = "redis://mock";
+      // turn off workers
+      process.env.WORKERS = "0";
+      // turn on web server
+      process.env.WEB_SERVER = "true";
+      if (!process.env.PORT) process.env.PORT = "3000";
+    };
+    this.name = "config";
+    this.description = "Interactively configure Grouparoo";
+    this.inputs = {};
+    cli.GrouparooCLI.timestampOption(this);
+  }
+
+  async run({ params }) {
+    cli.GrouparooCLI.logCLI(this.name, false);
+    const { main } = require("@grouparoo/core/dist/grouparoo");
+    await main(); // run the server
+    return false;
+  }
+}
+
+exports.Apply = Apply;
