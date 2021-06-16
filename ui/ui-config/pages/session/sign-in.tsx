14dffa0d1372744448d76ef83a5e74a9756b5668
Sean C Davis <sean.davis@grouparoo.com>
2021-06-16 05:50:07 -0400

    Fix autofocus for config sign in

--- ui/ui-config/pages/session/sign-in.tsx
+++ ui/ui-config/pages/session/sign-in.tsx
@@ -80,7 +80,6 @@ export default function SignInPage(props) {
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control
                   disabled={loading}
-                  autoFocus
                   required
                   name="email"
                   type="email"
