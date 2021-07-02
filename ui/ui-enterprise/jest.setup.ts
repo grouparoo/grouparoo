d7f68a3696c74d9b90d6e51a4c6161440b0bc134
Evan Tahler <evan.tahler@grouparoo.com>
2021-07-02 14:20:31 -0700

    Remove un-used test lib

--- ui/ui-enterprise/jest.setup.ts
+++ ui/ui-enterprise/jest.setup.ts
@@ -1,7 +1,2 @@
 process.env.NEXT_DISABLED = "false";
 import "@grouparoo/spec-helper/dist/lib/environment";
-
-import { configure } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-
-configure({ adapter: new Adapter() });
