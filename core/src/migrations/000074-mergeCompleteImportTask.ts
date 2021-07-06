575897d3ada3f686e75ea3a2679750c390c9104a
Pedro S. Lopez <pedroslopez@me.com>
2021-07-06 17:07:54 -0400

    migration query tweaks

--- core/src/migrations/000074-mergeCompleteImportTask.ts
+++ core/src/migrations/000074-mergeCompleteImportTask.ts
@@ -3,8 +3,8 @@ export default {
     await migration.sequelize.transaction(async () => {
       // Kick profiles back to "pending" if they're ready but their imports are not
       await migration.sequelize.query(
-        `UPDATE profiles SET "state"='pending' WHERE "state"='ready' AND "id" IN 
-          (SELECT "profileId" FROM imports WHERE "exportedAt" IS NULL AND "profileUpdatedAt" IS NULL AND "groupsUpdatedAt" IS NULL)`
+        `UPDATE "profiles" SET "state"='pending' WHERE "state"='ready' AND "id" IN 
+          (SELECT DISTINCT("profileId") FROM "imports" WHERE "exportedAt" IS NULL AND "profileUpdatedAt" IS NULL AND "groupsUpdatedAt" IS NULL)`
       );
     });
   },
