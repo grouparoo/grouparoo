ea24cc39f41a37475f1f7bc827e09f77a3111bbb
Pedro S. Lopez <pedroslopez@me.com>
2021-08-26 10:51:03 -0400

    add tests

--- core/__tests__/modules/codeConfig/codeConfig.ts
+++ core/__tests__/modules/codeConfig/codeConfig.ts
@@ -1,4 +1,8 @@
 import { helper } from "@grouparoo/spec-helper";
+import path from "path";
+import os from "os";
+import fs from "fs";
+import tar from "tar";
 import {
   plugin,
   Property,
@@ -17,9 +21,9 @@ import {
   GroupMember,
   GroupRule,
 } from "../../../src";
-import path from "path";
 import { api, specHelper } from "actionhero";
 import { loadConfigDirectory } from "../../../src/modules/configLoaders";
+import { getConfigDir } from "../../../src/utils/pluginDetails";
 
 describe("modules/codeConfig", () => {
   helper.grouparooTestServer({
@@ -1582,6 +1586,92 @@ describe("modules/codeConfig", () => {
       expect(errors[0]).toMatch(
         /fileId is not an option for a app-no-options app/
       );
+    });
+  });
+
+  describe("Get config directory", () => {
+    let previousConfigDir = process.env.GROUPAROO_CONFIG_DIR;
+    let previousConfigArchive = process.env.GROUPAROO_CONFIG_ARCHIVE;
+
+    beforeEach(() => {
+      delete process.env.GROUPAROO_CONFIG_DIR;
+      delete process.env.GROUPAROO_CONFIG_ARCHIVE;
+    });
+
+    afterAll(() => {
+      process.env.GROUPAROO_CONFIG_DIR = previousConfigDir;
+      process.env.GROUPAROO_CONFIG_ARCHIVE = previousConfigArchive;
+    });
+
+    test("can get the config directory from local path", async () => {
+      process.env.GROUPAROO_CONFIG_DIR = "/my/config/dir";
+      const configDir = await getConfigDir();
+      expect(configDir).toBe("/my/config/dir");
+    });
+
+    test("configDir defaults to local project's `/config`", async () => {
+      const configDir = await getConfigDir();
+      expect(configDir).toMatch(/core\/config$/);
+    });
+
+    test("configDir can be disabled", async () => {
+      process.env.GROUPAROO_CONFIG_DIR = "false";
+      const configDir = await getConfigDir();
+      expect(configDir).toBe(false);
+    });
+
+    describe("with config archive", () => {
+      let archivePath: string;
+      let extractedPath: string;
+
+      beforeAll(async () => {
+        const configPath = path.join(
+          __dirname,
+          "..",
+          "..",
+          "fixtures",
+          "codeConfig",
+          "initial"
+        );
+        const tmpDir = `${os.tmpdir()}/test/${
+          process.env.JEST_WORKER_ID
+        }/codeConfig/archive`;
+
+        fs.mkdirSync(tmpDir, { recursive: true });
+
+        archivePath = path.join(tmpDir, "grouparoo.tar.gz");
+        tar.create(
+          {
+            file: archivePath,
+            gzip: true,
+            cwd: configPath,
+            strict: true,
+            sync: true,
+          },
+          ["."]
+        );
+
+        expect(fs.existsSync(archivePath)).toBe(true);
+      });
+
+      test("config can be loaded from an archive", async () => {
+        process.env.GROUPAROO_CONFIG_ARCHIVE = archivePath;
+        process.env.GROUPAROO_CONFIG_DIR = "/";
+
+        const configDir = await getConfigDir();
+        expect(configDir).toMatch(/extracted\/$/);
+
+        extractedPath = configDir as string;
+        expect(fs.existsSync(path.join(extractedPath, "config.js")));
+      });
+
+      test("getting config dir a second time does not extract again", async () => {
+        process.env.GROUPAROO_CONFIG_ARCHIVE = archivePath;
+        process.env.GROUPAROO_CONFIG_DIR = "/";
+
+        const configDir = await getConfigDir();
+        expect(configDir).toBe(extractedPath);
+      });
     });
   });
 });
