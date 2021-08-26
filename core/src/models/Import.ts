3301f1fffa6752b431d7f3f9dfefd279e62522d0
Evan Tahler <evan.tahler@grouparoo.com>
2021-08-26 13:28:00 -0700

    remove unused return

--- core/src/models/Import.ts
+++ core/src/models/Import.ts
@@ -214,8 +214,6 @@ export class Import extends Model {
       const run = await Run.findOne({ where: { id: this.creatorId } });
       if (run && run.state === "complete") await run.buildErrorMessage();
     }
-
-    return this;
   }
 
   async associateProfile() {
