2c2410836f3d379f5f99fcdb52f59d039bd52801
Krishna Glick <krishna.glick@grouparoo.com>
2021-08-31 16:34:08 -0400

    Mongo logging

--- plugins/@grouparoo/mongo/src/lib/connect.ts
+++ plugins/@grouparoo/mongo/src/lib/connect.ts
@@ -1,7 +1,14 @@
 import { ConnectPluginAppMethod } from "@grouparoo/core";
+import { log } from "actionhero";
 import { MongoClient } from "mongodb";
 
 export const connect: ConnectPluginAppMethod = async ({ appOptions }) => {
   const uri = appOptions.uri?.toString();
-  return await new MongoClient(uri).connect();
+  const client = await new MongoClient(uri, {
+    monitorCommands: process.env.GROUPAROO_LOG_LEVEL === "debug",
+  }).connect();
+  client.on("commandStarted", (event) =>
+    log(`[ mongo ] ${JSON.stringify(event)}`, "debug")
+  );
+  return client;
 };
