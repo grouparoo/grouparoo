be6a1b2cf842ee703df7b74e5c49a0a84543460e
Evan Tahler <evan.tahler@grouparoo.com>
2021-09-02 15:51:14 -0400

    CSV tests passing

--- plugins/@grouparoo/csv/__tests__/integration/csv-remote.ts
+++ plugins/@grouparoo/csv/__tests__/integration/csv-remote.ts
@@ -85,7 +85,7 @@ describe("integration/runs/csv/remote", () => {
         name: "csv source",
         appId: app.id,
         options: {
-          url: "https://raw.githubusercontent.com/grouparoo/grouparoo/main/core/__tests__/data/records-10.csv",
+          url: "https://raw.githubusercontent.com/grouparoo/grouparoo/main/core/__tests__/data/profiles-10.csv",
           fileAgeHours: 1,
         },
         mapping: { id: "userId" },
