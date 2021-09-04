7441e92b5961781a20b7e6e12490bc80cc6d4178
Brian Leonard <brian@bleonard.com>
2021-09-03 22:29:56 -0700

    Uppercase snowflake column names

--- plugins/@grouparoo/demo/src/util/configFiles.ts
+++ plugins/@grouparoo/demo/src/util/configFiles.ts
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs-extra";
 import os from "os";
 import replaceInFiles from "replace-in-files";
+import glob from "glob";
 import { api, log } from "actionhero";
 import { loadConfigDirectory } from "@grouparoo/core/dist/modules/configLoaders";
 import { getConfigDir } from "@grouparoo/core/dist/modules/pluginDetails";
@@ -103,6 +104,8 @@ async function updateDatabase(db: Connection, configDir) {
     return;
   }
 
+  const name = db.name();
+
   // update with app options
   const appPath = path.join(configDir, "apps", "demo_db.json");
   if (fs.existsSync(appPath)) {
@@ -116,8 +119,104 @@ async function updateDatabase(db: Connection, configDir) {
 
   // update plugin name
   const files = path.resolve(path.join(configDir, "**", "*.json"));
-  const options = { files, from: "DBNAME", to: db.name() };
+  const options = { files, from: "DBNAME", to: name };
   await replaceInFiles(options);
+
+  // if needed, uppercase database elements
+  if (["snowflake"].includes(name)) {
+    await uppercaseDatabase(configDir, name);
+  }
+}
+
+async function uppercaseDatabase(configDir: string, name: string) {
+  // type: snowflake-table-import
+  // options.table, mapping.keys
+
+  // schedule with sourceId with type: snowflake-table-import
+  // options.column, filters(all).key
+
+  // property with sourceId with type: snowflake-table-import
+  // options.column, options.sortColumn, filters(all).key,
+
+  const sourcesPath = path.resolve(path.join(configDir, "sources", "*.json"));
+  const propertiesPath = path.resolve(
+    path.join(configDir, "properties", "*.json")
+  );
+
+  const sourceIds = [];
+  const sourceFiles = glob.sync(sourcesPath);
+  for (const file of sourceFiles) {
+    console.log(file);
+    const contents = fs.readJSONSync(file);
+    let source = null;
+    let schedule = null;
+    let write = false;
+    if (contents.length === undefined) {
+      source = contents;
+    } else {
+      // it's an array
+      source = contents.find(
+        (config) => config.class.toLowerCase() === "source"
+      );
+      schedule = contents.find(
+        (config) => config.class.toLowerCase() === "schedule"
+      );
+    }
+    // find the source
+    if (source && source.type === `${name}-table-import`) {
+      write = true;
+      sourceIds.push(source.id);
+      if (source.options?.table) {
+        source.options.table = source.options.table.toUpperCase();
+      }
+      for (const key in source.mapping || {}) {
+        const value = source.mapping[key];
+        delete source.mapping[key];
+        source.mapping[key.toUpperCase()] = value;
+      }
+    }
+    if (schedule && sourceIds.includes(schedule.sourceId)) {
+      write = true;
+      if (schedule.options?.column) {
+        schedule.options.column = schedule.options.column.toUpperCase();
+      }
+      for (const filter of schedule.filters || []) {
+        if (filter.key) {
+          filter.key = filter.key.toUpperCase();
+        }
+      }
+    }
+    if (write) {
+      const out = JSON.stringify(contents);
+      fs.writeFileSync(file, out);
+    }
+  }
+
+  const propertyFiles = glob.sync(propertiesPath);
+  for (const file of propertyFiles) {
+    console.log(file);
+    let write = false;
+    const contents = fs.readJSONSync(file);
+    const property = contents;
+    if (sourceIds.includes(property.sourceId)) {
+      write = true;
+      if (property.options?.column) {
+        property.options.column = property.options.column.toUpperCase();
+      }
+      if (property.options?.sortColumn) {
+        property.options.sortColumn = property.options.sortColumn.toUpperCase();
+      }
+      for (const filter of property.filters || []) {
+        if (filter.key) {
+          filter.key = filter.key.toUpperCase();
+        }
+      }
+    }
+    if (write) {
+      const out = JSON.stringify(contents);
+      fs.writeFileSync(file, out);
+    }
+  }
 }
 
 async function unlockAll() {
