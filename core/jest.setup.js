ac86a969f69291a2ea556c0d6bdada21ba40dc59
Krishna Glick <krishna.glick@grouparoo.com>
2021-09-08 17:04:35 -0400

    I don't think this was the right place for this

--- core/jest.setup.js
+++ core/jest.setup.js
@@ -2,4 +2,3 @@ require("@grouparoo/spec-helper/dist/lib/environment");
 
 // tell `@grouparoo/spec-helper` to load everything from `src` in core, so we can test without re-compiling
 process.env.GROUPAROO_SPEC_HELPER_LOAD_PATH = "src";
-process.env.TZ = "UTC";
