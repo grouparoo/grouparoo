cb9ec60408c61fd1de27a18582e47bed088ce92c
Pedro S. Lopez <pedro.lopez@grouparoo.com>
2021-09-15 17:23:12 -0400

    fix: dont throw on `profile:destroy` if it has already been deleted (backports #2298) (#2300)
    * fix: dont throw on `profile:destroy` if it has already been deleted
    backports #2298
    
    * try test fix
    
    * Use node.js v16.8.0 for CI (#2292)
    
    * Use node.js v16.8.0 for CI
    
    * Share docker image
    
    Co-authored-by: Evan Tahler <evan.tahler@grouparoo.com>

--- plugins/@grouparoo/csv/__tests__/remote-import/profiles.ts
+++ plugins/@grouparoo/csv/__tests__/remote-import/profiles.ts
@@ -8,7 +8,7 @@ import { profiles } from "../../src/lib/remote-import/profiles";
 
 // these used and set by test
 const sourceOptions: SimpleSourceOptions = {
-  url: "https://raw.githubusercontent.com/grouparoo/grouparoo/main/core/__tests__/data/profiles-10.csv",
+  url: "https://raw.githubusercontent.com/grouparoo/grouparoo/main/core/__tests__/data/records-10.csv",
   fileAgeHours: 1,
 };
 
