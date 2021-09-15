547d6ac7c97841768e17020ea90ef0fbc6a9ace8
Pedro S. Lopez <pedroslopez@me.com>
2021-09-15 15:36:10 -0400

    try test fix

--- plugins/@grouparoo/csv/__tests__/remote-import/profileProperty.ts
+++ plugins/@grouparoo/csv/__tests__/remote-import/profileProperty.ts
@@ -7,7 +7,7 @@ import { Profile, Property, SimpleSourceOptions } from "@grouparoo/core";
 import { profileProperty } from "../../src/lib/remote-import/profileProperty";
 
 const sourceOptions: SimpleSourceOptions = {
-  url: "https://raw.githubusercontent.com/grouparoo/grouparoo/main/core/__tests__/data/profiles-10.csv",
+  url: "https://raw.githubusercontent.com/grouparoo/grouparoo/main/core/__tests__/data/records-10.csv",
   fileAgeHours: 1,
 };
 let profile: Profile;
