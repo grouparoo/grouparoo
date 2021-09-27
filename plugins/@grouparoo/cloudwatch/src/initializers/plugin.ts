b95f75b920a996bd8817ab310b365e32e853aad0
Evan Tahler <evan.tahler@grouparoo.com>
2021-09-27 12:13:58 -0700

    `@grouparoo/cloudwatch` plugin

---
+++ plugins/@grouparoo/cloudwatch/src/initializers/plugin.ts
@@ -0,0 +1,53 @@
+import { Initializer, loggers, env, ActionheroLogLevel } from "actionhero";
+import { URL } from "url";
+import * as winston from "winston";
+import WinstonCloudWatch from "winston-cloudwatch";
+
+const packageJSON = require("./../../package.json");
+
+export default class CloudwatchInitializer extends Initializer {
+  constructor() {
+    super();
+    this.name = packageJSON.name;
+    this.loadPriority = 1;
+  }
+
+  async initialize() {
+    if (!process.env.S3_ACCESS_KEY) return;
+    if (!process.env.S3_SECRET_ACCESS_KEY) return;
+    if (!process.env.S3_REGION) return;
+
+    // try to extract the name of the instance from the URL we are using
+    // we could alternatively use the ClusterName setting
+    let name = `grouparoo-${env}`;
+    if (process.env.WEB_URL) {
+      const parsed = new URL(process.env.WEB_URL);
+      name = parsed.host.split(":")[0].replace(/\./g, "-");
+    }
+
+    const cloudwatchLogger = new WinstonCloudWatch({
+      name,
+      level: "notice" as ActionheroLogLevel,
+      logGroupName: name,
+      logStreamName: () => {
+        let date = new Date().toISOString().split("T")[0];
+        return `${name}-${date}`;
+      },
+      jsonMessage: true,
+      retentionInDays: 30, // store the logs for 30 days
+      awsAccessKeyId: process.env.S3_ACCESS_KEY,
+      awsSecretKey: process.env.S3_SECRET_ACCESS_KEY,
+      awsRegion: process.env.S3_REGION,
+    });
+
+    // stop method from https://github.com/lazywithclass/winston-cloudwatch#programmatically-flush-logs-and-exit
+    cloudwatchLogger.kthxbye = () => {};
+
+    const preparedLogger = winston.createLogger({
+      levels: winston.config.syslog.levels,
+      transports: [cloudwatchLogger],
+    });
+
+    loggers.push(preparedLogger);
+  }
+}
