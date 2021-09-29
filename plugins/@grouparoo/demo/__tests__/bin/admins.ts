49d57d70a7cc273fdcb7e990fa55d1569644f46d
Brian Leonard <brian@bleonard.com>
2021-09-29 16:53:23 -0700

    Update demo tests

--- plugins/@grouparoo/demo/__tests__/bin/admins.ts
+++ plugins/@grouparoo/demo/__tests__/bin/admins.ts
@@ -10,11 +10,12 @@ import {
   TeamMember,
 } from "@grouparoo/core";
 
-describe("demo accounts", () => {
+describe("demo admins", () => {
   hooks();
+
   test("runs without crash", async () => {
     const command = new Demo();
-    const toStop = await command.run({ params: { _arguments: ["accounts"] } });
+    const toStop = await command.run({ params: { _arguments: ["admins"] } });
     expect(toStop).toBe(true);
   });
 
@@ -26,25 +27,22 @@ describe("demo accounts", () => {
     expect(members).toEqual(["demo"]);
 
     const sources = (await Source.findAll()).map((o) => o.id).sort();
-    expect(sources).toEqual([
-      "account_users",
-      "demo_accounts",
-      "demo_payments",
-    ]);
+    expect(sources).toEqual(["demo_admins"]);
 
     const properties = (await Property.findAll()).map((o) => o.id).sort();
     expect(properties).toEqual([
-      "account_id",
-      "account_name",
-      "account_value",
-      "contact_email",
+      "admin_email",
+      "admin_first_name",
+      "admin_id",
+      "admin_last_name",
+      "admin_permission",
     ]);
 
     const schedules = (await Schedule.findAll()).map((o) => o.id).sort();
-    expect(schedules).toEqual(["demo_accounts_schedule"]);
+    expect(schedules).toEqual(["demo_admins_schedule"]);
 
     const groups = (await Group.findAll()).map((o) => o.id).sort();
-    expect(groups).toEqual(["all_accounts", "high_value_accounts"]);
+    expect(groups).toEqual(["all_admins", "owners"]);
 
     const destinations = (await Destination.findAll()).map((o) => o.id).sort();
     expect(destinations).toEqual([]);
