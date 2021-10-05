29236e2d9f67f090b7b85b1ce665d39553e8952e
Paulo Ouriques <ourixilva@gmail.com>
2021-10-04 23:25:46 -0300

    chore: add Zendesk marketplace headers.

--- plugins/@grouparoo/zendesk/src/lib/connect.ts
+++ plugins/@grouparoo/zendesk/src/lib/connect.ts
@@ -5,6 +5,16 @@ export async function connect(appOptions: SimpleAppOptions) {
   const { username, token, subdomain } = appOptions;
   const remoteUri = `https://${subdomain}.zendesk.com/api/v2`;
 
-  const client = new zendesk.createClient({ username, token, remoteUri });
-  return client;
+  const customHeaders = {
+    "X-Zendesk-Marketplace-Name": "Grouparoo",
+    "X-Zendesk-Marketplace-Organization-Id": 5084,
+    "X-Zendesk-Marketplace-App-Id": 577390,
+  };
+
+  return new zendesk.createClient({
+    username,
+    token,
+    remoteUri,
+    customHeaders,
+  });
 }
