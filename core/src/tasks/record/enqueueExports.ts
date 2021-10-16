6dd62192b0e9c1dce4d743d6a85677221ecac257
Evan Tahler <evan.tahler@grouparoo.com>
2021-10-16 07:14:26 -0700

    split query

--- core/src/tasks/record/enqueueExports.ts
+++ core/src/tasks/record/enqueueExports.ts
@@ -5,6 +5,7 @@ import { GrouparooRecord } from "../../models/GrouparooRecord";
 import { CLSTask } from "../../classes/tasks/clsTask";
 import { RecordProperty } from "../../models/RecordProperty";
 import { CLS } from "../../modules/cls";
+import Sequelize from "Sequelize";
 
 export class GrouparooRecordsEnqueueExports extends CLSTask {
   constructor() {
@@ -20,26 +21,31 @@ export class GrouparooRecordsEnqueueExports extends CLSTask {
   async runWithinTransaction() {
     const limit: number = config.batchSize.imports;
 
-    const records = await GrouparooRecord.findAll({
-      limit,
-      where: { state: "ready" },
-      include: [
-        {
-          model: Import,
-          attributes: [],
-          where: {
-            recordUpdatedAt: { [Op.not]: null },
-            groupsUpdatedAt: { [Op.not]: null },
-            exportedAt: null,
-          },
-        },
-        {
-          model: RecordProperty,
-          attributes: ["state"],
-          required: true,
-        },
+    const _imports = await Import.findAll({
+      attributes: [
+        [Sequelize.fn("DISTINCT", Sequelize.col("recordId")), "recordId"],
       ],
-      subQuery: false,
+      where: {
+        recordUpdatedAt: { [Op.not]: null },
+        groupsUpdatedAt: { [Op.not]: null },
+        exportedAt: null,
+      },
+      group: ["recordId"],
+      limit,
+    });
+
+    if (_imports.length === 0) return;
+
+    const records = await GrouparooRecord.findAll({
+      where: {
+        state: "ready",
+        id: { [Op.in]: _imports.map((i) => i.recordId) },
+      },
+      include: {
+        model: RecordProperty,
+        attributes: ["state"],
+        required: true,
+      },
     });
 
     const readyRecords = records.filter(
