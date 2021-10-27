f7673f9136322618d68e6a95cc9e389e4a1d4e6e
Evan Tahler <evan.tahler@grouparoo.com>
2021-10-27 14:49:25 -0700

    Use `router.replace` to update URL with Sample Records (#2468)
    * Use `router.replace` to update URL with Sample Records
    
    * simplify method args

--- ui/ui-components/components/destination/recordPreview.tsx
+++ ui/ui-components/components/destination/recordPreview.tsx
@@ -55,17 +55,14 @@ export default function RecordPreview(props) {
     JSON.stringify(destination.destinationGroupMemberships),
   ]);
 
-  function storeRecordPropertyId(recordId: string = "") {
+  function storeRecordPropertyId(recordId = "") {
     setRecordId(recordId);
     let url = `${window.location.pathname}?`;
+    if (recordId) url += `recordId=${recordId}&`;
 
-    if (recordId !== "") {
-      url += `recordId=${recordId}&`;
+    if (url !== `${window.location.pathname}?`) {
+      router.replace(router.route, url, { shallow: true });
     }
-
-    const routerMethod =
-      url === `${window.location.pathname}?` ? "replace" : "push";
-    router[routerMethod](router.route, url, { shallow: true });
   }
 
   async function load(_recordId = "", _sleep = sleep) {
