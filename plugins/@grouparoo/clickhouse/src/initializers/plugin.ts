3c835f3aaae87b1c2e26de09e1417473892078cb
Edmundo Ruiz Ghanem <edmundo.rg@grouparoo.com>
2021-10-27 16:40:36 -0400

    Update clickhouse to use the mysql plugin as the app connect command

--- plugins/@grouparoo/clickhouse/src/initializers/plugin.ts
+++ plugins/@grouparoo/clickhouse/src/initializers/plugin.ts
@@ -1,7 +1,15 @@
+import path from "path";
+
 import { Initializer } from "actionhero";
 import { plugin } from "@grouparoo/core";
 
+import { AppTemplate } from "@grouparoo/app-templates/dist/app";
+import { connect } from "@grouparoo/mysql/dist/lib/connect";
+import { disconnect } from "@grouparoo/mysql/dist/lib/disconnect";
+import { test } from "@grouparoo/mysql/dist/lib/test";
+
 const packageJSON = require("./../../package.json");
+const templateRoot = path.join(__dirname, "..", "..", "public", "templates");
 
 export class Plugins extends Initializer {
   name = packageJSON.name;
@@ -9,9 +17,54 @@ export class Plugins extends Initializer {
   async initialize() {
     plugin.registerPlugin({
       name: packageJSON.name,
-      icon: "",
-      templates: [],
-      apps: [],
+      icon: "/public/@grouparoo/clickhouse/clickhouse.png",
+      templates: [
+        new AppTemplate("clickhouse", [
+          path.join(templateRoot, "app", "*.template"),
+        ]),
+      ],
+      apps: [
+        {
+          name: "clickhouse",
+          displayName: "ClickHouse",
+          options: [
+            {
+              key: "host",
+              displayName: "Host",
+              required: false,
+              description: "The ClickHouse host.",
+              placeholder: "localhost",
+            },
+            {
+              key: "port",
+              displayName: "Port",
+              required: false,
+              description: "The ClickHouse MySQL interface port.",
+              defaultValue: 9004,
+            },
+            {
+              key: "database",
+              displayName: "Database",
+              required: true,
+              description: "The ClickHouse database.",
+            },
+            {
+              key: "user",
+              displayName: "User",
+              required: false,
+              description: "The ClickHouse user.",
+            },
+            {
+              key: "password",
+              type: "password",
+              displayName: "Password",
+              required: false,
+              description: "The ClickHouse user's password.",
+            },
+          ],
+          methods: { test, connect, disconnect },
+        },
+      ],
       connections: [],
     });
   }
