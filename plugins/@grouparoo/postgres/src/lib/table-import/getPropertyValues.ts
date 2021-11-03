d3eaf9eec57076a8a1987bfded050d58aacc3d3d
Brian Leonard <brian@bleonard.com>
2021-11-02 21:32:08 -0700

    Postgres Partition

--- plugins/@grouparoo/postgres/src/lib/table-import/getPropertyValues.ts
+++ plugins/@grouparoo/postgres/src/lib/table-import/getPropertyValues.ts
@@ -21,53 +21,65 @@ export const getPropertyValues: GetPropertyValuesMethod = async ({
   primaryKeys,
 }) => {
   let responses: { [key: string]: { [column: string]: DataResponse[] } } = {};
-  let aggSelect = columnNames.map((col) => `"${col}"`).join(", ");
-  let orderBy = "";
-  let groupByColumns = [tablePrimaryKeyCol];
+  const columnList = columnNames.map((col) => `"${col}"`).join(", ");
+  const orderBys = [];
+  const groupByColumns = [];
+  let aggFunc = null;
 
   if (primaryKeys.length === 0) return responses;
 
   switch (aggregationMethod) {
     case AggregationMethod.Exact:
-      groupByColumns.push(...columnNames);
       if (sortColumn) {
-        orderBy = `"${sortColumn}" ASC`;
-        groupByColumns.push(sortColumn);
+        orderBys.push(`"${sortColumn}" ASC`);
       }
       break;
     case AggregationMethod.Average:
-      aggSelect = `COALESCE(AVG(${aggSelect}), 0) as ${aggSelect}`;
+      aggFunc = `COALESCE(AVG(${columnList}), 0) as ${columnList}`;
       break;
     case AggregationMethod.Count:
-      aggSelect = `COUNT(${aggSelect})::integer as ${aggSelect}`;
+      aggFunc = `COUNT(${columnList})::integer as ${columnList}`;
       break;
     case AggregationMethod.Sum:
-      aggSelect = `COALESCE(SUM(${aggSelect}), 0) as ${aggSelect}`;
+      aggFunc = `COALESCE(SUM(${columnList}), 0) as ${columnList}`;
       break;
     case AggregationMethod.Min:
-      aggSelect = `MIN(${aggSelect}) as ${aggSelect}`;
+      aggFunc = `MIN(${columnList}) as ${columnList}`;
       break;
     case AggregationMethod.Max:
-      aggSelect = `MAX(${aggSelect}) as ${aggSelect}`;
+      aggFunc = `MAX(${columnList}) as ${columnList}`;
       break;
     case AggregationMethod.MostRecentValue:
       if (!sortColumn) throw new Error("Sort Column is needed");
-      orderBy = `"${sortColumn}" DESC`;
-      groupByColumns.push(columnNames[0]);
-      groupByColumns.push(sortColumn);
+      orderBys.push(`"${sortColumn}" DESC`);
       break;
     case AggregationMethod.LeastRecentValue:
       if (!sortColumn) throw new Error("Sort Column is needed");
-      orderBy = `"${sortColumn}" ASC`;
-      groupByColumns.push(columnNames[0]);
-      groupByColumns.push(sortColumn);
+      orderBys.push(`"${sortColumn}" ASC`);
       break;
     default:
       throw new Error(`${aggregationMethod} is not a known aggregation method`);
   }
 
-  const params: Array<any> = [tableName];
-  let query = `SELECT ${aggSelect}, "${tablePrimaryKeyCol}" as __pk FROM %I WHERE`;
+  const params: Array<any> = [];
+  let ranked = false;
+  let query = `SELECT "${tablePrimaryKeyCol}" as __pk`;
+
+  if (aggFunc) {
+    query += `, ${aggFunc}`;
+    groupByColumns.push(tablePrimaryKeyCol);
+  } else {
+    query += `, ${columnList}`;
+    if (!isArray && orderBys.length > 0) {
+      const order = `ORDER BY ${orderBys.join(", ")}`;
+      query += `, ROW_NUMBER() OVER (PARTITION BY "${tablePrimaryKeyCol}" ${order}) AS __rownum`;
+      ranked = true;
+    }
+  }
+
+  query += ` FROM %I WHERE`;
+  params.push(tableName);
+
   let addAnd = false;
 
   for (const condition of matchConditions) {
@@ -93,8 +105,21 @@ export const getPropertyValues: GetPropertyValuesMethod = async ({
     query += ` GROUP BY %I`;
     params.push(groupByColumns);
   }
-  if (orderBy.length > 0) {
-    query += ` ORDER BY ${orderBy}`;
+  if (!ranked && orderBys.length > 0) {
+    query += ` ORDER BY ${orderBys.join(", ")}`;
+  }
+
+  if (ranked) {
+    // -- Ranked example
+    // SELECT * FROM
+    //  (SELECT  "user_id" as __pk, "created_at",
+    //              ROW_NUMBER() OVER (PARTITION BY "user_id" ORDER BY "created_at" DESC) AS __rownum
+    //        FROM demo.purchases
+    //        WHERE  "state" = 'successful' AND "user_id" IN ('1','2','3','4')
+    //   ) AS __ranked
+    // WHERE __ranked.__rownum = 1;
+
+    query = `SELECT * FROM (${query}) AS __ranked WHERE __ranked.__rownum = 1`;
   }
 
   validateQuery(query);
