8cdb770fd83883e801e0378f441c7ff2d330fd16
Evan Tahler <evan.tahler@grouparoo.com>
2021-11-08 18:32:34 -0800

    Integration tests for Redshift Plugin

---
+++ plugins/@grouparoo/redshift/__tests__/utils/data.ts
@@ -0,0 +1,57 @@
+import { connect } from "@grouparoo/postgres/dist/lib/connect";
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
+
+const dirPath = path.resolve(path.join(__dirname, ".."));
+const realPath = path.join(dirPath, ".env");
+const parsed = dotenv.parse(fs.readFileSync(realPath));
+if (!process.env.REDSHIFT_URL && !parsed.REDSHIFT_URL) {
+  throw new Error(`process.env.REDSHIFT_URL is required`);
+}
+const conf = new URL(process.env.REDSHIFT_URL ?? parsed.REDSHIFT_URL);
+
+export const schema = "test";
+export const usersTableName = `users`;
+export const purchasesTableName = `purchases`;
+export const accountsTableName = `accounts`;
+export const appId = "app_31bb06e8-0a4e-49c3-ad42-545f2e8662e1";
+
+export const appOptions = {
+  user: conf.username,
+  password: conf.password,
+  host: conf.hostname,
+  port: conf.port,
+  database: conf.pathname.replace(/^\//, ""),
+  schema,
+};
+
+let client;
+
+export async function getClient() {
+  if (client) return client;
+
+  client = await connect({
+    appOptions,
+    app: null,
+    appId,
+  });
+
+  return client;
+}
+
+export async function endClient() {
+  if (client) {
+    await client.end();
+    client = null;
+  }
+}
+
+export function getConfig() {
+  return {
+    appOptions,
+    appId,
+    usersTableName,
+    purchasesTableName,
+  };
+}
