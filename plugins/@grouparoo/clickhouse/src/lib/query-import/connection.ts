760f7f930e7b4301d596a50b8e2ac17145d174f5
Edmundo Ruiz Ghanem <edmundo.rg@grouparoo.com>
2021-11-09 13:02:25 -0500

    Add ClickHouse query import records test * Add missing getChangedRows to query-import

--- plugins/@grouparoo/clickhouse/src/lib/query-import/connection.ts
+++ plugins/@grouparoo/clickhouse/src/lib/query-import/connection.ts
@@ -1,5 +1,6 @@
 import { buildConnection } from "@grouparoo/app-templates/dist/source/query";
 import { executeQuery } from "@grouparoo/mysql/dist/lib/query-import/executeQuery";
+import { getChangedRows } from "@grouparoo/mysql/dist/lib/query-import/getChangedRows";
 
 export function getConnection() {
   return buildConnection({
@@ -8,5 +9,6 @@ export function getConnection() {
     displayName: "ClickHouse Query Import",
     description: "Import or update Records via a custom ClickHouse query.",
     executeQuery,
+    getChangedRows,
   });
 }
