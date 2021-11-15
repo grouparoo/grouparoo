5ed2f07a68f177f680e9fb9c78ee2594057a39be
Brian Leonard <brian@bleonard.com>
2021-11-15 14:49:40 -0800

    Generate clickhouse config in demo command (#2550)
    * Generate clickhosue config
    
    * Updated default URL
    
    * Load via 9004 but connect over 8123

--- plugins/@grouparoo/demo/src/connections/clickhouse.ts
+++ plugins/@grouparoo/demo/src/connections/clickhouse.ts
@@ -1,8 +1,91 @@
+import mysql from "mysql";
+import { log } from "actionhero";
+import Connection from "../util/connection";
+import { promisify } from "util";
 import { TYPES } from "../util/data";
 
-import MySQL from "./mysql";
+function findConfig(): any {
+  let connectionURL = process.env.DEMO_CLICKHOUSE_URL;
+  if (!connectionURL) {
+    // return the default
+    connectionURL = "clickhouse://default:@localhost:8123/grouparoo_demo";
+    log(`Using default local Clickhouse database: ${connectionURL}`);
+    log(`Set your own via DEMO_CLICKHOUSE_URL env variable`);
+  }
 
-export default class ClickHouse extends MySQL {
+  let dialect = null;
+  const clientConfig = {
+    user: null,
+    password: null,
+    host: null,
+    database: null,
+    port: null,
+  };
+  const parsed = new URL(connectionURL);
+  if (parsed.protocol) dialect = parsed.protocol.slice(0, -1);
+  if (parsed.username) clientConfig.user = parsed.username;
+  if (parsed.password) clientConfig.password = parsed.password;
+  if (parsed.hostname) clientConfig.host = parsed.hostname;
+  if (parsed.port) clientConfig.port = parsed.port;
+  if (parsed.pathname) clientConfig.database = parsed.pathname.substring(1);
+
+  log(`Clickhouse config: ${JSON.stringify(clientConfig)}`, "debug");
+  if (dialect !== "mysql" && dialect !== "clickhouse") {
+    throw new Error("Set DEMO_CLICKHOUSE_URL to a Clickhouse database.");
+  }
+
+  return clientConfig;
+}
+
+export default class ClickHouse extends Connection {
+  client: any;
+  config: { [key: string]: any };
+  constructor() {
+    super();
+    this.config = Object.assign({}, findConfig());
+    this.client = null;
+  }
+
+  name() {
+    return "clickhouse";
+  }
+
+  getAppOptions() {
+    const { host, port, database, user, password } = this.config;
+    const appOptions = { host, port, database, user, password };
+    for (const key in appOptions) {
+      appOptions[key] = (appOptions[key] || "").toString();
+    }
+    return appOptions;
+  }
+
+  async disconnect() {
+    if (this.client) {
+      await this.client.end();
+      this.client = null;
+    }
+  }
+  async connect() {
+    if (this.client) {
+      return this.client;
+    }
+
+    // connects over sql, not http
+    const sqlConn = Object.assign({}, this.config, { port: 9004 });
+
+    const conn = mysql.createConnection(sqlConn);
+    const connect = promisify(conn.connect).bind(conn);
+    const query = promisify(conn.query).bind(conn);
+    const end = promisify(conn.end).bind(conn);
+
+    const client = { connect, query, end };
+
+    await client.connect();
+
+    this.client = client;
+    return this.client;
+  }
+
   async createTable(tableName: string, typeColumn: string, keys: string[]) {
     const sqlTable = tableName;
 
@@ -65,7 +148,7 @@ export default class ClickHouse extends MySQL {
     keys: string[],
     row: Record<string, string | number | Date | boolean>
   ) {
-    const compatibleRow = keys.reduce((acc, key) => {
+    row = keys.reduce((acc, key) => {
       const value = row[key];
 
       // ClickHouse BOOLEAN is an Int8. The compatible values are 1 and 0
@@ -75,6 +158,21 @@ export default class ClickHouse extends MySQL {
       return acc;
     }, {});
 
-    super.insertRow(tableName, keys, compatibleRow);
+    const sqlTable = `\`${tableName}\``;
+    const columnNames = keys.join(", ");
+    const variables = keys.map((key, index) => "?");
+    const insertQuery = `INSERT INTO ${sqlTable} (${columnNames}) VALUES (${variables})`;
+
+    const values: Array<any> = keys.map((key) => row[key]);
+    await this.query(2, insertQuery, values);
+  }
+
+  async query(level, sql, params = null) {
+    log("query", "debug", { level, sql, params });
+    const client = await this.connect();
+    if (params) {
+      return client.query(sql, params);
+    }
+    return client.query(sql);
   }
 }
