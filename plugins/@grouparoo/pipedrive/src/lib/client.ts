06dbe7eace35c8fe1fd2384873c9e0d8642fd373
Ahmed El-Afifi <con-ahmedelafifi@cpm.org>
2021-11-18 18:43:00 +0200

    Add organization API calls for Pipedrive client
    - Generalize common API calls.
    - Add API calls for organizations.

--- plugins/@grouparoo/pipedrive/src/lib/client.ts
+++ plugins/@grouparoo/pipedrive/src/lib/client.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
+type Entity = 'persons' | 'organizations';
+type EntityField = 'personFields' | 'organizationFields';
+
 export class PipedriveClient {
   request: AxiosInstance;
 
@@ -35,17 +38,51 @@ export class PipedriveClient {
     return data.data.items[0].item.id;
   }
 
-  async getPerson(id: number): Promise<Record<string, any>> {
+  async getEntityById(entity: Entity, id: number): Promise<Record<string, any>> {
     const { data } = await this.request.get<Record<string, any>>(
-      `/persons/${id}`
-    );
+      `/${entity}/${id}`);
     return data.data;
+  }
+
+  async getPerson(id: number): Promise<Record<string, any>> {
+    return await this.getEntityById('persons', id);
+  }
+
+  async getOrganization(id: number): Promise<Record<string, any>> {
+    return await this.getEntityById('organizations', id);
+  }
+
+  async createEntity(
+    entity: Entity,
+    payload: Record<string, any>
+  ): Promise<Record<string, any>> {
+    const { data } = await this.request.post(`/${entity}`, payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return data;
   }
 
   async createPerson(
     payload: Record<string, any>
   ): Promise<Record<string, any>> {
-    const { data } = await this.request.post(`/persons`, payload, {
+    return await this.createEntity('persons', payload);
+  }
+
+  async createOrganization(
+    payload: Record<string, any>
+  ): Promise<Record<string, any>> {
+    return await this.createEntity('organizations', payload);
+  }
+
+  async updateEntity(
+    entity: Entity,
+    id: number,
+    payload: Record<string, any>
+  ): Promise<Record<string, any>> {
+    const { data } = await this.request.put(`/${entity}/${id}`, payload, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -58,50 +95,101 @@ export class PipedriveClient {
     id: number,
     payload: Record<string, any>
   ): Promise<Record<string, any>> {
-    const { data } = await this.request.put(`/persons/${id}`, payload, {
+    return await this.updateEntity('persons', id, payload);
+  }
+
+  async updateOrganization(
+    id: number,
+    payload: Record<string, any>
+  ): Promise<Record<string, any>> {
+    return await this.updateEntity('organizations', id, payload);
+  }
+
+  async deleteEntity(
+    entity: Entity,
+    id: number
+  ): Promise<Record<string, any>> {
+    const { data } = await this.request.delete(`/${entity}/${id}`);
+    return data;
+  }
+
+  async deletePerson(id: number): Promise<Record<string, any>> {
+    return await this.deleteEntity('persons', id);
+  }
+
+  async deleteOrganization(id: number): Promise<Record<string, any>> {
+    return await this.deleteEntity('organizations', id);
+  }
+
+  async getAllFields(entityField: EntityField): Promise<Record<string, any>[]> {
+    const { data } = await this.request.get<Record<string, any>>(
+      `/${entityField}`
+    );
+    return data.data;
+  }
+
+  async getAllPersonFields(): Promise<Record<string, any>[]> {
+    return await this.getAllFields('personFields');
+  }
+
+  async getAllOrganizationFields(): Promise<Record<string, any>[]> {
+    return await this.getAllFields('organizationFields');
+  }
+
+  async createField(
+    entityField: EntityField,
+    payload: Record<string, any>
+  ): Promise<Record<string, any>> {
+    const { data } = await this.request.post(`/${entityField}`, payload, {
       headers: {
         "Content-Type": "application/json",
       },
     });
 
     return data;
-  }
-
-  async deletePerson(id: number): Promise<Record<string, any>> {
-    const { data } = await this.request.delete(`/persons/${id}`);
-    return data;
-  }
-
-  async getAllPersonFields(): Promise<Record<string, any>[]> {
-    const { data } = await this.request.get<Record<string, any>>(
-      "/personFields"
-    );
-    return data.data;
   }
 
   async createPersonField(
     payload: Record<string, any>
   ): Promise<Record<string, any>> {
-    const { data } = await this.request.post(`/personFields`, payload, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    return data;
+    return await this.createField('personFields', payload);
   }
 
-  async bulkDeletePersonFields(ids: number[]): Promise<Record<string, any>> {
-    const { data } = await this.request.delete(`/personFields`, {
+  async createOrganizationField(
+    payload: Record<string, any>
+  ): Promise<Record<string, any>> {
+    return await this.createField('personFields', payload);
+  }
+
+  async bulkDeleteFields(
+    entityField: EntityField,
+    ids: number[]
+  ): Promise<Record<string, any>> {
+    const { data } = await this.request.delete(`/${entityField}`, {
       params: { ids: ids.join(",") },
     });
 
     return data;
   }
 
+  async bulkDeletePersonFields(ids: number[]): Promise<Record<string, any>> {
+    return this.bulkDeleteFields('personFields', ids);
+  }
+
+  async bulkDeleteOrganizationFields(ids: number[]): Promise<Record<string, any>> {
+    return this.bulkDeleteFields('organizationFields', ids);
+  }
+
   async getAllPersonFilters(): Promise<Record<string, any>[]> {
     const { data } = await this.request.get<Record<string, any>>("/filters", {
       params: { type: "people" },
+    });
+    return data.data;
+  }
+
+  async getAllOrganizationFilters(): Promise<Record<string, any>[]> {
+    const { data } = await this.request.get<Record<string, any>>("/filters", {
+      params: { type: "org" },
     });
     return data.data;
   }
