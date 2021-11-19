26745e3d92fd9deb9b79c4e38b70ef26f363926b
Evan Tahler <evan.tahler@grouparoo.com>
2021-11-19 10:57:54 -0800

    remove break

--- ui/ui-components/pages/model/[modelId]/source/[sourceId]/multipleProperties.tsx
+++ ui/ui-components/pages/model/[modelId]/source/[sourceId]/multipleProperties.tsx
@@ -94,7 +94,6 @@ export default function Page(props) {
 
       if (optMatch) {
         existingProperty = property;
-        break;
       } else {
         otherProperties.push(property);
       }
