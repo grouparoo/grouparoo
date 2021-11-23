32404a8c274749b10448d31cb998be5240da1e25
Ahmed El-Afifi <con-ahmedelafifi@cpm.org>
2021-11-23 17:47:08 +0200

    Fix initializers issue

--- plugins/@grouparoo/pipedrive/src/initializers/plugin.ts
+++ plugins/@grouparoo/pipedrive/src/initializers/plugin.ts
@@ -3,15 +3,20 @@ import { Initializer } from "actionhero";
 import { plugin, DestinationSyncMode } from "@grouparoo/core";
 
 import { test } from "./../lib/test";
-
-import { exportRecord } from "../lib/export/exportRecord";
-import { destinationOptions } from "../lib/export/destinationOptions";
-import { destinationMappingOptions } from "../lib/export/destinationMappingOptions";
-import { exportArrayProperties } from "../lib/export/exportArrayProperties";
-
-const templateRoot = path.join(__dirname, "..", "..", "public", "templates");
 import { AppTemplate } from "@grouparoo/app-templates/dist/app";
 import { DestinationTemplate } from "@grouparoo/app-templates/dist/destination/templates";
+
+import { exportRecord as personExportRecord } from "../lib/export/persons/exportRecord";
+import { destinationOptions as personDestinationOptions } from "../lib/export/persons/destinationOptions";
+import { destinationMappingOptions as personDestinationMappingOptions } from "../lib/export/persons/destinationMappingOptions";
+import { exportArrayProperties as personExportArrayProperties } from "../lib/export/persons/exportArrayProperties";
+
+import { exportRecord as orgExportRecord } from "../lib/export/organizations/exportRecord";
+import { destinationOptions as orgDestinationOptions } from "../lib/export/organizations/destinationOptions";
+import { destinationMappingOptions as orgDestinationMappingOptions } from "../lib/export/organizations/destinationMappingOptions";
+import { exportArrayProperties as orgExportArrayProperties } from "../lib/export/organizations/exportArrayProperties";
+
+const templateRoot = path.join(__dirname, "..", "..", "public", "templates");
 
 const packageJSON = require("./../../package.json");
 
@@ -66,10 +71,26 @@ export class Plugins extends Initializer {
           defaultSyncMode,
           options: [],
           methods: {
-            exportRecord,
-            destinationOptions,
-            destinationMappingOptions,
-            exportArrayProperties,
+            exportRecord: personExportRecord,
+            destinationOptions: personDestinationOptions,
+            destinationMappingOptions: personDestinationMappingOptions,
+            exportArrayProperties: personExportArrayProperties,
+          },
+        },
+        {
+          name: "pipedrive-export-organizations",
+          displayName: "Pipedrive Export Organizations",
+          direction: "export",
+          description: "Export records to Pipedrive as Organization contacts",
+          apps: ["pipedrive"],
+          syncModes,
+          defaultSyncMode,
+          options: [],
+          methods: {
+            exportRecord: orgExportRecord,
+            destinationOptions: orgDestinationOptions,
+            destinationMappingOptions: orgDestinationMappingOptions,
+            exportArrayProperties: orgExportArrayProperties,
           },
         },
       ],
