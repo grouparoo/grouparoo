5c1decf8ccddb38d0350ad609e12937237173dd8
Evan Tahler <evan.tahler@grouparoo.com>
2021-11-24 14:28:33 -0800

    Update ui/ui-components/components/record/List.tsx
    Co-authored-by: Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>

--- ui/ui-components/components/record/List.tsx
+++ ui/ui-components/components/record/List.tsx
@@ -114,7 +114,7 @@ export default function RecordsList(props) {
   const primaryProperty = properties.find((property) => property.isPrimaryKey);
   const uniqueRecordPropertyKeys = properties
     .filter((property) =>
-      primaryProperty ? property.id !== primaryProperty.id : true
+      !primaryProperty || propery.id !== primaryProperty.id
     )
     .filter((rule) => rule.unique)
     .map((rule) => rule.key);
