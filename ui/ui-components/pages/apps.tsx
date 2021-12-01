3e4bfda676026b34ec6a4285abb519db596f67ba
Teal Larson <teal.larson@grouparoo.com>
2021-12-01 11:23:52 -0500

    add appRefreshQuery to /apps (#2577)
    * add appRefreshQuery to /apps
    
    * wrap in useCallback, add eslint

--- ui/ui-components/pages/apps.tsx
+++ ui/ui-components/pages/apps.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { UseApi } from "../hooks/useApi";
 import { useOffset, updateURLParams } from "../hooks/URLParams";
 import { useSecondaryEffect } from "../hooks/useSecondaryEffect";
+import { useCallback } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import Link from "../components/GrouparooLink";
@@ -12,11 +13,17 @@ import StateBadge from "../components/badges/StateBadge";
 import { Models, Actions } from "../utils/apiData";
 import { formatTimestamp } from "../utils/formatTimestamp";
 import { ErrorHandler } from "../utils/errorHandler";
+import { SuccessHandler } from "../utils/successHandler";
 import LinkButton from "../components/LinkButton";
+import LoadingButton from "../components/LoadingButton";
+import { grouparooUiEdition } from "../utils/uiEdition";
 import { formatName } from "../utils/formatName";
 
 export default function Page(props) {
-  const { errorHandler }: { errorHandler: ErrorHandler } = props;
+  const {
+    errorHandler,
+    successHandler,
+  }: { errorHandler: ErrorHandler; successHandler: SuccessHandler } = props;
   const router = useRouter();
   const { execApi } = UseApi(props, errorHandler);
   const [apps, setApps] = useState<Models.AppType[]>(props.apps);
@@ -49,6 +56,31 @@ export default function Page(props) {
     }
   }
 
+  const runRefreshQuery = useCallback(
+    async (app: typeof apps[number]) => {
+      setLoading(true);
+      const appRefreshQuery = app.appRefreshQuery;
+      try {
+        const response: Actions.AppRefreshQueryRun = await execApi(
+          "post",
+          `/appRefreshQuery/${appRefreshQuery.id}/run`
+        );
+        if (response?.valueUpdated == true) {
+          successHandler.set({
+            message: `Query returned ${response.appRefreshQuery.value}. Enqueueing Schedules.`,
+          });
+        } else {
+          successHandler.set({
+            message: `Query returned ${response.appRefreshQuery.value}. No schedules enqueued.`,
+          });
+        }
+      } finally {
+        setLoading(false);
+      }
+    },
+    [execApi, successHandler]
+  );
+
   return (
     <>
       <Head>
@@ -74,10 +106,13 @@ export default function Page(props) {
             <th>State</th>
             <th>Created At</th>
             <th>Updated At</th>
+            <th>Refresh</th>
           </tr>
         </thead>
         <tbody>
           {apps.map((app) => {
+            const recurringFrequencyMinutes =
+              app.appRefreshQuery?.recurringFrequency / (60 * 1000);
             return (
               <tr key={`model-${app.id}`}>
                 <td>
@@ -96,6 +131,36 @@ export default function Page(props) {
                 </td>
                 <td>{formatTimestamp(app.createdAt)}</td>
                 <td>{formatTimestamp(app.updatedAt)}</td>
+                <td>
+                  {app.appRefreshQuery ? (
+                    <>
+                      Query: {app.appRefreshQuery.refreshQuery}
+                      <br />
+                      Value: {app.appRefreshQuery.value}
+                      <br />
+                      Last Checked:{" "}
+                      {formatTimestamp(app.appRefreshQuery.lastConfirmedAt)}
+                      <br />
+                      Frequency: Every{" "}
+                      {recurringFrequencyMinutes > 1 ??
+                        recurringFrequencyMinutes}{" "}
+                      Minute{recurringFrequencyMinutes > 1 ?? "s"}
+                      <br />
+                      {grouparooUiEdition() !== "config" && (
+                        <LoadingButton
+                          variant="outline-success"
+                          size="sm"
+                          disabled={loading}
+                          onClick={() => runRefreshQuery(app)}
+                        >
+                          Run Now
+                        </LoadingButton>
+                      )}
+                    </>
+                  ) : (
+                    "No Refresh"
+                  )}
+                </td>
               </tr>
             );
           })}
