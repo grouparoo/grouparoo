02cf895bcd8ef89776a89f3e09b0c42bc52fb949
Evan Tahler <evan.tahler@grouparoo.com>
2021-12-03 15:28:39 -0800

    global swagger path replacement

--- core/src/actions/swagger.ts
+++ core/src/actions/swagger.ts
@@ -82,13 +82,7 @@ export class Swagger extends Action {
         if (!action) return;
 
         const tag = action.name.split(":")[0];
-        const formattedPath = route.path
-          .replace("/v:apiVersion", "")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}");
+        const formattedPath = route.path.replace(/\/:(\w*)/g, "/{$1}");
 
         swaggerPaths[formattedPath] = swaggerPaths[formattedPath] || {};
         swaggerPaths[formattedPath][method] = {
