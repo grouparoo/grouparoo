f5a5bfa3f9120a86c3c9afb15e13c4f5aaea5cce
Evan Tahler <evan.tahler@grouparoo.com>
2021-12-06 12:09:54 -0800

    better lock error message

--- core/src/modules/lockableHelper.ts
+++ core/src/modules/lockableHelper.ts
@@ -26,31 +26,35 @@ export namespace LockableHelper {
         throw new Error(
           `you cannot update this locked ${modelName(instance)} (${
             instance.id
-          }) [${changedCols.map((k) => `\`${k}\``).join(", ")} has changes]`
+          } - ${instance.locked}) [${changedCols
+            .map((k) => `\`${k}\``)
+            .join(", ")} has changes]`
         );
       }
     }
   }
 
   export async function beforeUpdateOptions(instance, hasChanges = true) {
-    if (instance.locked === null || instance.locked === undefined) return;
+    if (!Boolean(instance.locked)) return;
     if (api?.codeConfig?.allowLockedModelChanges !== false) return;
 
     if (hasChanges) {
       throw new Error(
         `you cannot update the options for a locked ${modelName(instance)} (${
           instance.id
-        })`
+        }) - ${instance.locked}`
       );
     }
   }
 
   export async function beforeDestroy(instance) {
-    if (instance.locked === null || instance.locked === undefined) return;
+    if (!Boolean(instance.locked)) return;
     if (api?.codeConfig?.allowLockedModelChanges !== false) return;
 
     throw new Error(
-      `you cannot destroy a locked ${modelName(instance)} (${instance.id})`
+      `you cannot destroy a locked ${modelName(instance)} (${instance.id} - ${
+        instance.locked
+      })`
     );
   }
 }
