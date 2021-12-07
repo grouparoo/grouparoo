b79258ec4b59fb5edf9b38b66ed5c377cc15f1da
Krishna Glick <krishna.glick@grouparoo.com>
2021-12-07 16:28:09 -0500

    Adding eslint

---
+++ plugins/@grouparoo/postgres/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/consistent-indexed-object-style": ["warn", "record"],
+  },
+  ignorePatterns: ["/dist/"],
+};
