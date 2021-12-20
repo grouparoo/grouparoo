bdd648ebb2f01a40d071d99e39e54e9c84121dfe
Paulo Ouriques <ourixilva@gmail.com>
2021-12-20 20:27:02 -0300

    chore: rebase and add migration.

---
+++ plugins/@grouparoo/google-sheets/src/migrations/000001-plugin-google-sheets-aggregationMethod.ts
@@ -0,0 +1,57 @@
+import Sequelize from "sequelize";
+import crypto from "crypto";
+
+export default {
+  up: async (queryInterface: Sequelize.QueryInterface) => {
+    const allProperties = [];
+    const newOptions = [];
+    const [sources] = await queryInterface.sequelize.query(
+      `SELECT * FROM "sources" WHERE type = 'google-sheet-import'`
+    );
+    console.log(sources);
+    for (const source of sources) {
+      const [properties] = await queryInterface.sequelize.query(
+        `SELECT * FROM "properties" WHERE "sourceId" = '${source["id"]}'`
+      );
+      allProperties.push(...properties);
+    }
+
+    for (const property of allProperties) {
+      newOptions.push({
+        id: `opt_${crypto.randomBytes(16).toString("hex")}`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        ownerId: property["id"],
+        ownerType: "property",
+        key: "aggregationMethod",
+        value: "exact",
+        type: "string",
+      });
+    }
+    if (newOptions.length > 0) {
+      await queryInterface.bulkInsert("options", newOptions);
+    }
+  },
+
+  down: async (queryInterface: Sequelize.QueryInterface) => {
+    const allProperties = [];
+    const [sources] = await queryInterface.sequelize.query(
+      `SELECT * FROM "sources" WHERE type = 'google-sheet-import'`
+    );
+    for (const source of sources) {
+      const [properties] = await queryInterface.sequelize.query(
+        `SELECT * FROM "properties" WHERE "sourceId" = '${source["id"]}'`
+      );
+      allProperties.push(...properties);
+    }
+    const propertiesIds = allProperties.map((property) => property.id);
+    if (propertiesIds.length > 0) {
+      await queryInterface.sequelize.query(
+        `DELETE FROM "options" WHERE "key"='aggregationMethod' AND "ownerId" IN(?)`,
+        {
+          replacements: [propertiesIds],
+        }
+      );
+    }
+  },
+};
