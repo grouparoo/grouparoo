a38ff142c116733524a113c8e39e98bbcb39f0ec
Evan Tahler <evan.tahler@grouparoo.com>
2021-12-21 09:44:39 -0800

    Update icons

--- ui/ui-components/components/layouts/Main.tsx
+++ ui/ui-components/components/layouts/Main.tsx
@@ -51,96 +51,29 @@ export default function Main(props) {
 
         {/* Favicon */}
         <link
-          rel="apple-touch-icon-precomposed"
-          sizes="57x57"
-          href="/public/favicon/apple-touch-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="114x114"
-          href="/public/favicon/apple-touch-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="72x72"
-          href="/public/favicon/apple-touch-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="144x144"
-          href="/public/favicon/apple-touch-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="60x60"
-          href="/public/favicon/apple-touch-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="120x120"
-          href="/public/favicon/apple-touch-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="76x76"
-          href="/public/favicon/apple-touch-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="152x152"
-          href="/public/favicon/apple-touch-icon-152x152.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/public/favicon/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
-          href="/public/favicon/favicon-196x196.png"
-          sizes="196x196"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/public/favicon/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/public/favicon/favicon-32x32.png"
           sizes="32x32"
+          href="/public/favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
-          href="/public/favicon/favicon-16x16.png"
           sizes="16x16"
+          href="/public/favicon/favicon-16x16.png"
         />
+        <link rel="manifest" href="/public/favicon/site.webmanifest" />
         <link
-          rel="icon"
-          type="image/png"
-          href="/favicon/favicon-128.png"
-          sizes="128x128"
+          rel="mask-icon"
+          href="/public/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
         />
-        <meta name="msapplication-TileColor" content="#242436" />
-        <meta
-          name="msapplication-TileImage"
-          content="/favicon/mstile-144x144.png"
-        />
-        <meta
-          name="msapplication-square70x70logo"
-          content="/favicon/mstile-70x70.png"
-        />
-        <meta
-          name="msapplication-square150x150logo"
-          content="/favicon/mstile-150x150.png"
-        />
-        <meta
-          name="msapplication-wide310x150logo"
-          content="/favicon/mstile-310x150.png"
-        />
-        <meta
-          name="msapplication-square310x310logo"
-          content="/favicon/mstile-310x310.png"
-        />
+        <meta name="msapplication-TileColor" content="#da532c" />
 
         {/* Google Fonts */}
         <link href={fontUrl} rel="stylesheet" />
