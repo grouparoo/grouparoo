fcb11b84bf7eab43cb2c5b4733c297ffc587faac
Brian Leonard <brian@bleonard.com>
2021-12-29 17:05:46 -0800

    Meta destination options

--- plugins/@grouparoo/airtable/__tests__/with-meta-access/destination-options.ts
+++ plugins/@grouparoo/airtable/__tests__/with-meta-access/destination-options.ts
@@ -5,9 +5,9 @@ import { connect } from "../../src/lib/connect";
 import * as utils from "../utils/shared";
 import { destinationOptions as methodToTest } from "../../src/lib/destination/destinationOptions";
 
-helper.useNock(__filename, getUpdater("BASIC"));
-const appOptions = loadAppOptions("BASIC");
-const tableData = loadTableData("BASIC");
+helper.useNock(__filename, getUpdater("META"));
+const appOptions = loadAppOptions("META");
+const tableData = loadTableData("META");
 let connection;
 
 async function runDestinationOptions(destinationOptions) {
@@ -31,7 +31,8 @@ describe("Destination Options Tests", () => {
   });
 
   describe("Table with Data", () => {
-    const { allName: tableName, allPrimaryKey: primaryKey } = tableData;
+    // TODO: Meta uses ID
+    const { allId: tableId, allPrimaryKey: primaryKey } = tableData;
 
     test("New Options", async () => {
       const result = await runDestinationOptions({
@@ -41,36 +42,36 @@ describe("Destination Options Tests", () => {
       const table = result.tableId;
       const key = result.primaryKey;
 
-      expect(table).toEqual({ type: "text" });
+      expect(table.type).toEqual("list");
+      const all = table.descriptions.indexOf("All");
+      const empty = table.descriptions.indexOf("Empty");
+      expect(table.options[all]).toEqual(tableData.allId);
+      expect(table.options[empty]).toEqual(tableData.emptyId);
+
       expect(key).toEqual({ type: "pending", options: [] });
     });
 
     test("Table Selected", async () => {
       const result = await runDestinationOptions({
         primaryKey: "",
-        tableId: tableName,
+        tableId,
       });
       const table = result.tableId;
       const key = result.primaryKey;
 
-      expect(table).toEqual({ type: "text" });
+      expect(table.type).toEqual("list");
+
       expect(key).toEqual({
         type: "typeahead",
         options: [
           "Name",
-          "f_autoNumber",
           "f_checkbox",
-          "f_count",
-          "f_createdTime",
           "f_currency",
           "f_date",
           "f_dateTime",
           "f_duration",
           "f_email",
-          "f_formula",
-          "f_lastModifiedTime",
           "f_multilineText",
-          "f_multipleLookupValues",
           "f_multipleRecordLinks",
           "f_multipleSelects",
           "f_number",
@@ -78,7 +79,6 @@ describe("Destination Options Tests", () => {
           "f_phoneNumber",
           "f_rating",
           "f_richText",
-          "f_rollup",
           "f_singleLineText",
           "f_singleSelect",
           "f_url",
@@ -88,7 +88,7 @@ describe("Destination Options Tests", () => {
   });
 
   describe("Empty table", () => {
-    const { emptyName: tableName } = tableData;
+    const { emptyId: tableId } = tableData;
 
     test("New Options", async () => {
       const result = await runDestinationOptions({
@@ -98,20 +98,48 @@ describe("Destination Options Tests", () => {
       const table = result.tableId;
       const key = result.primaryKey;
 
-      expect(table).toEqual({ type: "text" });
+      expect(table.type).toEqual("list");
+      const all = table.descriptions.indexOf("All");
+      const empty = table.descriptions.indexOf("Empty");
+      expect(table.options[all]).toEqual(tableData.allId);
+      expect(table.options[empty]).toEqual(tableData.emptyId);
+
       expect(key).toEqual({ type: "pending", options: [] });
     });
 
     test("Table Selected", async () => {
       const result = await runDestinationOptions({
         primaryKey: "",
-        tableId: tableName,
+        tableId,
       });
       const table = result.tableId;
       const key = result.primaryKey;
 
-      expect(table).toEqual({ type: "text" });
-      expect(key).toEqual({ type: "text" }); // can't figure it out!
+      expect(table.type).toEqual("list");
+
+      expect(key).toEqual({
+        type: "typeahead",
+        options: [
+          "Name",
+          "f_checkbox",
+          "f_currency",
+          "f_date",
+          "f_dateTime",
+          "f_duration",
+          "f_email",
+          "f_multilineText",
+          "f_multipleRecordLinks",
+          "f_multipleSelects",
+          "f_number",
+          "f_percent",
+          "f_phoneNumber",
+          "f_rating",
+          "f_richText",
+          "f_singleLineText",
+          "f_singleSelect",
+          "f_url",
+        ],
+      });
     });
   });
 });
