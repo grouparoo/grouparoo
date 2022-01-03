b3d53e3f9b7dac6c63e424d64995d27049465476
Evan Tahler <evantahler@gmail.com>
2022-01-03 03:15:57 -0800

    setup nvm first

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,8 @@ echo "--- CONFIURING CODESPACE ---"
 # general setup
 sudo apt-get upddate
 sudo apt-get install htop vim -y
+nvm install v16
+nvm alias default 16
 
 # configure redis
 sudo apt-get install redis-tools -y
@@ -15,7 +17,6 @@ sudo apt-get install postgresql-client -y
 docker run -p 5432:5432 --name postgres -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_USER=codespace -e POSTGRES_PASSWORD="" -e POSTGRES_DB=grouparoo_development -d postgres
 
 # configure node
-nvm install v16
 npm install -g pnpm
 pnpm install --unsafe-perm
 
