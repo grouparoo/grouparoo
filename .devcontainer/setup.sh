5982c6ad1d52bf93a4e7f1f1772eab7e0fed19a1
Evan Tahler <evantahler@gmail.com>
2022-01-03 03:05:51 -0800

    update setup commands

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,11 +12,13 @@ docker run -p 6379:6379 --name redis -d redis
 
 # configure postgres
 sudo apt-get install postgresql-client -y
-docker run -p 5432:5432 --name postgres -d postgres -e 'POSTGRES_PASSWORD=""' -e 'POSTGRES_HOST_AUTH_METHOD=trust' -e "POSTGRES_DB=grouparoo_development"
+docker run -p 5432:5432 --name postgres -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_USER=codespace -e POSTGRES_PASSWORD="" -e POSTGRES_DB=grouparoo_development -d postgres
 
 # configure node
 nvm install v16
 npm install -g pnpm
 pnpm install --unsafe-perm
 
-
+# setup apps
+cp apps/staging-enterprise/.env.example apps/staging-enterprise/.env
+cp apps/staging-community/.env.example apps/staging-community/.env
