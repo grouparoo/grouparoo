9028af1abb938c8a54f591e57738699db26f5d6d
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-07 14:18:47 -0800

    Update core/__tests__/classes/codeConfig.ts
    Co-authored-by: Krishna Glick <krishna.glick@grouparoo.com>

--- core/__tests__/classes/codeConfig.ts
+++ core/__tests__/classes/codeConfig.ts
@@ -273,7 +273,7 @@ describe("classes/codeConfig", () => {
   });
 });
 
-function shuffleArray(array: any[]) {
+function shuffleArray(array: unknown[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
