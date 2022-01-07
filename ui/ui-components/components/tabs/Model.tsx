9dad75af7dcdc2f30ecb1c8a8d76538aabe8a395
Edmundo Ruiz Ghanem <edmundo.rg@grouparoo.com>
2022-01-07 15:36:14 -0500

    Show tabs and breadcrumbs in model overview for consistent navigation

--- ui/ui-components/components/tabs/Model.tsx
+++ ui/ui-components/components/tabs/Model.tsx
@@ -1,11 +1,13 @@
 import Tabs from "../Tabs";
 import { Models } from "../../utils/apiData";
 
-export default function AppTabs({
-  model,
-}: {
+interface Props {
   model: Models.GrouparooModelType;
-}) {
-  const tabs = ["edit"];
-  return <Tabs name={model.name} draftType={model.type} tabs={tabs} />;
+}
+
+export default function ModelTabs({ model }: Props) {
+  const tabs = ["overview", "edit"];
+  return (
+    <Tabs name={model.name} draftType={model.type} tabs={tabs} hideScope />
+  );
 }
