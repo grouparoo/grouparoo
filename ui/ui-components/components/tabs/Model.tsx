25c877b0dc213adaeebc766d23284708cbf997e7
Edmundo Ruiz Ghanem <edmundo.rg@grouparoo.com>
2022-01-12 10:16:24 -0500

    Only show model tabs in non-community UI

--- ui/ui-components/components/tabs/Model.tsx
+++ ui/ui-components/components/tabs/Model.tsx
@@ -1,12 +1,18 @@
 import Tabs from "../Tabs";
 import { Models } from "../../utils/apiData";
+import { grouparooUiEdition } from "../../utils/uiEdition";
 
 interface Props {
   model: Models.GrouparooModelType;
 }
 
 export default function ModelTabs({ model }: Props) {
+  if (grouparooUiEdition() === "community") {
+    return null;
+  }
+
   const tabs = ["overview", "edit"];
+
   return (
     <Tabs name={model.name} draftType={model.type} tabs={tabs} hideScope />
   );
