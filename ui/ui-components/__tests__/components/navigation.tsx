f60870b3cf9370930e69866ec3b6452611ebd1a9
Edmundo Ruiz Ghanem <edmundo.rg@grouparoo.com>
2022-01-12 15:29:06 -0500

    Add page context to navigation component test

--- ui/ui-components/__tests__/components/navigation.tsx
+++ ui/ui-components/__tests__/components/navigation.tsx
@@ -3,6 +3,7 @@ import mockAxios from "jest-mock-axios";
 import Component from "../../components/Navigation";
 import "../../components/Icons"; // this is needed to load the library
 import commonProps from "../__utils__/commonProps";
+import { PageContext } from "../../contexts/page";
 
 const useRouter = jest.spyOn(require("next/router"), "useRouter");
 
@@ -13,29 +14,33 @@ describe("navigation", () => {
       asPath: "/",
     }));
 
+    const pageContext: PageContext = {
+      currentTeamMember: {
+        firstName: "mario",
+        id: "abc123",
+      },
+      navigationMode: "authenticated",
+      navigation: {
+        navigationItems: [
+          { type: "link", title: "Dashboard", href: "/dashboard" },
+        ],
+        platformItems: [],
+        bottomMenuItems: [
+          {
+            type: "link",
+            title: "Something Cool",
+            href: "/something/cool",
+          },
+          { type: "link", title: "Sign Out", href: "/session/sign-out" },
+        ],
+      },
+      clusterName: { default: false, value: "" },
+    };
+
     render(
-      <Component
-        {...commonProps}
-        currentTeamMember={{
-          firstName: "mario",
-          id: "abc123",
-        }}
-        navigationMode="authenticated"
-        navigation={{
-          navigationItems: [
-            { type: "link", title: "Dashboard", href: "/dashboard" },
-          ],
-          platformItems: [],
-          bottomMenuItems: [
-            {
-              type: "link",
-              title: "Something Cool",
-              href: "/something/cool",
-            },
-            { type: "link", title: "Sign Out", href: "/session/sign-out" },
-          ],
-        }}
-      />
+      <PageContext.Provider value={pageContext}>
+        <Component {...commonProps} />
+      </PageContext.Provider>
     );
   });
 
