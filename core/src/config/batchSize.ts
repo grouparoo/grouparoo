0e152544df6289887ec4d519c673c5764bbe3789
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-19 08:40:53 -0800

    Use `GROUPAROO_INTERNAL_BATCH_SIZE` to configure `config.batchSize.recordProperties`

--- core/src/config/batchSize.ts
+++ core/src/config/batchSize.ts
@@ -13,7 +13,7 @@ export const DEFAULT = {
       // How many Imports and should a Run enqueue in each batch before deferring to associate those Imports already enqueued?
       imports: parseInt(process.env.GROUPAROO_IMPORTS_BATCH_SIZE ?? "1000"),
       // How many Record Properties needing import should we process at once?
-      recordProperties: parseInt(process.env.GROUPAROO_IMPORTS_BATCH_SIZE ?? "500"),
+      recordProperties: parseInt(process.env.GROUPAROO_INTERNAL_BATCH_SIZE ?? "500"),
       // How many Records should a Run try to send at once to Destinations which support batch exporting?
       exports: parseInt(process.env.GROUPAROO_EXPORTS_BATCH_SIZE ?? "100"),
     };
