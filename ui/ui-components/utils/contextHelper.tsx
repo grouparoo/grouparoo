e1e78c93f5d61506635a09ce2e60c17c80cc36dc
Krishna Glick <krishna.glick@grouparoo.com>
2022-01-20 10:00:45 -0500

    Stronger typings

--- ui/ui-components/utils/contextHelper.tsx
+++ ui/ui-components/utils/contextHelper.tsx
@@ -1,18 +1,22 @@
-export type ContextValue = [Context: React.Context<any>, value: any];
+export type ContextValue<T = unknown> = [Context: React.Context<T>, value: T];
 
-export const renderContextProvider = <T extends object>(
+// Note: Once strict mode is on, we can more strongly type this
+export const renderContextProvider = <T extends any>(
   Context: React.Context<T>,
   value: T,
   children: React.ReactNode
 ) => <Context.Provider value={value}>{children}</Context.Provider>;
 
-export const renderNestedContextProviders = (
-  contextValues: ContextValue[],
+export const renderNestedContextProviders = <
+  TContextValues extends ContextValue<T>[],
+  T extends object = object
+>(
+  contextValues: TContextValues,
   children: React.ReactNode
 ): JSX.Element => {
   {
-    if (contextValues?.length > 0) {
-      const [Context, value] = contextValues[0];
+    if (contextValues.length) {
+      const [[Context, value]] = contextValues;
       return renderContextProvider(
         Context,
         value,
