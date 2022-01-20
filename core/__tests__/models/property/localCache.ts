4b0dccc894f9f6c31a5abfeeecbdf3606e18274a
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-20 09:40:54 -0800

    better cache tests

--- core/__tests__/models/property/localCache.ts
+++ core/__tests__/models/property/localCache.ts
@@ -8,6 +8,9 @@ describe("models/property", () => {
   let source: Source;
   let originalPropertyCount: number;
 
+  beforeEach(() => (CachedProperties.expires = new Date().getTime() + 5000));
+  afterAll(() => (CachedProperties.expires = 0));
+
   beforeAll(async () => {
     await helper.factories.properties();
     source = await Source.findOne({ where: { state: "ready" } });
@@ -103,20 +106,47 @@ describe("models/property", () => {
   });
 
   describe("#findOneWithCache", () => {
-    let emailProperty: Property;
+    let firstNameProperty: Property;
 
     beforeAll(async () => {
-      emailProperty = await Property.findOne({ where: { key: "email" } });
+      firstNameProperty = await Property.findOne({
+        where: { key: "firstName" },
+      });
+      await firstNameProperty.update({ key: "FIRST NAME" });
+    });
+
+    test("after a Property is updated, the local cache should be invalid", async () => {
+      expect(CachedProperties.expires).toBeGreaterThan(0);
+      const lastNameProperty = await Property.findOne({
+        where: { key: "lastName" },
+      });
+      await lastNameProperty.update({ key: "LAST NAME" });
+      expect(CachedProperties.expires).toEqual(0);
     });
 
     test("it will find by id by default", async () => {
-      const found = await Property.findOneWithCache(emailProperty.id);
-      expect(found.key).toBe("email");
+      const found = await Property.findOneWithCache(firstNameProperty.id);
+      expect(found.id).toBe("firstName");
+      expect(found.key).toBe("FIRST NAME");
+    });
+
+    test("it will avoid using SQL when a cached property exists", async () => {
+      const cachedEmail = CachedProperties.properties.find(
+        (p) => p.id === firstNameProperty.id
+      );
+      // @ts-ignore
+      cachedEmail.__isCached = true;
+      const found = await Property.findOneWithCache(firstNameProperty.id);
+      expect(found.id).toBe("firstName");
+      expect(found.key).toBe("FIRST NAME");
+      // @ts-ignore
+      expect(found.__isCached).toBe(true);
     });
 
     test("it can find by other keys", async () => {
-      const found = await Property.findOneWithCache("email", "key");
-      expect(found.key).toBe("email");
+      const found = await Property.findOneWithCache("FIRST NAME", null, "key");
+      expect(found.id).toBe("firstName");
+      expect(found.key).toBe("FIRST NAME");
     });
 
     test("a cache miss will invalidate the cache", async () => {
