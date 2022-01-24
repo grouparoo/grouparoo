ff07b452eba07b36ed2c4be73cef6fef872f083e
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-24 08:27:46 -0800

    SQLite Sample Rows no longer `ORDER BY RANDOM()` (#2848)

--- plugins/@grouparoo/sqlite/src/lib/table-import/getSampleRows.ts
+++ plugins/@grouparoo/sqlite/src/lib/table-import/getSampleRows.ts
@@ -7,7 +7,7 @@ export const getSampleRows: GetSampleRowsMethod = async ({
   connection,
   tableName,
 }) => {
-  const query = `SELECT * FROM "${tableName}" ORDER BY RANDOM() LIMIT 10;`;
+  const query = `SELECT * FROM "${tableName}" LIMIT 10;`;
   const out: DataResponseRow[] = await connection.asyncQuery(query);
 
   return out;
