593560e9e68151c8db0c36bdb05290c8837923f9
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-25 14:59:53 -0800

    update migration

--- core/src/migrations/000099-addIncrementalToSchedules.ts
+++ core/src/migrations/000099-addIncrementalToSchedules.ts
@@ -11,6 +11,19 @@ export default {
       defaultValue: true,
     });
 
+    const [nonIncrementalSources]: [Record<string, any>[], unknown] =
+      await queryInterface.sequelize.query(
+        `SELECT * FROM "sources" WHERE type IN ('csv-import-table', 'google-sheet-import')`
+      );
+
+    if (nonIncrementalSources.length > 0) {
+      await queryInterface.sequelize.query(
+        `UPDATE schedules SET incremental=false WHERE id IN (${nonIncrementalSources
+          .map((s) => s.id)
+          .join(", ")})`
+      );
+    }
+
     await queryInterface.changeColumn("schedules", "incremental", {
       type: DataTypes.BOOLEAN,
       allowNull: false,
