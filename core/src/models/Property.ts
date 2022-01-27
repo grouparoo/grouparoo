6340e30e22624a69197fe5ce3bab7495af2a71c3
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-01-27 16:06:46 -0500

    Ensure that Property model is unique when it's also primary key (#2869)
    * Add before save checker to property to ensure a primary key property is unique
    
    * Update error message when property primary key is not unique

--- core/src/models/Property.ts
+++ core/src/models/Property.ts
@@ -389,6 +389,15 @@ export class Property extends LoggedModel<Property> {
   }
 
   @BeforeSave
+  static async ensureUniquePrimaryKey(instance: Property) {
+    if (instance.isPrimaryKey && !instance.unique) {
+      throw new Error(
+        `Property "${instance.key}" must be unique because it‘s the model‘s Primary Key.`
+      );
+    }
+  }
+
+  @BeforeSave
   static async ensureOptions(instance: Property) {
     const source = await Source.findById(instance.sourceId);
     await source.validateOptions(null);
