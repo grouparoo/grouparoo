7b5abcda401c888a4988ef3fdb02ff52e2aea5b9
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-27 12:23:15 -0800

    mailchimp import can be incremental

--- plugins/@grouparoo/mailchimp/src/lib/import/connection.ts
+++ plugins/@grouparoo/mailchimp/src/lib/import/connection.ts
@@ -10,7 +10,7 @@ const connection: PluginConnection = {
   displayName: "Mailchimp Import Contacts",
   direction: "import",
   description: "Import or update records with data from Mailchimp contacts.",
-  supportIncrementalSchedule: false,
+  supportIncrementalSchedule: true,
   apps: ["mailchimp", "mailchimp-oauth"],
   options: [
     {
