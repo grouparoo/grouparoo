f9459d04e8cb7c8e902c6947839b7d356fc7dbe0
Pedro S. Lopez <pedroslopez@me.com>
2022-01-27 10:36:22 -0400

    add command tests

--- core/__tests__/bin/retryExports.ts
+++ core/__tests__/bin/retryExports.ts
@@ -1,13 +1,18 @@
 import { helper } from "@grouparoo/spec-helper";
+import { Destination, Export } from "../../src";
 import { RetryExportsCLI } from "../../src/bin/retryExports";
 
 describe("bin/retryExports", () => {
   helper.grouparooTestServer({ truncate: true, enableTestPlugin: true });
 
-  beforeAll(async () => await helper.factories.properties());
-
+  let destination: Destination;
   let messages: string[] = [];
   const spies: jest.SpyInstance[] = [];
+
+  beforeAll(async () => {
+    await helper.factories.properties();
+    destination = await helper.factories.destination();
+  });
 
   beforeEach(() => {
     messages = [];
@@ -100,11 +105,127 @@ describe("bin/retryExports", () => {
     test("it errors if passed destinations do not exist", async () => {
       const command = new RetryExportsCLI();
       await command.run({
-        params: { startAgoSeconds: "1000", destinationIds: ["foo"] },
+        params: {
+          startAgoSeconds: "1000",
+          destinationIds: [destination.id, "foo"],
+        },
+      });
+
+      const output = messages.join(" ");
+      expect(output).toContain('Destination with id "foo" was not found');
+    });
+  });
+
+  describe("with exports", () => {
+    let failedExport: Export;
+    let otherFailedExport: Export;
+    let instance: RetryExportsCLI;
+
+    beforeEach(async () => {
+      instance = new RetryExportsCLI();
+
+      failedExport = await helper.factories.export(null, destination);
+      await failedExport.update({ state: "failed" });
+
+      otherFailedExport = await helper.factories.export(null);
+      await otherFailedExport.update({ state: "failed" });
+    });
+
+    afterEach(async () => {
+      await failedExport.destroy();
+      await otherFailedExport.destroy();
+    });
+
+    test("can preview exports to retry", async () => {
+      await instance.run({
+        params: { startAgoSeconds: "3600", preview: true },
       });
       const output = messages.join(" ");
+      expect(output).toContain(
+        "ℹ️  (Preview) Found 2 failed Exports to retry."
+      );
 
-      expect(output).toContain('Destination with id "foo" was not found');
+      await failedExport.reload();
+      expect(failedExport.state).toBe("failed");
+
+      await otherFailedExport.reload();
+      expect(otherFailedExport.state).toBe("failed");
+    });
+
+    test("can preview exports to retry for a destination", async () => {
+      await instance.run({
+        params: {
+          startAgoSeconds: "3600",
+          preview: true,
+          destinationIds: [destination.id],
+        },
+      });
+      const output = messages.join(" ");
+      expect(output).toContain(
+        "ℹ️  (Preview) Found 1 failed Exports to retry."
+      );
+
+      await failedExport.reload();
+      expect(failedExport.state).toBe("failed");
+
+      await otherFailedExport.reload();
+      expect(otherFailedExport.state).toBe("failed");
+    });
+
+    test("can retry exports", async () => {
+      await instance.run({
+        params: {
+          startAgoSeconds: "3600",
+        },
+      });
+      const output = messages.join(" ");
+      expect(output).toContain(
+        "✅ Success! 2 failed Exports marked to be retried."
+      );
+
+      await failedExport.reload();
+      expect(failedExport.state).toBe("pending");
+
+      await otherFailedExport.reload();
+      expect(otherFailedExport.state).toBe("pending");
+    });
+
+    test("can retry exports for a destination", async () => {
+      await instance.run({
+        params: {
+          startAgoSeconds: "3600",
+          destinationIds: [destination.id],
+        },
+      });
+      const output = messages.join(" ");
+      expect(output).toContain(
+        "✅ Success! 1 failed Exports marked to be retried."
+      );
+
+      await failedExport.reload();
+      expect(failedExport.state).toBe("pending");
+
+      await otherFailedExport.reload();
+      expect(otherFailedExport.state).toBe("failed");
+    });
+
+    test("returns different messaging if there were no exports to retry", async () => {
+      const destination2 = await helper.factories.destination();
+
+      await instance.run({
+        params: {
+          startAgoSeconds: "3600",
+          destinationIds: [destination2.id],
+        },
+      });
+      const output = messages.join(" ");
+      expect(output).toContain("✨ Success! No failed Exports found to retry.");
+
+      await failedExport.reload();
+      expect(failedExport.state).toBe("failed");
+
+      await otherFailedExport.reload();
+      expect(otherFailedExport.state).toBe("failed");
     });
   });
 });
