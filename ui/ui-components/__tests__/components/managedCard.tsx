a9f9c9f894ab9ee30f5352dcf6880802459ae1f9
Reina Feather <reina.feather@grouparoo.com>
2022-02-03 08:44:21 -0800

    Use a cleaner snapshot

--- ui/ui-components/__tests__/components/managedCard.tsx
+++ ui/ui-components/__tests__/components/managedCard.tsx
@@ -4,8 +4,8 @@ import ManagedCard from "../../components/lib/ManagedCard";
 describe("managedCard", () => {
   it("will render a spinner when in the loading state", async () => {
     let card = render(<ManagedCard title="test" loading={false}></ManagedCard>);
-    expect(card).toMatchSnapshot("noLoading");
+    expect(card.container).toMatchSnapshot("noLoading");
     card = render(<ManagedCard title="test" loading={true}></ManagedCard>);
-    expect(card).toMatchSnapshot("loading");
+    expect(card.container).toMatchSnapshot("loading");
   });
 });
