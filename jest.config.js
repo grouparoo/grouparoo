89c3183c99c8c71f710c4724a1011ef7d9df53a7
Teal Larson <teal.larson@grouparoo.com>
2022-02-07 13:51:49 -0500

    cleanup pt 1

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,8 @@
-const { helper } = require("@grouparoo/spec-helper");
-
 module.exports = {
   testEnvironment: "node",
-  testTimeout: helper.defaultTime,
+  testTimeout: 10000,
   maxWorkers: "50%",
-  setupFiles: ["<rootDir>/jest.setup.js"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  testPathIgnorePatterns: [
-    "<rootDir>/__tests__/utils",
-    "<rootDir>/__tests__/fixtures",
-    "<rootDir>/src/.*/*.ts",
-    "<rootDir>/dist/.*/*.ts",
-    "<rootDir>/dist/.*/*.js",
-  ],
 };
