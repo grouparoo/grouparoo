213a238613191273bf0133bdc96374cfc04b7fec
Edmundo Ruiz Ghanem <edmundo.rg@grouparoo.com>
2022-02-07 15:40:45 -0500

    Update client to use interceptors and consolidate catch-all browser-side errors into Axios interceptor

--- ui/ui-components/client/client.ts
+++ ui/ui-components/client/client.ts
@@ -1,22 +1,16 @@
-import axios, {
-  AxiosRequestConfig,
-  AxiosRequestHeaders,
-  AxiosResponse,
-  Method,
-} from "axios";
+import axios, { AxiosRequestConfig, AxiosRequestHeaders, Method } from "axios";
 import type { IncomingMessage, ServerResponse } from "http";
 import PackageJSON from "../package.json";
-import { errorHandler } from "../eventHandlers";
 import { getRequestContext } from "../utils/appContext";
 import type { NextContext, NextContextName } from "../utils/appContext";
-import type { ErrorHandler } from "../eventHandlers/errorHandler";
-import { isBrowser } from "../utils/isBrowser";
+import { errorHandler } from "../eventHandlers";
 import { getRedirectFromErrorCode } from "../utils/getRedirectFromErrorCode";
+import { isBrowser } from "../utils/isBrowser";
+
 import { ClientCache, ClientCacheGetObject } from "./clientCache";
 
 interface ClientRequestOptions {
   useCache: boolean;
-  errorHandler: ErrorHandler;
 }
 
 const API_VERSION = process.env.API_VERSION || "v1";
@@ -38,13 +32,53 @@ const apiInstance = axios.create({
   },
 });
 
+apiInstance.interceptors.response.use(
+  (response) => {
+    if (response.data && response.data.error) {
+      const error = response.data.error.message
+        ? response.data.error.message
+        : response.data.error;
+
+      throw error;
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.data && error.response.data.error) {
+      const err =
+        error.response?.data?.error?.message ??
+        error.response?.data?.error ??
+        error;
+
+      const newError = new Error(err);
+      newError["code"] = error.response?.data?.error?.code;
+      throw newError;
+    } else {
+      throw error;
+    }
+  }
+);
+
+if (isBrowser()) {
+  apiInstance.interceptors.response.use(undefined, (error) => {
+    errorHandler.set({ message: error });
+
+    const redirect = getRedirectFromErrorCode(
+      error.code,
+      window.location.pathname
+    );
+
+    if (redirect) {
+      window.location.href = redirect.destination;
+      return new Promise(() => {});
+    }
+
+    return {};
+  });
+}
+
 export class Client {
   private cache = new ClientCache();
-
-  private static readonly defaultOptions: ClientRequestOptions = {
-    useCache: true,
-    errorHandler: isBrowser() ? errorHandler : undefined,
-  };
 
   constructor(
     private getRequestContext: () => {
@@ -54,127 +88,63 @@ export class Client {
     } = () => ({})
   ) {}
 
-  private redirectOnAccessError({ code }: { code: string }): boolean {
-    if (!isBrowser()) {
-      return false;
-    }
-
-    const redirect = getRedirectFromErrorCode(code, window.location.pathname);
-
-    if (redirect) {
-      window.location.href = redirect.destination;
-    }
-
-    return !!redirect;
-  }
-
-  private async sendRequest<Response>(
-    config: AxiosRequestConfig,
-    options: Partial<ClientRequestOptions>,
-    unlock?: ClientCacheGetObject["unlock"]
-  ) {
-    try {
-      const response: AxiosResponse<Response & { error?: Error }> =
-        await apiInstance.request<Response>(config);
-
-      if (unlock) {
-        unlock(response.data);
-      }
-
-      if (response.data && response.data.error) {
-        throw response.data.error.message
-          ? response.data.error.message
-          : response.data.error;
-      }
-      return response.data;
-    } catch (error) {
-      if (unlock) {
-        unlock();
-      }
-
-      if (error.response && error.response.data && error.response.data.error) {
-        if (this.redirectOnAccessError(error.response.data.error)) {
-          if (options.errorHandler && error.response.data.error.message) {
-            options.errorHandler.set({
-              message: error.response.data.error.message,
-            });
-          }
-          return new Promise<Response>(() => {});
-        }
-
-        const err =
-          error.response?.data?.error?.message ??
-          error.response?.data?.error ??
-          error;
-
-        const newError = new Error(err);
-        newError["code"] = error.response?.data?.error?.code;
-
-        throw newError;
-      } else {
-        throw error;
-      }
-    }
-  }
-
   public async request<Response = any>(
     verb: Method = "get",
     path: string,
     data: AxiosRequestConfig["data"] = {},
     options: Partial<ClientRequestOptions> = {}
   ): Promise<Response> {
-    options = { ...Client.defaultOptions, ...options };
+    options = { useCache: true, ...options };
     const headers: AxiosRequestHeaders = {};
 
-    try {
-      const { req } = this.getRequestContext();
-      if (req?.headers?.cookie) {
-        headers["X-GROUPAROO-SERVER-TOKEN"] = SERVER_TOKEN;
-        headers["cookie"] = req?.headers?.cookie;
-        options.useCache = false; // do not ever cache responses on the server
-      }
+    const { req } = this.getRequestContext();
+    if (req?.headers?.cookie) {
+      headers["X-GROUPAROO-SERVER-TOKEN"] = SERVER_TOKEN;
+      headers["cookie"] = req?.headers?.cookie;
+      options.useCache = false; // do not ever cache responses on the server
+    }
 
-      const config: AxiosRequestConfig = {
-        url: path,
-        method: verb.toLowerCase() as Method,
-        headers,
-      };
+    const config: AxiosRequestConfig = {
+      url: path,
+      method: verb.toLowerCase() as Method,
+      headers,
+    };
 
-      data.csrfToken = getCsrfToken();
+    data.csrfToken = getCsrfToken();
 
-      if (data) {
-        for (const i in data) {
-          if (data[i] === null || data[i] === undefined) {
-            delete data[i];
-          }
-        }
-
-        if (config.method === "get") {
-          config.params = data;
-        } else {
-          config.data = data;
+    if (data) {
+      for (const i in data) {
+        if (data[i] === null || data[i] === undefined) {
+          delete data[i];
         }
       }
 
-      let unlock: ClientCacheGetObject["unlock"];
-      if (config.method === "get" && options.useCache) {
-        const { cacheData, unlock: _unlock } = await this.cache.get<Response>(
-          config.url + JSON.stringify(data)
-        );
-        if (cacheData) {
-          return cacheData;
-        }
-        unlock = _unlock;
+      if (config.method === "get") {
+        config.params = data;
       } else {
-        this.cache.clear();
+        config.data = data;
       }
+    }
 
-      return await this.sendRequest(config, options, unlock);
-    } catch (error) {
-      if (options.errorHandler) {
-        options.errorHandler.set({ message: error });
-        return {} as Response;
+    let unlock: ClientCacheGetObject["unlock"];
+    if (config.method === "get" && options.useCache) {
+      const { cacheData, unlock: _unlock } = await this.cache.get<Response>(
+        config.url + JSON.stringify(data)
+      );
+      if (cacheData) {
+        return cacheData;
       }
+      unlock = _unlock;
+    } else {
+      this.cache.clear();
+    }
+
+    try {
+      const response = await apiInstance.request<Response>(config);
+      unlock?.(response.data);
+      return response.data;
+    } catch (error) {
+      unlock?.();
       throw error;
     }
   }
