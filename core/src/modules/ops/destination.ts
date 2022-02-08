043b2a627e132587dc9601de04bf21b63285d4d1
Evan Tahler <evan.tahler@grouparoo.com>
2022-02-08 11:34:31 -0800

    guard against no remainig exports to consider

--- core/src/modules/ops/destination.ts
+++ core/src/modules/ops/destination.ts
@@ -935,10 +935,12 @@ export namespace DestinationOps {
 
     await ExportOps.completeBatch(exportsWithoutChanges);
 
-    const mostRecentExportIds = await Export.sequelize
-      .query(
-        {
-          query: `
+    const mostRecentExportIds =
+      exportsWithChanges.length > 0
+        ? await Export.sequelize
+            .query(
+              {
+                query: `
     SELECT
       "id"
     FROM (
@@ -952,17 +954,18 @@ export namespace DestinationOps {
         AND "exports"."recordId" IN (?)) AS __ranked
     WHERE
       "__ranked"."__rownum" = 1;`,
-          values: [
-            destination.id,
-            exportsWithChanges.map(({ recordId }) => recordId),
-          ],
-        },
-        {
-          type: "SELECT",
-          model: Export,
-        }
-      )
-      .then((exports) => exports.map((e) => e.id));
+                values: [
+                  destination.id,
+                  exportsWithChanges.map(({ recordId }) => recordId),
+                ],
+              },
+              {
+                type: "SELECT",
+                model: Export,
+              }
+            )
+            .then((exports) => exports.map((e) => e.id))
+        : [];
 
     try {
       for (const consideredExport of exportsWithChanges) {
