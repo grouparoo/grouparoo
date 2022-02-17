d6e0980a71a3f539d047e23e845b952835783b57
Krishna Glick <krishna.glick@grouparoo.com>
2022-02-17 15:44:23 -0500

    Fixed email tests

--- core/__tests__/models/recordProperty/recordProperty.ts
+++ core/__tests__/models/recordProperty/recordProperty.ts
@@ -190,7 +190,7 @@ describe("models/recordProperty", () => {
         expect(response).toBe("mario@example.com");
       });
 
-      test("emails are lower cased", async () => {
+      test("emails are not lower cased", async () => {
         const recordProperty = new RecordProperty({
           recordId: record.id,
           propertyId: emailProperty.id,
@@ -211,9 +211,6 @@ describe("models/recordProperty", () => {
           "someone.com",
           "someone.com@",
           "someone@site",
-          "someone with spaces@site.com",
-          "someone@site.com ",
-          " someone@site.com",
         ];
 
         for (const e of badEmails) {
@@ -222,6 +219,41 @@ describe("models/recordProperty", () => {
           expect(response).toBe(null);
           expect(recordProperty.invalidValue).toBe(e);
           expect(recordProperty.invalidReason).toBe("Invalid email value");
+        }
+      });
+
+      test("weird emails validate", async () => {
+        const recordProperty = new RecordProperty({
+          recordId: record.id,
+          propertyId: emailProperty.id,
+        });
+
+        const weirdEmails = [
+          "email@example.com",
+          "firstname.lastname@example.com",
+          "email@subdomain.example.com",
+          "firstname+lastname@example.com",
+          "email@123.123.123.123",
+          "email@[123.123.123.123]",
+          '"email"@example.com',
+          "1234567890@example.com",
+          "email@example-one.com",
+          "_______@example.com",
+          "email@example.name",
+          "email@example.museum",
+          "email@example.co.jp",
+          "firstname-lastname@example.com",
+          "much.”more unusual”@example.com",
+          "very.unusual.”@”.unusual.com@example.com",
+          'very.”(),:;<>[]”.VERY.”very@\\ "very”.unusual@strange.example.com',
+        ];
+
+        for (const e of weirdEmails) {
+          await recordProperty.setValue(e);
+          const response = await recordProperty.getValue();
+          expect(response).toBe(e);
+          expect(recordProperty.invalidValue).toBe(null);
+          expect(recordProperty.invalidReason).toBe(null);
         }
       });
 
