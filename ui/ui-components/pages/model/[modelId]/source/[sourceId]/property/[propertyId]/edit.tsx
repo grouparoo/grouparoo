f7287fe8d61c1d4d9d91576a6a8aa155204e3af3
Krishna Glick <krishna.glick@grouparoo.com>
2022-02-22 09:57:30 -0500

    Unnecessary field removed

--- ui/ui-components/pages/model/[modelId]/source/[sourceId]/property/[propertyId]/edit.tsx
+++ ui/ui-components/pages/model/[modelId]/source/[sourceId]/property/[propertyId]/edit.tsx
@@ -370,10 +370,6 @@ const Page: NextPageWithInferredProps<typeof getServerSideProps> = ({
                       disabled={loading}
                     />
                   </Form.Group>
-                  <Form.Group controlId="sourceId">
-                    <Form.Label>Property Source</Form.Label>
-                    <Form.Control as="input" disabled value={source.name} />
-                  </Form.Group>
                   <hr />
                   <p>
                     <strong>
