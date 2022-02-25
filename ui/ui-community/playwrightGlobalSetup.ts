97e66d43b903491bbf53fe13ae6e5b587dbf8bdd
Teal Larson <teal.larson@grouparoo.com>
2022-02-25 09:33:36 -0500

    rebasing things

---
+++ ui/ui-community/playwrightGlobalSetup.ts
@@ -0,0 +1,14 @@
+process.env.GROUPAROO_INJECTED_PLUGINS = JSON.stringify({
+  "@grouparoo/ui-community": { path: __dirname },
+});
+import { PlaywrightTestConfig } from "@playwright/test";
+
+// @ts-ignore
+process.env.NODE_ENV = "test";
+process.env.NEXT_DISABLED = "false";
+process.env.GROUPAROO_UI_EDITION = "enterprise";
+
+require("@grouparoo/spec-helper/dist/lib/environment");
+
+let globalSetup = (testConfig: PlaywrightTestConfig) => {};
+export default globalSetup;
